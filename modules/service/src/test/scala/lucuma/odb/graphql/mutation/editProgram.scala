7a3f5c1aee04d9d6a9c4ec488bb54e3ca919174d
Rob Norris <rob_norris@mac.com>
2022-02-17 13:33:24 -0600

    test stubs

--- modules/service/src/test/scala/lucuma/odb/graphql/mutation/editProgram.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/mutation/editProgram.scala
@@ -4,30 +4,54 @@ package mutation
 import io.circe.literal._
 import lucuma.odb.graphql.OdbSuite
 import cats.syntax.all._
+import lucuma.core.model.Partner
 
 class editProgram extends OdbSuite {
 
-  val pi       = TestUsers.Standard.pi(1, 30)
-  val guest    = TestUsers.guest(2)
-  val service  = TestUsers.service(3)
+  val pi       = TestUsers.Standard.pi(1, 101)
+  val ngo      = TestUsers.Standard.ngo(2, 102, Partner.Ca)
+  val staff    = TestUsers.Standard.staff(3, 103)
+  val admin    = TestUsers.Standard.admin(4, 104)
+  val guest    = TestUsers.guest(5)
+  val service  = TestUsers.service(6)
 
-  val validUsers = List(pi, guest, service).toList
+  val validUsers = List(pi, ngo, staff, admin, guest, service).toList
 
-  test("edits are applied".ignore) {
+  test("edits are applied".fail) {
     // create a program
     // edit it, ensure edits are applied
   }
 
-  test("empty edit is an error".ignore) {
+  test("empty edit is an error".fail) {
     // create a program
     // ensure that an empty edit fails
   }
 
-  test("Guest/Pi can edit program with Pi/Coi") {
+  test("Guest + Standard/pi can edit if they have Pi/Coi roles".fail) {
   }
 
+  test("Guest + Standard/pi cannot edit without a role, nor with Observer or Support role".fail) {
+  }
 
-  test("Staff/Admin/Service can edit any program") {
+  test("Guest + Standard/ngo (non-admin) can edit with matching partner support role.".fail) {
+  }
+
+  test("Guest + Standard/ngo (non-admin) cannot edit a program with a matching time allocation.".fail) {
+  }
+
+  test("Guest + Standard/ngo (non-admin) cannot edit without a role, nor with coi, observer, non-matching partner support, or staff support role.".fail) {
+  }
+
+  test("Guest + Standard/ngo (admin) can edit with matching partner support role.".fail) {
+  }
+
+  test("Guest + Standard/ngo (admin) can edit a program with a matching time allocation.".fail) {
+  }
+
+  test("Guest + Standard/ngo (admin) cannot edit without a role, nor with coi, observer, non-matching partner support, or staff support role.".fail) {
+  }
+
+  test("Service + Standard/staff,admin users can edit regardless of roles".fail) {
   }
 
 }
