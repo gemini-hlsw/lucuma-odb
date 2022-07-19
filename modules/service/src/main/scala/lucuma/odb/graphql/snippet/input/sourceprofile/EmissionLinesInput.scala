b1e1d6a7350b82d442963330b07aba0ccdae1c02
Rob Norris <rob_norris@mac.com>
2022-07-19 12:24:02 -0500

    checkpoint

--- modules/service/src/main/scala/lucuma/odb/graphql/snippet/input/sourceprofile/EmissionLinesInput.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/snippet/input/sourceprofile/EmissionLinesInput.scala
@@ -14,7 +14,7 @@ import lucuma.core.model.EmissionLine
 import lucuma.core.model.SpectralDefinition.EmissionLines
 import lucuma.odb.graphql.util.Bindings._
 
-import scala.collection.immutable.TreeMap
+import scala.collection.immutable.{ SortedMap, TreeMap }
 
 object EmissionLinesInput {
 
@@ -22,17 +22,37 @@ object EmissionLinesInput {
     Order[Wavelength].toOrdering
 
   object Integrated {
-    val CreateBinding = createBinding[Integrated](
-      EmissionLineInput.Integrated.CreateBinding,
-      FluxDensityContinuumInput.Integrated.Binding,
-    )
+
+    val CreateBinding: Matcher[EmissionLines[Integrated]] =
+      createBinding[Integrated](
+        EmissionLineInput.Integrated.CreateBinding,
+        FluxDensityContinuumInput.Integrated.Binding,
+      )
+
+    val EditBinding: Matcher[EmissionLines[Integrated] => EmissionLines[Integrated]] =
+      editBinding[Integrated](
+        EmissionLineInput.Integrated.CreateBinding,
+        EmissionLineInput.Integrated.EditBinding,
+        FluxDensityContinuumInput.Integrated.Binding,
+      )
+
   }
 
   object Surface {
-    val CreateBinding = createBinding[Surface](
-      EmissionLineInput.Surface.CreateBinding,
-      FluxDensityContinuumInput.Surface.Binding,
-    )
+
+    val CreateBinding: Matcher[EmissionLines[Surface]] =
+      createBinding[Surface](
+        EmissionLineInput.Surface.CreateBinding,
+        FluxDensityContinuumInput.Surface.Binding,
+      )
+
+    val EditBinding: Matcher[EmissionLines[Surface] => EmissionLines[Surface]] =
+      editBinding[Surface](
+        EmissionLineInput.Surface.CreateBinding,
+        EmissionLineInput.Surface.EditBinding,
+        FluxDensityContinuumInput.Surface.Binding,
+      )
+
   }
 
   def createBinding[A](
@@ -50,6 +70,35 @@ object EmissionLinesInput {
           case (Some(lines), None, None, Some(fluxDensityContinuum)) => Result(EmissionLines(lines.to(TreeMap), fluxDensityContinuum))
           case (Some(lines), _, _, Some(fluxDensityContinuum))       => Result.warning("editLines and deleteLines are ignored on creation.", EmissionLines(lines.to(TreeMap), fluxDensityContinuum))
           case _                                                     => Result.failure("Both lines and fluxDensityContinuum are required on creation.")
+        }
+    }
+
+  // y u not in stdlib?
+  private def edit[A, B](m: SortedMap[A, B], k: A, f: B => B): SortedMap[A, B] =
+    m.get(k) match {
+      case None => m
+      case Some(v) => m.updated(k, f(v))
+    }
+
+  def editBinding[A](
+    line: Matcher[(Wavelength, EmissionLine[A])],
+    editLine: Matcher[(Wavelength, EmissionLine[A] => EmissionLine[A])],
+    fluxDensityContinuum:  Matcher[Measure[PosBigDecimal] Of FluxDensityContinuum[A]],
+  ): Matcher[EmissionLines[A] => EmissionLines[A]] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        line.List.Option("lines", rLines),
+        editLine.List.Option("editLines", rEditLines),
+        WavelengthInput.Binding.List.Option("deleteLines", rDeleteLines),
+        fluxDensityContinuum.Option("fluxDensityContinuum", rFluxDensityContinuum),
+      ) =>
+        (rLines, rEditLines, rDeleteLines, rFluxDensityContinuum).parMapN {
+          (lines, editLines, deleteLines, fluxDensityContinuum) => in =>
+            val a0 = lines.foldLeft(in)((in, ls) => in.copy(lines = ls.to(TreeMap)))
+            val a1 = editLines.foldLeft(a0)((in, es) => in.copy(lines = es.foldLeft(in.lines)((m, e) => edit(m, e._1, e._2))))
+            val a2 = deleteLines.foldLeft(a1)((in, ks) => in.copy(lines = in.lines -- ks))
+            val a3 = fluxDensityContinuum.foldLeft(a2)((in, fdc) => in.copy(fluxDensityContinuum = fdc))
+            a3
         }
     }
 
--- modules/service/src/main/scala/lucuma/odb/graphql/snippet/input/sourceprofile/GaussianInput.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/snippet/input/sourceprofile/GaussianInput.scala
@@ -21,4 +21,19 @@ object GaussianInput {
         }
     }
 
+  val EditBinding: Matcher[SourceProfile.Gaussian => Result[SourceProfile.Gaussian]] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        AngleInput.Binding.Option("fwhm", rFwhm),
+        SpectralDefinitionInput.Integrated.EditBinding.Option("spectralDefinition", rSpectralDefinition)
+      ) =>
+        (rFwhm, rSpectralDefinition).parMapN {
+          case (None, None)       => g => Result(g)
+          case (Some(v), None)    => g => Result(g.copy(fwhm = v))
+          case (None, Some(f))    => g => f(g.spectralDefinition).map(sd => g.copy(spectralDefinition = sd))
+          case (Some(v), Some(f)) => g => f(g.spectralDefinition).map(sd => g.copy(fwhm = v, spectralDefinition = sd))
+        }
+
+    }
+
 }
