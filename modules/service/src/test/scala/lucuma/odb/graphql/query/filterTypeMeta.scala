32281d701cc05f941e6576cce5d39be7fb5dfb68
Rob Norris <rob_norris@mac.com>
2022-07-21 13:26:42 -0500

    enum FilterType, type FilterTypeMeta, Query.filterTypeMeta

--- modules/service/src/test/scala/lucuma/odb/graphql/query/filterTypeMeta.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/filterTypeMeta.scala
@@ -7,9 +7,7 @@ package query
 import cats.syntax.all._
 import io.circe.literal._
 import lucuma.odb.graphql.OdbSuite
-import munit.IgnoreSuite
 
-@IgnoreSuite
 class filterTypeMeta extends OdbSuite {
 
   val pi       = TestUsers.Standard.pi(1, 30)
