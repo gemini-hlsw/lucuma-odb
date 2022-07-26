d24adbbb028b0bebc3631aa494552daa5ab37b4d
Shane Walker <swalker@gemini.edu>
2022-07-26 16:35:46 -0400

    merge constraints into observation

--- modules/service/src/main/scala/lucuma/odb/graphql/snippet/ObservationSnippet.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/snippet/ObservationSnippet.scala
@@ -22,26 +22,19 @@ import io.circe.{Encoder, Json}
 import lucuma.core.enums.{CloudExtinction, ImageQuality, SkyBackground, WaterVapor}
 import lucuma.core.model.ElevationRange.AirMass.DecimalValue
 import lucuma.core.model.ElevationRange.HourAngle.DecimalHour
-import lucuma.core.model.Observation
-import lucuma.core.model.Program
-import lucuma.core.model.User
+import lucuma.core.model.{ConstraintSet, Observation, Program, User}
 import lucuma.odb.data.Existence
 import lucuma.odb.data.ObsActiveStatus
 import lucuma.odb.data.ObsStatus
 import lucuma.odb.graphql.snippet.input.ConstraintSetInput
 import lucuma.odb.graphql.util._
-import lucuma.odb.service.{ConstraintSetService, ObservationService}
+import lucuma.odb.service.ObservationService
 import lucuma.odb.service.ObservationService.InsertObservationResponse.NotAuthorized
 import lucuma.odb.service.ObservationService.InsertObservationResponse.Success
 import lucuma.odb.util.Codecs._
 import skunk.Session
 
 object ObservationSnippet {
-
-  final case class Services[F[_]](
-    obs: ObservationService[F],
-    cs:  ConstraintSetService[F]
-  )
 
   def apply[F[_]: MonadCancelThrow](
     m:      SnippetMapping[F] with SkunkMapping[F] with MutationCompanionOps[F],
@@ -69,12 +62,7 @@ object ObservationSnippet {
     val AirMassRangeType    = schema.ref("AirMassRange")
     val HourAngleRangeType  = schema.ref("HourAngleRange")
 
-    val pool = dbPool.map { s =>
-      Services[F](
-        ObservationService.fromUserAndSession(user, s),
-        ConstraintSetService.fromSession(user, s)
-      )
-    }
+    val pool = dbPool.map(ObservationService.fromUserAndSession(user, _))
 
     object Predicates {
 
@@ -88,26 +76,22 @@ object ObservationSnippet {
 
     // Column references for our mapping.
     object ObservationTable extends TableDef("t_observation") {
-      val ProgramId: m.ColumnRef    = col("c_program_id", program_id)
-      val Id: m.ColumnRef           = col("c_observation_id", observation_id)
-      val Existence: m.ColumnRef    = col("c_existence", existence)
-      val Name: m.ColumnRef         = col("c_name", text_nonempty.opt)
+      val ProgramId: m.ColumnRef    = col("c_program_id",          program_id)
+      val Id: m.ColumnRef           = col("c_observation_id",      observation_id)
+      val Existence: m.ColumnRef    = col("c_existence",           existence)
+      val Name: m.ColumnRef         = col("c_name",                text_nonempty.opt)
 //      val Instrument: m.ColumnRef   = col("c_instrument", tag.opt)
-      val Status: m.ColumnRef       = col("c_status", obs_status)
-      val ActiveStatus: m.ColumnRef = col("c_active_status", obs_active_status)
-    }
+      val Status: m.ColumnRef       = col("c_status",              obs_status)
+      val ActiveStatus: m.ColumnRef = col("c_active_status",       obs_active_status)
 
-    object ConstraintSetTable extends TableDef("t_constraint_set") {
-      val ObservationId: m.ColumnRef   = col("c_observation_id", observation_id)
-      val CloudExtinction: m.ColumnRef = col("c_cloud_extinction", cloud_extinction)
-      val ImageQuality: m.ColumnRef    = col("c_image_quality", image_quality)
-      val SkyBackground: m.ColumnRef   = col("c_sky_background", sky_background)
-      val WaterVapor: m.ColumnRef      = col("c_water_vapor", water_vapor)
-
-      val AirMassMin: m.ColumnRef      = col("c_air_mass_min", air_mass_range_value)
-      val AirMassMax: m.ColumnRef      = col("c_air_mass_max", air_mass_range_value)
-      val HourAngleMin: m.ColumnRef    = col("c_hour_angle_min", hour_angle_range_value)
-      val HourAngleMax: m.ColumnRef    = col("c_hour_angle_max", hour_angle_range_value)
+      val CloudExtinction: m.ColumnRef = col("c_cloud_extinction", tag)
+      val ImageQuality: m.ColumnRef    = col("c_image_quality",    tag)
+      val SkyBackground: m.ColumnRef   = col("c_sky_background",   tag)
+      val WaterVapor: m.ColumnRef      = col("c_water_vapor",      tag)
+      val AirMassMin: m.ColumnRef      = col("c_air_mass_min",     air_mass_range_value)
+      val AirMassMax: m.ColumnRef      = col("c_air_mass_max",     air_mass_range_value)
+      val HourAngleMin: m.ColumnRef    = col("c_hour_angle_min",   hour_angle_range_value)
+      val HourAngleMax: m.ColumnRef    = col("c_hour_angle_max",   hour_angle_range_value)
     }
 
     // Column references for our mapping.
@@ -126,9 +110,10 @@ object ObservationSnippet {
           env.getR[Existence]("existence"),
           env.getR[ObsStatus]("obsStatus"),
           env.getR[ObsActiveStatus]("obsActiveStatus"),
-        ).parTupled.flatTraverse { case (pid, name, existence, os, as) =>
+          env.getR[ConstraintSet]("constraintSet")
+        ).parTupled.flatTraverse { case (pid, name, existence, os, as, cs) =>
           pool.use { svc =>
-            svc.obs.insertObservation(pid, name, existence, os, as).map {
+            svc.insertObservation(pid, name, existence, os, as, cs).map {
               case NotAuthorized(user) => Result.failure(s"User ${user.id} is not authorized to perform this action")
               case Success(id)         => uniqueObservationNoFiltering(id, child)
             }
@@ -153,17 +138,17 @@ object ObservationSnippet {
             SqlField("name", ObservationTable.Name),
             SqlField("status", ObservationTable.Status),
             SqlField("activeStatus", ObservationTable.ActiveStatus),
+            SqlObject("constraintSet"),
             SqlObject("program", Join(ObservationTable.ProgramId, ProgramTable.Id)),
           ),
         ),
         ObjectMapping(
           tpe = ConstraintSetType,
           fieldMappings = List(
-            SqlField("observationId",   ConstraintSetTable.ObservationId, key = true),
-            SqlField("cloudExtinction", ConstraintSetTable.CloudExtinction),
-            SqlField("imageQuality",    ConstraintSetTable.ImageQuality),
-            SqlField("skyBackground",   ConstraintSetTable.SkyBackground),
-            SqlField("waterVapor",      ConstraintSetTable.WaterVapor),
+            SqlField("cloudExtinction", ObservationTable.CloudExtinction),
+            SqlField("imageQuality",    ObservationTable.ImageQuality),
+            SqlField("skyBackground",   ObservationTable.SkyBackground),
+            SqlField("waterVapor",      ObservationTable.WaterVapor),
             SqlObject("elevationRange")
           )
         ),
@@ -177,15 +162,15 @@ object ObservationSnippet {
         ObjectMapping(
           tpe = AirMassRangeType,
           fieldMappings = List(
-            SqlField("min", ConstraintSetTable.AirMassMin),
-            SqlField("max", ConstraintSetTable.AirMassMax),
+            SqlField("min", ObservationTable.AirMassMin),
+            SqlField("max", ObservationTable.AirMassMax),
           )
         ),
         ObjectMapping(
           tpe = HourAngleRangeType,
           fieldMappings = List(
-            SqlField("minHours", ConstraintSetTable.HourAngleMin),
-            SqlField("maxHours", ConstraintSetTable.HourAngleMax)
+            SqlField("minHours", ObservationTable.HourAngleMin),
+            SqlField("maxHours", ObservationTable.HourAngleMax)
           )
         ),
         ObjectMapping(
--- modules/service/src/main/scala/lucuma/odb/service/ObservationService.scala
+++ modules/service/src/main/scala/lucuma/odb/service/ObservationService.scala
@@ -6,16 +6,15 @@ package lucuma.odb.service
 import cats.effect.MonadCancelThrow
 import cats.syntax.all._
 import eu.timepit.refined.types.string.NonEmptyString
-import lucuma.core.model.GuestRole
-import lucuma.core.model.Observation
-import lucuma.core.model.Program
-import lucuma.core.model.ServiceRole
+import lucuma.core.enums.{CloudExtinction, ImageQuality, SkyBackground, WaterVapor}
+import lucuma.core.model.ElevationRange.{AirMass, HourAngle}
+import lucuma.core.model.{ConstraintSet, ElevationRange, GuestRole, Observation, Program, ServiceRole, User}
 import lucuma.core.model.StandardRole._
-import lucuma.core.model.User
 import lucuma.odb.data.Existence
 import lucuma.odb.data.ObsActiveStatus
 import lucuma.odb.data.ObsStatus
 import lucuma.odb.data.Tag
+import lucuma.odb.graphql.snippet.input.ConstraintSetInput
 import lucuma.odb.util.Codecs._
 import skunk._
 import skunk.implicits._
@@ -24,11 +23,12 @@ trait ObservationService[F[_]] {
   import ObservationService._
 
   def insertObservation(
-    programId:   Program.Id,
-    name:        Option[NonEmptyString] = NonEmptyString.unapply("Untitled Observation"),
-    existence:   Existence              = Existence.Present,
-    status:      ObsStatus              = ObsStatus.New,
-    activeState: ObsActiveStatus        = ObsActiveStatus.Active,
+    programId:     Program.Id,
+    name:          Option[NonEmptyString] = NonEmptyString.unapply("Untitled Observation"),
+    existence:     Existence              = Existence.Present,
+    status:        ObsStatus              = ObsStatus.New,
+    activeState:   ObsActiveStatus        = ObsActiveStatus.Active,
+    constraintSet: ConstraintSet          = ConstraintSetInput.NominalConstraints
   ): F[InsertObservationResponse]
 
 }
@@ -44,40 +44,51 @@ object ObservationService {
   import InsertObservationResponse._
 
   def fromUserAndSession[F[_]: MonadCancelThrow](user: User, session: Session[F]): ObservationService[F] =
-    new ObservationService[F] {
-
-      def insertObservation(
-        programId:   Program.Id,
-        name:        Option[NonEmptyString],
-        existence:   Existence,
-        status:      ObsStatus,
-        activeState: ObsActiveStatus
-      ): F[InsertObservationResponse] = {
-        val af = Statements.insertObservationAs(user, programId, name, existence, status, activeState)
-        session.prepare(af.fragment.query(observation_id)).use { pq =>
-          pq.option(af.argument).map {
-            case Some(oid) => Success(oid)
-            case None      => NotAuthorized(user)
-          }
+    (programId:     Program.Id,
+     name:          Option[NonEmptyString],
+     existence:     Existence,
+     status:        ObsStatus,
+     activeState:   ObsActiveStatus,
+     constraintSet: ConstraintSet
+    ) => {
+      val af = Statements.insertObservationAs(user, programId, name, existence, status, activeState, constraintSet)
+      session.prepare(af.fragment.query(observation_id)).use { pq =>
+        pq.option(af.argument).map {
+          case Some(oid) => Success(oid)
+          case None      => NotAuthorized(user)
         }
       }
-
     }
 
   object Statements {
     import ProgramService.Statements.{ existsUserAsPi, existsUserAsCoi, existsAllocationForPartner }
 
     def insertObservationAs(
-      user: User,
-      programId:   Program.Id,
-      name:        Option[NonEmptyString],
-      existence:   Existence,
-      status:      ObsStatus,
-      activeState: ObsActiveStatus
+      user:          User,
+      programId:     Program.Id,
+      name:          Option[NonEmptyString],
+      existence:     Existence,
+      status:        ObsStatus,
+      activeState:   ObsActiveStatus,
+      constraintSet: ConstraintSet
     ): AppliedFragment = {
 
       val insert: AppliedFragment =
-        InsertObservation.apply(programId ~ name ~ existence ~ status ~ activeState)
+        InsertObservation.apply(
+          programId    ~
+           name        ~
+           existence   ~
+           status      ~
+           activeState ~
+           constraintSet.cloudExtinction ~
+           constraintSet.imageQuality    ~
+           constraintSet.skyBackground   ~
+           constraintSet.waterVapor      ~
+           ElevationRange.airMass.getOption(constraintSet.elevationRange).map(_.min)        ~
+           ElevationRange.airMass.getOption(constraintSet.elevationRange).map(_.max)        ~
+           ElevationRange.hourAngle.getOption(constraintSet.elevationRange).map(_.minHours) ~
+           ElevationRange.hourAngle.getOption(constraintSet.elevationRange).map(_.maxHours)
+        )
 
       val where: AppliedFragment =
         user.role match {
@@ -97,21 +108,51 @@ object ObservationService {
 
     }
 
-    val InsertObservation: Fragment[Program.Id ~ Option[NonEmptyString] ~ Existence ~ ObsStatus ~ ObsActiveStatus] =
+    val InsertObservation: Fragment[
+      Program.Id                    ~
+      Option[NonEmptyString]        ~
+      Existence                     ~
+      ObsStatus                     ~
+      ObsActiveStatus               ~
+      CloudExtinction               ~
+      ImageQuality                  ~
+      SkyBackground                 ~
+      WaterVapor                    ~
+      Option[AirMass.DecimalValue]  ~
+      Option[AirMass.DecimalValue]  ~
+      Option[HourAngle.DecimalHour] ~
+      Option[HourAngle.DecimalHour]
+    ] =
       sql"""
         INSERT INTO t_observation (
           c_program_id,
           c_name,
           c_existence,
           c_status,
-          c_active_status
+          c_active_status,
+          c_cloud_extinction,
+          c_image_quality,
+          c_sky_background,
+          c_water_vapor,
+          c_air_mass_min,
+          c_air_mass_max,
+          c_hour_angle_min,
+          c_hour_angle_max
         )
         SELECT
           $program_id,
           ${text_nonempty.opt},
           $existence,
           $obs_status,
-          $obs_active_status
+          $obs_active_status,
+          $cloud_extinction,
+          $image_quality,
+          $sky_background,
+          $water_vapor,
+          ${air_mass_range_value.opt},
+          ${air_mass_range_value.opt},
+          ${hour_angle_range_value.opt},
+          ${hour_angle_range_value.opt}
       """
 
   }
