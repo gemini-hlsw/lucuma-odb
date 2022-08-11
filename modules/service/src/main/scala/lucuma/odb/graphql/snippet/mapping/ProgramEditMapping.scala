ecd923d08183c0a83d1b8bd45d1a259ce8721ab4
Rob Norris <rob_norris@mac.com>
2022-08-11 13:09:07 -0500

    programEdits

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/snippet/mapping/ProgramEditMapping.scala
@@ -0,0 +1,33 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package snippet
+package mapping
+
+import table.ProgramTable
+
+import edu.gemini.grackle.skunk.SkunkMapping
+import java.util.concurrent.atomic.AtomicLong
+import edu.gemini.grackle.Result
+
+trait ProgramEditMapping[F[_]]
+  extends ProgramTable[F] { this: SkunkMapping[F] =>
+
+  lazy val ProgramEditType = schema.ref("ProgramEdit")
+
+  private val ProgramEditId = AtomicLong(0L)
+
+  lazy val ProgramEditMapping =
+    ObjectMapping(
+      tpe = ProgramEditType,
+      fieldMappings = List(
+        SqlField("synthetic-key", ProgramTable.Id, key = true, hidden = true),
+        // todo: id
+        SqlObject("value"),
+        // todo: editType
+      )
+    )
+
+}
+
