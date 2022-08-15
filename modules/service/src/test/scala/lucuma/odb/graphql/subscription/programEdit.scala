d6eb12d84fee35b97b5d6f49cc92affb86e70768
Rob Norris <rob_norris@mac.com>
2022-08-15 13:38:52 -0500

    tests

--- modules/service/src/test/scala/lucuma/odb/graphql/subscription/programEdit.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/subscription/programEdit.scala
@@ -57,14 +57,13 @@ class programEdit extends OdbSuite {
   test("trigger for my own new programs") {
     import Group1._
     List(pi, guest, service).traverse { user =>
-      subscriptionTest(
+      subscriptionExpect(
         user = user,
         query =
           """
             subscription {
               programEdit {
                 editType
-                id
                 value {
                   name
                 }
@@ -87,7 +86,7 @@ class programEdit extends OdbSuite {
 
   test("trigger for my own new programs (but nobody else's) as guest user") {
     import Group2._
-    subscriptionTest(
+    subscriptionExpect(
       user = guest,
       query =
         """
@@ -115,7 +114,7 @@ class programEdit extends OdbSuite {
 
   test("trigger for my own new programs (but nobody else's) as standard user in PI role") {
     import Group2._
-    subscriptionTest(
+    subscriptionExpect(
       user = pi,
       query =
         """
@@ -143,7 +142,7 @@ class programEdit extends OdbSuite {
 
   test("trigger for all programs as service user") {
     import Group2._
-    subscriptionTest(
+    subscriptionExpect(
       user = service,
       query =
         """
@@ -166,6 +165,74 @@ class programEdit extends OdbSuite {
         List(
           json"""{ "programEdit": { "editType" : "CREATED", "value": { "name": "foo" } } }""",
           json"""{ "programEdit": { "editType" : "CREATED", "value": { "name": "bar" } } }""",
+          json"""{ "programEdit": { "editType" : "CREATED", "value": { "name": "baz" } } }""",
+        )
+    )
+  }
+
+  test("event ids should be distinct") {
+    import Group2._
+    subscription(
+      user = service,
+      query =
+        """
+          subscription {
+            programEdit {
+              id
+            }
+          }
+        """,
+      mutations =
+        Right(
+          createProgram(guest, "foo") >>
+          createProgram(pi, "bar") >>
+          createProgram(service, "baz")
+        ),
+    ).map { js =>
+      val ids = js.map(_.hcursor.downFields("programEdit", "id").require[Long]).distinct
+      assertEquals(ids.length, 3, "Should get three distinct IDs.")
+    }
+  }
+
+  test("edit event should show up") {
+    import Group2._
+    subscriptionExpect(
+      user = service,
+      query =
+        """
+          subscription {
+            programEdit {
+              editType
+              value {
+                name
+              }
+            }
+          }
+        """,
+      mutations =
+        Right(
+          createProgram(guest, "foo").flatMap { id =>
+            IO.sleep(1.second) >> // give time to see the creation before we do an update
+            query(
+              service,
+              s"""
+              mutation {
+                updatePrograms(input: {
+                  WHERE: { id: { EQ: "$id" } }
+                  SET: { name: "foo2" }
+                }) {
+                  id
+                }
+              }
+              """
+            )
+          } >>
+          createProgram(service, "baz").void
+        ),
+      expected =
+        List(
+          json"""{ "programEdit": { "editType" : "CREATED", "value": { "name": "foo" } } }""",
+          json"""{ "programEdit": { "editType" : "UPDATED", "value": { "name": "foo2" } } }""",
           json"""{ "programEdit": { "editType" : "CREATED", "value": { "name": "baz" } } }""",
         )
     )
