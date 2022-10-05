5008053aeef00dd7e3e79a682140ac15a22504fb
Shane Walker <pistes.clary_0w@icloud.com>
2022-10-05 12:55:32 -0300

    annotate

--- modules/service/src/main/scala/lucuma/odb/graphql/table/ObservationView.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/table/ObservationView.scala
@@ -57,14 +57,14 @@ trait ObservationView[F[_]] extends BaseMapping[F] {
         val Mode: ColumnRef = col("c_science_mode", science_mode.embedded)
 
         object Spectroscopy {
-          val Wavelength         = col("c_spec_wavelength",          wavelength_pm.embedded)
-          val Resolution         = col("c_spec_resolution",          pos_int.opt)
-          val SignalToNoise      = col("c_spec_signal_to_noise",     signal_to_noise.opt)
-          val SignalToNoiseAt    = col("c_spec_signal_to_noise_at",  wavelength_pm.embedded)
-          val WavelengthCoverage = col("c_spec_wavelength_coverage", wavelength_pm.embedded)
-          val FocalPlane         = col("c_spec_focal_plane",         focal_plane.opt)
-          val FocalPlaneAngle    = col("c_spec_focal_plane_angle",   angle_µas.embedded)
-          val Capability         = col("c_spec_capability",          spectroscopy_capabilities.opt)
+          val Wavelength: ColumnRef         = col("c_spec_wavelength",          wavelength_pm.embedded)
+          val Resolution: ColumnRef         = col("c_spec_resolution",          pos_int.opt)
+          val SignalToNoise: ColumnRef      = col("c_spec_signal_to_noise",     signal_to_noise.opt)
+          val SignalToNoiseAt: ColumnRef    = col("c_spec_signal_to_noise_at",  wavelength_pm.embedded)
+          val WavelengthCoverage: ColumnRef = col("c_spec_wavelength_coverage", wavelength_pm.embedded)
+          val FocalPlane: ColumnRef         = col("c_spec_focal_plane",         focal_plane.opt)
+          val FocalPlaneAngle: ColumnRef    = col("c_spec_focal_plane_angle",   angle_µas.embedded)
+          val Capability: ColumnRef         = col("c_spec_capability",          spectroscopy_capabilities.opt)
         }
       }
     }
