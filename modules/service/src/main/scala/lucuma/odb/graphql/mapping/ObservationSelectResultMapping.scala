5b60e17a00926d2a51a7d534141c001c8b1be53b
Rob Norris <rob_norris@mac.com>
2022-10-14 13:29:31 -0500

    ObservationSelectResult

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/ObservationSelectResultMapping.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql.mapping
+
+import edu.gemini.grackle.Cursor
+import edu.gemini.grackle.Result
+import lucuma.odb.graphql.BaseMapping
+import skunk.codec.numeric.int8
+
+import scala.tools.util.PathResolver.Environment
+
+trait ObservationSelectResultMapping[F[_]] extends SelectResultMapping[F] {
+
+  lazy val ObservationSelectResultMapping: ObjectMapping =
+    selectResultMapping(ObservationSelectResultType)
+
+}
