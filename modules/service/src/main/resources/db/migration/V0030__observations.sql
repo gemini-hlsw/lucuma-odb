8c6319a6ac359bb9fb7fb321bcf2e584f88346c5
Shane Walker <pistes.clary_0w@icloud.com>
2022-10-25 12:32:35 -0300

    test cases

--- modules/service/src/main/resources/db/migration/V0030__observations.sql
+++ modules/service/src/main/resources/db/migration/V0030__observations.sql
@@ -219,7 +219,8 @@ comment on table t_observation is 'Observations.';
 
 create view v_observation as
   select *,
-  case when c_explicit_ra    is not null then c_observation_id end as c_explicit_base_id,
-  case when c_air_mass_min   is not null then c_observation_id end as c_air_mass_id,
-  case when c_hour_angle_min is not null then c_observation_id end as c_hour_angle_id
+  case when c_explicit_ra         is not null then c_observation_id end as c_explicit_base_id,
+  case when c_air_mass_min        is not null then c_observation_id end as c_air_mass_id,
+  case when c_hour_angle_min      is not null then c_observation_id end as c_hour_angle_id,
+  case when c_observing_mode_type is not null then c_observation_id end as c_observing_mode_id
   from t_observation
