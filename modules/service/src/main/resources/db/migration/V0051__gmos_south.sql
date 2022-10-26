497a053343a4043238a609843619eede8fbffde9
Shane Walker <pistes.clary_0w@icloud.com>
2022-10-26 18:08:58 -0300

    table and input

--- modules/service/src/main/resources/db/migration/V0051__gmos_south.sql
+++ modules/service/src/main/resources/db/migration/V0051__gmos_south.sql
@@ -121,3 +121,38 @@ insert into t_gmos_south_fpu values ('IfuRed',        'IFU-R', '   IFU Right Sli
 insert into t_gmos_south_fpu values ('IfuNS2Slits',   'IFU-NS-2', 'IFU N and S 2 Slits',           310000, -31750000);
 insert into t_gmos_south_fpu values ('IfuNSBlue',     'IFU-NS-B', 'IFU N and S Left Slit (blue)',  310000, -30875000);
 insert into t_gmos_south_fpu values ('IfuNSRed',      'IFU-NS-R', 'IFU N and S Right Slit (red)',  310000, -32625000);
+
+--- GMOS SOUTH LONG SLIT OBSERVING MODE
+
+create table t_gmos_south_long_slit (
+
+  c_observation_id             d_observation_id      NOT NULL,
+  c_observing_mode_type        e_observing_mode_type NOT NULL DEFAULT 'gmos_south_long_slit' check (c_observing_mode_type = 'gmos_south_long_slit'),
+
+  c_grating                    d_tag                 NOT NULL             REFERENCES t_gmos_south_disperser(c_tag),
+  c_filter                     d_tag                 NULL DEFAULT NULL    REFERENCES t_gmos_south_filter(c_tag),
+  c_fpu                        d_tag                 NOT NULL             REFERENCES t_gmos_south_fpu(c_tag),
+  c_central_wavelength         d_wavelength_pm       NOT NULL,
+
+  c_xbin                       d_tag                 NULL DEFAULT NULL   REFERENCES t_gmos_binning(c_tag),
+  c_ybin                       d_tag                 NULL DEFAULT NULL   REFERENCES t_gmos_binning(c_tag),
+  c_amp_read_mode              d_tag                 NULL DEFAULT NULL   REFERENCES t_gmos_amp_read_mode(c_tag),
+  c_amp_gain                   d_tag                 NULL DEFAULT NULL   REFERENCES t_gmos_amp_gain(c_tag),
+  c_roi                        d_tag                 NULL DEFAULT NULL   REFERENCES t_gmos_roi(c_tag),
+
+  -- stuff wavelength dithers and offsets into a string until grackle supports array columns?
+  c_wavelength_dithers         text                  NULL DEFAULT NULL,
+  c_spatial_offsets            text                  NULL DEFAULT NULL,
+
+  -- hold on to the initial grating, filter, fpu and central wavelength regardless of subsequent changes
+  c_initial_grating            d_tag                 NOT NULL             REFERENCES t_gmos_south_disperser(c_tag),
+  c_initial_filter             d_tag                 NULL DEFAULT NULL    REFERENCES t_gmos_south_filter(c_tag),
+  c_initial_fpu                d_tag                 NOT NULL             REFERENCES t_gmos_south_fpu(c_tag),
+  c_initial_central_wavelength d_wavelength_pm       NOT NULL,
+
+  CONSTRAINT wavelength_dither_format CHECK (c_wavelength_dithers ~ '^-?\d+(\.\d+)?(,-?\d+(\.\d+)?)*$'),
+  CONSTRAINT offset_format            CHECK (c_spatial_offsets ~ '^-?\d+(\.\d+)?(,-?\d+(\.\d+)?)*$'),
+
+  PRIMARY KEY (c_observation_id, c_observing_mode_type),
+  FOREIGN KEY (c_observation_id, c_observing_mode_type) REFERENCES t_observation(c_observation_id, c_observing_mode_type) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED
+);
