3de34738958dccc4823071e9480ccc8b8a33d8df
Shane Walker <pistes.clary_0w@icloud.com>
2022-12-05 09:03:58 -0300

    codec tests

---
+++ modules/service/src/test/scala/lucuma/odb/graphql/instances/SourceProfileCodecSuite.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql.instances
+
+import io.circe.testing.ArbitraryInstances
+import io.circe.testing.CodecTests
+import lucuma.core.model.SourceProfile
+import lucuma.core.model.arb.ArbSourceProfile._
+import munit.DisciplineSuite
+
+class SourceProfileCodecSuite extends DisciplineSuite with ArbitraryInstances {
+
+  import SourceProfileCodec.given
+
+  checkAll("SourceProfileCodec", CodecTests[SourceProfile].codec)
+
+}
