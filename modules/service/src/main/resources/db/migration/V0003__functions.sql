d114a55cc6f92a2f1fa918daf1c00be4feeaa362
Rob Norris <rob_norris@mac.com>
2023-01-03 16:59:56 -0600

    fix function definition

--- modules/service/src/main/resources/db/migration/V0003__functions.sql
+++ modules/service/src/main/resources/db/migration/V0003__functions.sql
@@ -1,3 +1,12 @@
 -- https://stackoverflow.com/questions/54372666/create-an-immutable-clone-of-concat-ws/54384767#54384767
-CREATE OR REPLACE FUNCTION immutable_concat_ws(text, VARIADIC text[])
-RETURNS text AS 'text_concat_ws' LANGUAGE internal IMMUTABLE PARALLEL SAFE;
+-- CREATE OR REPLACE FUNCTION immutable_concat_ws(text, VARIADIC text[])
+-- RETURNS text AS 'text_concat_ws' LANGUAGE internal IMMUTABLE PARALLEL SAFE;
+
+-- We don't have permission to do the above hack on Heroku. But this works.
+CREATE OR REPLACE FUNCTION immutable_concat_ws(delim text, parts VARIADIC text[])
+RETURNS text AS $$
+DECLARE
+BEGIN
+  RETURN array_to_string(parts, delim);
+END;
+$$ LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE;
