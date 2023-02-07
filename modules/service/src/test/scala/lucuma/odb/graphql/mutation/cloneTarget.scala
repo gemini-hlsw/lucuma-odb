e60a900a8cd512cc6e3530c0d67e72b7cc5adbf7
Rob Norris <rob_norris@mac.com>
2023-02-07 15:33:36 -0600

    hack hack hack

--- modules/service/src/test/scala/lucuma/odb/graphql/mutation/cloneTarget.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/mutation/cloneTarget.scala
@@ -7,6 +7,9 @@ package mutation
 import io.circe.Json
 import io.circe.literal.*
 import lucuma.core.model.Target
+import cats.effect.IO
+import lucuma.core.model.Observation
+import io.circe.syntax.*
 
 class cloneTarget extends OdbSuite {
   import createTarget.FullTargetGraph
@@ -158,6 +161,60 @@ class cloneTarget extends OdbSuite {
           expected = Left(List(s"No such target: $tid"))
         )
       }
+    }
+  }
+
+  test("clone and replace in an observation") {
+
+    def cloneTarget(tid: Target.Id, oids: List[Observation.Id]): IO[Target.Id] =
+      query(pi, s"""
+        mutation { 
+          cloneTarget(input: { 
+            targetId: "$tid"
+            REPLACE_IN: ${oids.asJson}
+          }) { 
+            newTarget { id }
+          } 
+        }
+      """).map(_.hcursor.downFields("cloneTarget", "newTarget", "id").require[Target.Id])
+
+    def asterism(oid: Observation.Id): IO[List[Target.Id]] =
+      query(pi, s"""
+        query { 
+          observation(observationId: "$oid") {
+            targetEnvironment {
+              asterism {
+                id
+              }
+            }
+          }
+        }
+      """).map {
+        _.hcursor.downFields("observation", "targetEnvironment", "asterism").require[List[Json]]
+         .map(_.hcursor.downField("id").require[Target.Id])
+      }
+
+    for {
+      // one program
+      pid  <- createProgramAs(pi)
+      // several targets
+      tid1 <- createTargetAs(pi, pid, "Target 1")
+      tid2 <- createTargetAs(pi, pid, "Target 2")
+      tid3 <- createTargetAs(pi, pid, "Target 3")
+      // several obs with both targets
+      oid1 <- createObservationAs(pi, pid, tid1, tid3) // 1, 3
+      oid2 <- createObservationAs(pi, pid, tid1, tid3) // 1, 3
+      oid3 <- createObservationAs(pi, pid, tid2, tid3) // 2, 3
+      // clone a target and replace it in some obs
+      tid4 <- cloneTarget(tid3, List(oid2, oid3))
+      // fetch our asterisms
+      ast1 <- asterism(oid1)
+      ast2 <- asterism(oid2)
+      ast3 <- asterism(oid3)
+    } yield {
+      assertEquals(ast1, List(tid1, tid3)) // unchanged
+      assertEquals(ast2, List(tid1, tid4)) // 1, 3 -> 4
+      assertEquals(ast3, List(tid2, tid4)) // 2, 3 -> 4
     }
   }
 
