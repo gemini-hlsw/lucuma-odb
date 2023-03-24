1766eb3b608b78ff2ed1eb0a84fc6371b1c57758
Shane Walker <pistes.clary_0w@icloud.com>
2023-03-24 16:51:33 -0300

    SmartGcalService

--- modules/service/src/main/scala/lucuma/odb/service/SmartGcalService.scala
+++ modules/service/src/main/scala/lucuma/odb/service/SmartGcalService.scala
@@ -5,6 +5,7 @@ package lucuma.odb.service
 
 import cats.effect.Sync
 import cats.syntax.foldable.*
+import cats.syntax.functor.*
 import lucuma.core.enums.SmartGcalType
 import lucuma.core.model.sequence.GmosFpuMask
 import lucuma.core.model.sequence.DynamicConfig.GmosNorth
@@ -17,7 +18,7 @@ import skunk.implicits.*
 trait SmartGcalService[F[_]] {
 
   def selectGmosNorth(gn: GmosNorth, sgt: SmartGcalType): F[List[(GmosNorth, Gcal)]]
-   // placeholder
+
 }
 
 object SmartGcalService {
@@ -27,35 +28,54 @@ object SmartGcalService {
   ): SmartGcalService[F] =
 
     new SmartGcalService[F] {
-      def selectGmosNorth(gn: GmosNorth, sgt: SmartGcalType): F[List[(GmosNorth, Gcal)]] =
-        ???
+      override def selectGmosNorth(
+        gn: GmosNorth,
+        sgt: SmartGcalType
+      ): F[List[(GmosNorth, Gcal)]] = {
+
+        val af = Statements.selectGmosNorth(gn, sgt)
+        session
+          .prepareR(af.fragment.query(step_config_gcal ~ pos_int ~ time_span))
+          .use(_.stream(af.argument, chunkSize = 16).compile.to(List))
+          .map {
+            _.flatMap { case ((gcal, count), exposureTime) =>
+              List.fill(count.value)(
+                GmosNorth.exposure.replace(exposureTime)(gn) -> gcal
+              )
+            }
+          }
+      }
 
     }
 
 
   object Statements {
 
-    def select(
+    def selectGmosNorth(
       gn:  GmosNorth,
       sgt: SmartGcalType
     ): AppliedFragment =
 
       val where = List(
-        gn.gratingConfig.map(_.grating).fold(void"s.c_disperser IS NULL")(sql"s.c_disperser = ${gmos_north_grating}"),
-        gn.filter.fold(void"s.c_filter IS NULL")(sql"s.c_filter = ${gmos_north_filter}"),
-        gn.fpu.flatMap(GmosFpuMask.builtin.getOption).fold(void"s.c_fpu IS NULL")(sql"s.c_fpu = ${gmos_north_fpu}"),
+        sql"s.c_disperser       IS NOT DISTINCT FROM ${gmos_north_grating.opt}"(gn.gratingConfig.map(_.grating)),
+        sql"s.c_filter          IS NOT DISTINCT FROM ${gmos_north_filter.opt}"(gn.filter),
+        sql"s.c_fpu             IS NOT DISTNICT FROM ${gmos_north_fpu.opt}"(gn.fpu.flatMap(GmosFpuMask.builtin.getOption)),
+        sql"s.c_disperser_order IS NOT DISTINCT FROM ${gmos_disperser_order.opt}"(gn.gratingConfig.map(_.order)),
         sql"s.c_x_binning = ${gmos_x_binning}"(gn.readout.xBin),
         sql"s.c_y_binning = ${gmos_y_binning}"(gn.readout.yBin),
         sql"s.c_amp_gain  = ${gmos_amp_gain}"(gn.readout.ampGain),
+        gn.gratingConfig.map(_.wavelength).fold(void"s.c_wavelength_range IS NULL")(
+          sql"s.c_wavelength_range @> ${wavelength_pm}"
+        ),
         sgt match {
-          case SmartGcalType.Arc           => void"AND g.gcal_lamp_type = 'Arc'"
-          case SmartGcalType.Flat          => void"AND g.gcal_lamp_type = 'Flat'"
-          case SmartGcalType.DayBaseline   => void"AND g.gcal_baseline  = 'Day'"
-          case SmartGcalType.NightBaseline => void"AND g.gcal_baseline  = 'Night'"
+          case SmartGcalType.Arc           => void"g.gcal_lamp_type = 'Arc'"
+          case SmartGcalType.Flat          => void"g.gcal_lamp_type = 'Flat'"
+          case SmartGcalType.DayBaseline   => void"g.gcal_baseline  = 'Day'"
+          case SmartGcalType.NightBaseline => void"g.gcal_baseline  = 'Night'"
         }
       ).foldMap(void" AND " |+| _)
 
-      sql"""
+      void"""
         SELECT g.c_gcal_continuum
                g.c_gcal_ar_arc
                g.c_gcal_cuar_arc
@@ -69,25 +89,7 @@ object SmartGcalService {
           FROM t_smart_gmos_north s
           JOIN t_gcal             g ON s.c_instrument = g.c_instrument
                                    AND s.c_gcal_id    = g.c_gcal_id
-         WHERE s.c_disperser IS NOT DISTINCT FROM ${gmos_north_grating.opt}
-           AND s.c_filter    IS NOT DISTINCT FROM ${gmos_north_filter.opt}
-           AND s.c_fpu       IS NOT DISTNICT FROM ${gmos_north_fpu.opt}
-           AND s.c_x_binning = $gmos_x_binning
-           AND s.c_y_binning = $gmos_y_binning
-           AND s.c_amp_gain  = $gmos_amp_gain
-      ORDER BY s.c_step_order
-      """.apply(
-        gn.gratingConfig.map(_.grating)               ~
-        gn.filter                                     ~
-        gn.fpu.flatMap(GmosFpuMask.builtin.getOption) ~
-        gn.readout.xBin                               ~
-        gn.readout.yBin                               ~
-        gn.readout.ampGain
-      )
-
-//           AND s.c_wavelength_range @> ${wavelength_pm}
-//           AND s.c_disperser_order   = ${gmos_disperser_order}
-
+         WHERE """ |+| where |+| void" ORDER BY s.c_step_order"
 
   }
 
