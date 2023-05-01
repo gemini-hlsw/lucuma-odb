a23cda2b87fcf2664f1ee302cbed9de064be4f6f
Rob Norris <rob_norris@mac.com>
2023-05-01 15:55:12 -0500

    group topic

--- modules/service/src/main/scala/lucuma/odb/graphql/BaseMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/BaseMapping.scala
@@ -63,6 +63,7 @@ trait BaseMapping[F[_]]
   lazy val GmosYBinningType                    = schema.ref("GmosYBinning")
   lazy val GroupType                           = schema.ref("Group")
   lazy val GroupIdType                         = schema.ref("GroupId")
+  lazy val GroupEditType                       = schema.ref("GroupEdit")
   lazy val GroupElementType                    = schema.ref("GroupElement")
   lazy val HmsStringType                       = schema.ref("HmsString")
   lazy val HourAngleRangeType                  = schema.ref("HourAngleRange")
---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/GroupEditInput.scala
@@ -0,0 +1,27 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package input
+
+import cats.syntax.apply.*
+import lucuma.core.model.Program
+import lucuma.odb.data.Group
+import lucuma.odb.graphql.binding.*
+
+final case class GroupEditInput(
+  groupId:   Option[Group.Id],
+  programId: Option[Program.Id]
+)
+
+object GroupEditInput {
+
+  val Binding = ObjectFieldsBinding.rmap {
+    case List(
+      GroupIdBinding.Option("groupId", rGroupId),
+      ProgramIdBinding.Option("programId", rProgramId)
+    ) =>
+      (rGroupId, rProgramId).mapN(apply)
+  }
+
+}
