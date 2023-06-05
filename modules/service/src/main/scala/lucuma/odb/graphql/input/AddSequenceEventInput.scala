037c5d73b41df89ff3c7541332656f7497a3733c
Shane Walker <pistes.clary_0w@icloud.com>
2023-06-05 08:48:30 -0400

    AddSequenceEventInput

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/AddSequenceEventInput.scala
@@ -0,0 +1,30 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package input
+
+import cats.syntax.parallel.*
+import lucuma.core.enums.SequenceCommand
+import lucuma.core.model.Visit
+import lucuma.odb.graphql.binding.*
+
+case class AddSequenceEventInput(
+  visitId: Visit.Id,
+  command: SequenceCommand
+)
+
+object AddSequenceEventInput {
+
+  val Binding: Matcher[AddSequenceEventInput] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        VisitIdBinding("visitId", rVisitId),
+        SequenceCommandBinding("command", rCommand)
+      ) =>
+        (rVisitId, rCommand).parMapN { (vid, cmd) =>
+          AddSequenceEventInput(vid, cmd)
+        }
+    }
+
+}
