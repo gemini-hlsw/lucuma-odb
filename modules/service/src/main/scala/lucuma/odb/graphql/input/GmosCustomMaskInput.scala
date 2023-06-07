91367105034b05db64caee9ee481adba718d951e
Shane Walker <pistes.clary_0w@icloud.com>
2023-06-07 16:09:26 -0400

    record step bindings

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/GmosCustomMaskInput.scala
@@ -0,0 +1,29 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package input
+
+import cats.syntax.parallel.*
+import edu.gemini.grackle.Result
+import eu.timepit.refined.types.string.NonEmptyString
+import lucuma.core.enums.GmosCustomSlitWidth
+import lucuma.core.model.sequence.gmos.GmosFpuMask.Custom
+import lucuma.odb.graphql.binding.*
+
+object GmosCustomMaskInput {
+
+  val Binding: Matcher[Custom] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        StringBinding("filename", rFilename),
+        GmosCustomSlitWidthBinding("slitWidth", rSlitWidth)
+      ) => (rFilename, rSlitWidth).parTupled.flatMap { (filename, slitWidth) =>
+        NonEmptyString.from(filename) match {
+          case Left(_)  => Result.failure("The GMOS custom FPU mask 'filename' cannot be empty.")
+          case Right(n) => Result(Custom(n, slitWidth))
+        }
+      }
+    }
+
+}
---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/StepConfigInput.scala
@@ -0,0 +1,33 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package input
+
+import cats.syntax.all.*
+import edu.gemini.grackle.Result
+import lucuma.core.model.sequence.StepConfig
+import lucuma.odb.graphql.binding.*
+
+object StepConfigInput {
+
+  val Binding: Matcher[StepConfig] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        BooleanBinding.Option("bias", rBias),
+        BooleanBinding.Option("dark", rDark),
+        StepConfigGcalInput.Binding.Option("gcal", rGcal),
+        StepConfigScienceInput.Binding.Option("science", rScience)
+      ) => (rBias, rDark, rGcal, rScience).parTupled.flatMap {
+         case (bias, dark, gcal, science) =>
+           oneOrFail(
+             bias.ifM(StepConfig.Bias.some, none) -> "bias",
+             dark.ifM(StepConfig.Dark.some, none) -> "dark",
+             gcal                                 -> "gcal",
+             science                              -> "science"
+           )
+
+      }
+    }
+
+}
