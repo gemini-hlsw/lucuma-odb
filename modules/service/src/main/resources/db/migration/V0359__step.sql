4ae23ccac56bfd54e20f2cda51971ba684ca8c0f
Shane Walker <pistes.clary_0w@icloud.com>
2023-06-08 13:54:13 -0400

    migration

--- modules/service/src/main/resources/db/migration/V0359__step.sql
+++ modules/service/src/main/resources/db/migration/V0359__step.sql
@@ -33,31 +33,13 @@ CREATE TABLE t_step (
 
 );
 
-CREATE TABLE t_step_config_science (
-  c_step_id     d_step_id     PRIMARY KEY,
-
-  c_step_type   e_step_type   NOT NULL DEFAULT ('science'),
-  CHECK (c_step_type = 'science'),
-
-  FOREIGN KEY (c_step_id, c_step_type) REFERENCES t_step (c_step_id, c_step_type),
-
-  c_offset_p    d_angle_µas   NOT NULL DEFAULT (0),
-  c_offset_q    d_angle_µas   NOT NULL DEFAULT (0),
-  c_guide_state e_guide_state NOT NULL DEFAULT ('enabled')
-
-);
-
 CREATE TABLE t_step_config_gcal (
   c_step_id       d_step_id        PRIMARY KEY,
 
-  c_step_type     e_step_type      NOT NULL DEFAULT ('smart_gcal'),
-  CHECK (c_step_type = 'smart_gcal'),
+  c_step_type     e_step_type      NOT NULL DEFAULT ('gcal'),
+  CHECK (c_step_type = 'gcal'),
 
   FOREIGN KEY (c_step_id, c_step_type) REFERENCES t_step (c_step_id, c_step_type),
-
-  c_offset_p      d_angle_µas     NOT NULL DEFAULT (0),
-  c_offset_q      d_angle_µas     NOT NULL DEFAULT (0),
-  c_guiding_state e_guiding_state NOT NULL DEFAULT ('enabled'),
 
   -- Gcal Configuration Value
   c_gcal_continuum   d_tag        REFERENCES t_gcal_continuum(c_tag) ON DELETE CASCADE,
@@ -73,4 +55,36 @@ CREATE TABLE t_step_config_gcal (
   c_gcal_diffuser    d_tag        NOT NULL REFERENCES t_gcal_diffuser(c_tag) ON DELETE CASCADE,
   c_gcal_shutter     d_tag        NOT NULL REFERENCES t_gcal_shutter(c_tag)  ON DELETE CASCADE
 
+);
+
+CREATE TABLE t_step_config_science (
+  c_step_id     d_step_id     PRIMARY KEY,
+
+  c_step_type   e_step_type   NOT NULL DEFAULT ('science'),
+  CHECK (c_step_type = 'science'),
+
+  FOREIGN KEY (c_step_id, c_step_type) REFERENCES t_step (c_step_id, c_step_type),
+
+  c_offset_p    d_angle_µas   NOT NULL DEFAULT (0),
+  c_offset_q    d_angle_µas   NOT NULL DEFAULT (0),
+  c_guide_state e_guide_state NOT NULL DEFAULT ('enabled')
+
+);
+
+CREATE TYPE e_smart_type AS ENUM (
+  'arc',
+  'flat',
+  'day_baseline',
+  'night_baseline'
+);
+
+CREATE TABLE t_step_config_smart_gcal (
+  c_step_id     d_step_id   PRIMARY KEY,
+
+  c_step_type   e_step_type NOT NULL DEFAULT ('smart_gcal'),
+  CHECK (c_step_type = 'smart_gcal'),
+
+  FOREIGN KEY (c_step_id, c_step_type) REFERENCES t_step (c_step_id, c_step_type),
+
+  c_smart_type e_smart_type NOT NULL
 );
