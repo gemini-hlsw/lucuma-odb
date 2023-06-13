12b369d74d10ece8004de7c0050d96d300617fa2
Rob Norris <rob_norris@mac.com>
2023-06-13 12:08:23 -0500

    program updates tests

---
+++ modules/service/src/test/scala/lucuma/odb/graphql/ChronicleOperations.scala
@@ -0,0 +1,31 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+
+import cats.effect.IO
+import io.circe.Json
+import lucuma.core.model.Program
+import lucuma.odb.util.Codecs.*
+import skunk.circe.codec.json.jsonb
+import skunk.syntax.all.*
+
+trait ChronicleOperations { this: OdbSuite =>
+
+  /** 
+   * Return all Chronicle updates for `pid`, order by chronicle ID. The timestamp and transaction
+   * id are removed to allow for easier asserting.
+   */
+  def chronProgramUpdates(pid: Program.Id): IO[List[Json]] =
+    withSession { s =>
+      s.prepareR(
+        sql"""
+          SELECT to_jsonb(row_to_json(t)) - ARRAY['c_timestamp', 'c_transaction_id', 'c_chron_id'] 
+          FROM t_chron_program_update t 
+          WHERE t.c_program_id = $program_id 
+          ORDER BY c_chron_id ASC
+        """.query(jsonb)
+      ).use(_.stream(pid, 1024).compile.toList)
+    }
+
+}
