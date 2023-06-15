411478554e967172d2ebf9c41401b24f5e9bf3d9
Shane Walker <pistes.clary_0w@icloud.com>
2023-06-15 11:48:17 -0400

    StepConfig mappings

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/OffsetMapping.scala
@@ -0,0 +1,69 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package mapping
+
+import edu.gemini.grackle.TypeRef
+import lucuma.core.math.Angle
+import lucuma.core.math.Offset
+
+import table.StepTable
+
+trait OffsetMapping[F[_]] extends StepTable[F] {
+
+  private def offsetComponentMapping(
+    typeRef:     TypeRef,
+    idColumn:    ColumnRef,
+    valueColumn: ColumnRef
+  ): ObjectMapping =
+    ObjectMapping(
+      tpe = typeRef,
+      fieldMappings = List(
+        SqlField("synthetic_id", idColumn, key = true, hidden = true),
+        SqlField("value", valueColumn, hidden = true),
+        FieldRef[Angle]("value").as("microarcseconds", _.toMicroarcseconds),
+        FieldRef[Angle]("value").as("milliarcseconds", a => BigDecimal(a.toMicroarcseconds) /     1_000L),
+        FieldRef[Angle]("value").as("arcseconds",      a => BigDecimal(a.toMicroarcseconds) / 1_000_000L)
+      )
+    )
+
+  lazy val OffsetPMapping: TypeMapping =
+    SwitchMapping(
+      pType,
+      List(
+        GmosNorthStepRecordType / "stepConfig" / "offset" / "p" -> offsetComponentMapping(pType, StepTable.Id, StepTable.Science.OffsetP),
+        GmosSouthStepRecordType / "stepConfig" / "offset" / "p" -> offsetComponentMapping(pType, StepTable.Id, StepTable.Science.OffsetP)
+      )
+    )
+
+  lazy val OffsetQMapping: TypeMapping =
+    SwitchMapping(
+      qType,
+      List(
+        GmosNorthStepRecordType / "stepConfig" / "offset" / "q" -> offsetComponentMapping(qType, StepTable.Id, StepTable.Science.OffsetQ),
+        GmosSouthStepRecordType / "stepConfig" / "offset" / "q" -> offsetComponentMapping(qType, StepTable.Id, StepTable.Science.OffsetQ)
+      )
+    )
+
+  private def offsetMapping(
+    idColumn: ColumnRef
+  ): ObjectMapping =
+    ObjectMapping(
+      tpe = OffsetType,
+      fieldMappings = List(
+        SqlField("id", idColumn, key = true, hidden = true),
+        SqlObject("p"),
+        SqlObject("q")
+      )
+    )
+
+  lazy val OffsetMapping: TypeMapping =
+    SwitchMapping(
+      OffsetType,
+      List(
+        GmosNorthStepRecordType / "stepConfig" / "offset" -> offsetMapping(StepTable.Id),
+        GmosSouthStepRecordType / "stepConfig" / "offset" -> offsetMapping(StepTable.Id)
+      )
+    )
+}
