d69a24b10db91626a1f237c5743e61bdb2f70faf
Rob Norris <rob_norris@mac.com>
2023-06-21 16:08:42 -0500

    inputs

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/ConditionsIntuitionInput.scala
@@ -0,0 +1,41 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+
+package input
+
+import cats.data.NonEmptyList
+import cats.syntax.all.*
+import edu.gemini.grackle.Path
+import edu.gemini.grackle.Predicate
+import eu.timepit.refined.types.numeric.NonNegInt
+import lucuma.core.model.Observation
+import lucuma.core.model.Program
+import lucuma.core.model.Target
+import lucuma.odb.data.Nullable
+import lucuma.odb.graphql.binding._
+import lucuma.odb.data.Tag
+import cats.data.Ior
+import edu.gemini.grackle.Result
+
+final case class ConditionsIntuitionInput(
+   value: Ior[ConditionsExpectationInput, Tag]
+)
+
+object ConditionsIntuitionInput:
+
+   val Binding: Matcher[ConditionsIntuitionInput] =
+      ObjectFieldsBinding.rmap {
+         case List(
+            ConditionsExpectationInput.Binding.Option("expectation", rExpectation),
+            TagBinding.Option("seeingTrend", rSeeingTrend)
+         ) =>
+            (rExpectation, rSeeingTrend).parTupled.flatMap { case (o1, o2) =>
+               Result.fromOption(
+                  Ior.fromOptions(o1, o2).map(ConditionsIntuitionInput.apply),
+                  "At least one of expectation and seeingTrend must be specified."
+               )
+            }
+      }
+
---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/ConditionsMeasurementInput.scala
@@ -0,0 +1,55 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+
+package input
+
+import cats.data.NonEmptyList
+import cats.syntax.all.*
+import edu.gemini.grackle.Path
+import edu.gemini.grackle.Predicate
+import eu.timepit.refined.types.numeric.NonNegInt
+import lucuma.core.model.Observation
+import lucuma.core.model.Program
+import lucuma.core.model.Target
+import lucuma.odb.data.Nullable
+import lucuma.odb.graphql.binding._
+import lucuma.core.math.Angle
+import lucuma.core.math.Wavelength
+import lucuma.odb.data.Tag
+import edu.gemini.grackle.Result
+import lucuma.odb.data.Extinction
+
+final case class ConditionsMeasurementInput(
+  source: Tag,
+  seeing: Option[Angle],
+  extinction: Option[Extinction],
+  wavelength: Option[Wavelength],
+  azimuth: Option[Angle],
+  elevation: Option[Angle]
+)
+
+object ConditionsMeasurementInput {
+
+  val Binding: Matcher[ConditionsMeasurementInput] =
+    ObjectFieldsBinding.rmap {
+      case List(
+        TagBinding("source", rSource),
+        AngleInput.Binding.Option("seeing", rSeeing),
+        ExtinctionBinding.Option("extinction", rExtinction),
+        WavelengthInput.Binding.Option("wavelength", rWavelength),
+        AngleInput.Binding.Option("azimuth", rAzimuth),
+        AngleInput.Binding.Option("elevation", rElevation),
+      ) =>
+        (rSource, rSeeing, rExtinction, rWavelength, rAzimuth, rElevation).parTupled.flatMap { (source, seeing, extinction, wavelength, azimuth, elevation) =>
+          if List(seeing, extinction, wavelength, azimuth, elevation).exists(_.isDefined) then
+            Result(ConditionsMeasurementInput(source, seeing, extinction, wavelength, azimuth, elevation))
+          else
+            Result.failure("At least one of seeing, wavelength, extinction, azimuth, and elevation must be defined.")                  
+        }
+    }
+
+}
+
+
