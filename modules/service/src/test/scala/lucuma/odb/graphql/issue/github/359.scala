fbd0f75ba3e44967ac8200ebc969b86499a55de9
Rob Norris <rob_norris@mac.com>
2023-07-11 13:12:22 -0500

    fix 359

---
+++ modules/service/src/test/scala/lucuma/odb/graphql/issue/github/359.scala
@@ -0,0 +1,106 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package issue.github
+
+import io.circe.syntax.*
+
+// https://github.com/gemini-hlsw/lucuma-odb/issues/359
+class GitHub_359 extends OdbSuite {
+  val pi = TestUsers.Standard.pi(nextId, nextId)
+  val validUsers = List(pi)
+
+  test("selecting originalObservation's id and asterism (after clone) should work") {
+    createProgramAs(pi).flatMap { pid =>
+      createObservationAs(pi, pid).flatMap { oid =>
+        query(
+          user = pi,
+          query = s"""
+            mutation {
+              cloneObservation(
+                input: {
+                  observationId: ${oid.asJson}
+                }
+              ) {
+                originalObservation {
+                  id
+                  targetEnvironment {
+                    asterism {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          """
+        )
+      }
+    }
+  }
+
+  test("selecting both observations' id and asterism (after clone) should work") {
+    createProgramAs(pi).flatMap { pid =>
+      createObservationAs(pi, pid).flatMap { oid =>
+        query(
+          user = pi,
+          query = s"""
+            mutation {
+              cloneObservation(
+                input: {
+                  observationId: ${oid.asJson}
+                }
+              ) {
+                originalObservation {
+                  id
+                  targetEnvironment {
+                    asterism {
+                      id
+                    }
+                  }
+                }
+                newObservation {
+                  id
+                  targetEnvironment {
+                    asterism {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          """
+        )
+      }
+    }
+  }
+
+  test("selecting newObservation's id and asterism (after clone) should work") {
+    createProgramAs(pi).flatMap { pid =>
+      createObservationAs(pi, pid).flatMap { oid =>
+        query(
+          user = pi,
+          query = s"""
+            mutation {
+              cloneObservation(
+                input: {
+                  observationId: ${oid.asJson}
+                }
+              ) {
+                newObservation {
+                  id
+                  targetEnvironment {
+                    asterism {
+                      id
+                    }
+                  }
+                }
+              }
+            }
+          """
+        )
+      }
+    }
+  }
+
+}
