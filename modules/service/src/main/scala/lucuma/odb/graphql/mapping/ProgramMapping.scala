b48e621c80458ce30561bd7cb127c876f755fad9
Shane Walker <pistes.clary_0w@icloud.com>
2023-07-31 16:29:09 -0400

    program plannedTime effect handler

--- modules/service/src/main/scala/lucuma/odb/graphql/mapping/ProgramMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/ProgramMapping.scala
@@ -5,22 +5,39 @@ package lucuma.odb.graphql
 
 package mapping
 
+import cats.Order
+import cats.Order.catsKernelOrderingForOrder
 import cats.syntax.all._
+import cats.effect.Resource
+import edu.gemini.grackle.Cursor
 import edu.gemini.grackle.Predicate
 import edu.gemini.grackle.Predicate._
 import edu.gemini.grackle.Query
 import edu.gemini.grackle.Query._
 import edu.gemini.grackle.Result
+import edu.gemini.grackle.ResultT
 import edu.gemini.grackle.TypeRef
 import edu.gemini.grackle.skunk.SkunkMapping
 import eu.timepit.refined.types.numeric.NonNegShort
+import io.circe.Json
+import io.circe.syntax.*
 import lucuma.core.model.Group
 import lucuma.core.model.ObsAttachment
 import lucuma.core.model.Observation
+import lucuma.core.model.Program
 import lucuma.core.model.User
+import lucuma.core.model.sequence.PlannedTime
+import lucuma.core.model.sequence.PlannedTimeRange
+import lucuma.itc.client.ItcClient
+import lucuma.odb.data.GroupTree
 import lucuma.odb.data.Tag
 import lucuma.odb.graphql.predicate.Predicates
 import lucuma.odb.graphql.table.GroupElementView
+import lucuma.odb.logic.PlannedTimeCalculator
+import lucuma.odb.sequence.util.CommitHash
+import lucuma.odb.service.Services
+
+import Services.Syntax.*
 
 import binding._
 import table._
@@ -38,6 +55,10 @@ trait ProgramMapping[F[_]]
      with GroupElementView[F] {
 
   def user: User
+  def itcClient: ItcClient[F]
+  def services: Resource[F, Services[F]]
+  def commitHash: CommitHash
+  def plannedTimeCalculator: PlannedTimeCalculator.ForInstrumentMode
 
   lazy val ProgramMapping: ObjectMapping =
     ObjectMapping(
@@ -54,8 +75,9 @@ trait ProgramMapping[F[_]]
         SqlObject("groupElements", Join(ProgramTable.Id, GroupElementView.ProgramId)),
         SqlObject("allGroupElements", Join(ProgramTable.Id, GroupElementView.ProgramId)),
         SqlObject("obsAttachments", Join(ProgramTable.Id, ObsAttachmentTable.ProgramId)),
-        SqlObject("proposalAttachments", Join(ProgramTable.Id, ProposalAttachmentTable.ProgramId))
-      ),
+        SqlObject("proposalAttachments", Join(ProgramTable.Id, ProposalAttachmentTable.ProgramId)),
+        EffectField("plannedTimeRange", plannedTimeHandler, List("id"))
+      )
     )
 
   lazy val ProgramElaborator: Map[TypeRef, PartialFunction[Select, Result[Query]]] =
@@ -122,6 +144,110 @@ trait ProgramMapping[F[_]]
           )
       }
     )
+
+  lazy val plannedTimeHandler: EffectHandler[F] =
+    new EffectHandler[F] {
+
+      case class Acc(
+        valid:      List[PlannedTimeRange],
+        complete:   Set[Observation.Id],
+        incomplete: Set[Observation.Id]
+      ) {
+
+        def add(r: RangeResult): Acc =
+          Acc(
+            r.plannedTimeRange.fold(valid)(_ :: valid),
+            complete   ++ r.completeObservations,
+            incomplete ++ r.incompleteObservations
+          )
+
+        def toRangeResult(minRequired: Int): RangeResult =
+          RangeResult(
+            if (valid.size < minRequired)
+              none[PlannedTimeRange]
+            else {
+              val longestToShortest = catsKernelOrderingForOrder(Order.reverse(Order[PlannedTime]))
+              PlannedTimeRange.from(
+                valid.map(_.min).sorted.take(minRequired).combineAllOption.getOrElse(PlannedTime.Zero),
+                valid.map(_.max).sorted(longestToShortest).take(minRequired).combineAllOption.getOrElse(PlannedTime.Zero)
+              ).some
+            },
+            complete,
+            incomplete
+          )
+
+      }
+
+      object Acc {
+        val Empty: Acc = Acc(List.empty, Set.empty, Set.empty)
+
+        def fromChildResults(cs: List[RangeResult]): Acc =
+          cs.foldLeft(Empty)(_.add(_))
+
+      }
+
+      case class RangeResult(
+        plannedTimeRange:       Option[PlannedTimeRange],
+        completeObservations:   Set[Observation.Id],
+        incompleteObservations: Set[Observation.Id]
+      )
+
+      object RangeResult {
+
+        def combine(
+          minRequired:  Option[NonNegShort],
+          childResults: List[RangeResult]
+        ): RangeResult =
+          Acc.fromChildResults(childResults)
+             .toRangeResult(minRequired.fold(childResults.size)(_.value.intValue))
+      }
+
+      def plannedTimeRange(pid: Program.Id, root: GroupTree): F[RangeResult] =
+        root match {
+
+          case GroupTree.Leaf(oid) =>
+            services.useNonTransactionally {
+              generator(commitHash, itcClient, plannedTimeCalculator)
+                .digest(pid, oid)
+                .map(_.fold(
+                  _ => RangeResult(None, Set.empty, Set(oid)),
+                  d =>
+                   val pt = d.fullPlannedTime
+                   RangeResult(PlannedTimeRange.from(pt, pt).some, Set(oid), Set.empty)
+                ))
+            }
+
+          case GroupTree.Branch(_, min, _, children, _, _, _, _) =>
+            children.traverse(plannedTimeRange(pid, _)).map(RangeResult.combine(min, _))
+
+          case GroupTree.Root(_, children) =>
+            children.traverse(plannedTimeRange(pid, _)).map(RangeResult.combine(None, _))
+
+        }
+
+      def calculate(pid: Program.Id): F[RangeResult] =
+        services
+          .useTransactionally(groupService.selectGroups(pid))
+          .flatMap(plannedTimeRange(pid, _))
+
+
+      def runEffects(queries: List[(Query, Cursor)]): F[Result[List[(Query, Cursor)]]] =
+        (for {
+          ctx <- ResultT(queries.traverse { case (_, cursor) => cursor.fieldAs[Program.Id]("id") }.pure[F])
+          res <- ctx.distinct.traverse { pid => ResultT(calculate(pid).map(Result.success)).tupleLeft(pid) }
+        } yield
+          ctx
+            .flatMap(pid => res.find(r => r._1 === pid).map(_._2).toList)
+            .zip(queries)
+            .map { case (result, (child, parentCursor)) =>
+              import lucuma.odb.json.plannedtime.given
+              import lucuma.odb.json.time.query.given
+              val json: Json     = Json.fromFields(List("plannedTimeRange" -> result.plannedTimeRange.asJson))
+              val cursor: Cursor = CirceCursor(parentCursor.context, json, Some(parentCursor), parentCursor.fullEnv)
+              (child, cursor)
+            }
+        ).value
+    }
 
 }
 
