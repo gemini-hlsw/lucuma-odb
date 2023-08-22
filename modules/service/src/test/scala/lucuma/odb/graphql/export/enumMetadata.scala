ae91c5a5dd23d04b515bc421bf55d71ff1e73e2b
Rob Norris <rob_norris@mac.com>
2023-08-22 14:31:17 -0500

    sanity check

---
+++ modules/service/src/test/scala/lucuma/odb/graphql/export/enumMetadata.scala
@@ -0,0 +1,25 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql
+package `export`
+
+import cats.effect.IO
+import org.http4s.client.JavaNetClientBuilder
+
+class enumMetadata extends OdbSuite {
+
+  val validUsers = Nil
+  val client = JavaNetClientBuilder[IO].create
+
+  test("enumMetadata") {
+    server.use { svr =>
+      client.expect[String](svr.baseUri / "export" / "enumMetadata").map { s =>
+        assert(s.startsWith("export const enumMetadata ='{"))
+      }
+    }
+  }
+
+}
+
+
