e91d25f84afaa5494adad7f5d96b6df237e8d08a
Shane Walker <pistes.clary_0w@icloud.com>
2023-10-24 10:25:25 -0300

    cleanup

--- modules/service/src/main/scala/lucuma/odb/graphql/mapping/ExecutionMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/ExecutionMapping.scala
@@ -10,7 +10,9 @@ import cats.syntax.bifunctor.*
 import cats.syntax.functor.*
 import cats.syntax.parallel.*
 import eu.timepit.refined.cats.*
+import eu.timepit.refined.types.numeric.NonNegInt
 import grackle.Env
+import grackle.Predicate
 import grackle.Predicate.True
 import grackle.Predicate.and
 import grackle.Query
@@ -30,12 +32,15 @@ import lucuma.core.model.Program
 import lucuma.core.model.User
 import lucuma.core.model.Visit
 import lucuma.core.model.sequence.Dataset
+import lucuma.core.util.Gid
 import lucuma.itc.client.ItcClient
 import lucuma.odb.graphql.binding.DatasetIdBinding
 import lucuma.odb.graphql.binding.ExecutionEventIdBinding
 import lucuma.odb.graphql.binding.NonNegIntBinding
 import lucuma.odb.graphql.binding.VisitIdBinding
+import lucuma.odb.graphql.predicate.LeafPredicates
 import lucuma.odb.graphql.predicate.Predicates
+import lucuma.odb.graphql.predicate.ProgramPredicates
 import lucuma.odb.json.all.query.given
 import lucuma.odb.logic.Generator
 import lucuma.odb.logic.PlannedTimeCalculator
@@ -66,84 +71,59 @@ trait ExecutionMapping[F[_]] extends ObservationEffectHandler[F] with Predicates
       )
     )
 
+  private def subquery[A: Gid](
+    rOFFSET:       Result[Option[A]],
+    rLIMIT:        Result[Option[NonNegInt]],
+    typeRef:       TypeRef,
+    idPredicate:   LeafPredicates[A],
+    progPredicate: ProgramPredicates
+  ): Elab[Unit] =
+    Elab.transformChild { child =>
+      (rOFFSET, rLIMIT).parTupled.flatMap { (OFFSET, LIMIT) =>
+        val limit = LIMIT.foldLeft(ResultMapping.MaxLimit)(_ min _.value)
+        ResultMapping.selectResult(child, limit) { q =>
+          FilterOrderByOffsetLimit(
+            pred = Some(and(List(
+              OFFSET.map(idPredicate.gtEql).getOrElse(True),
+              progPredicate.isVisibleTo(user),
+            ))),
+            oss = Some(List(OrderSelection[A](typeRef / "id"))),
+            offset = None,
+            limit = Some(limit + 1), // Select one extra row here.
+            child = q
+          )
+        }
+      }
+    }
+
   lazy val ExecutionElaborator: PartialFunction[(TypeRef, String, List[Binding]), Elab[Unit]] = {
+
     case (ExecutionType, "config", List(
       Generator.FutureLimit.Binding.Option(FutureLimitParam, rFutureLimit)
     )) =>
       Elab.liftR(rFutureLimit).flatMap { futureLimit =>
         Elab.env(FutureLimitParam -> futureLimit.getOrElse(Generator.FutureLimit.Default))
       }
+
     case (ExecutionType, "datasets", List(
       DatasetIdBinding.Option("OFFSET", rOFFSET),
       NonNegIntBinding.Option("LIMIT", rLIMIT)
     )) =>
-      Elab.transformChild { child =>
-        (rOFFSET, rLIMIT).parTupled.flatMap { (OFFSET, LIMIT) =>
-          val limit = LIMIT.foldLeft(ResultMapping.MaxLimit)(_ min _.value)
-          ResultMapping.selectResult(child, limit) { q =>
-            FilterOrderByOffsetLimit(
-              pred = Some(and(List(
-                OFFSET.map(Predicates.dataset.id.gtEql).getOrElse(True),
-                Predicates.dataset.observation.program.isVisibleTo(user),
-              ))),
-              oss = Some(List(
-                OrderSelection[Dataset.Id](DatasetType / "id")
-              )),
-              offset = None,
-              limit = Some(limit + 1), // Select one extra row here.
-              child = q
-            )
-          }
-        }
-      }
+      subquery(rOFFSET, rLIMIT, DatasetType, Predicates.dataset.id, Predicates.dataset.observation.program)
+
     case (ExecutionType, "events", List(
       ExecutionEventIdBinding.Option("OFFSET", rOFFSET),
       NonNegIntBinding.Option("LIMIT", rLIMIT)
     )) =>
-      Elab.transformChild { child =>
-        (rOFFSET, rLIMIT).parTupled.flatMap { (OFFSET, LIMIT) =>
-          val limit = LIMIT.foldLeft(ResultMapping.MaxLimit)(_ min _.value)
-          ResultMapping.selectResult(child, limit) { q =>
-            FilterOrderByOffsetLimit(
-              pred = Some(and(List(
-                OFFSET.map(Predicates.executionEvent.id.gtEql).getOrElse(True),
-                Predicates.executionEvent.observation.program.isVisibleTo(user),
-              ))),
-              oss = Some(List(
-                OrderSelection[ExecutionEvent.Id](ExecutionEventType / "id")
-              )),
-              offset = None,
-              limit = Some(limit + 1), // Select one extra row here.
-              child = q
-            )
-          }
-        }
-      }
+      subquery(rOFFSET, rLIMIT, ExecutionEventType, Predicates.executionEvent.id, Predicates.executionEvent.observation.program)
+
     case (ExecutionType, "visits", List(
       VisitIdBinding.Option("OFFSET", rOFFSET),
       NonNegIntBinding.Option("LIMIT", rLIMIT)
     )) =>
-      Elab.transformChild { child =>
-        (rOFFSET, rLIMIT).parTupled.flatMap { (OFFSET, LIMIT) =>
-          val limit = LIMIT.foldLeft(ResultMapping.MaxLimit)(_ min _.value)
-          ResultMapping.selectResult(child, limit) { q =>
-            FilterOrderByOffsetLimit(
-              pred = Some(and(List(
-                OFFSET.map(Predicates.visit.id.gtEql).getOrElse(True),
-                Predicates.visit.observation.program.isVisibleTo(user),
-              ))),
-              oss = Some(List(
-                OrderSelection[Visit.Id](VisitType / "id")
-              )),
-              offset = None,
-              limit = Some(limit + 1), // Select one extra row here.
-              child = q
-            )
-          }
-        }
-      }
-    }
-    
+      subquery(rOFFSET, rLIMIT, VisitType, Predicates.visit.id, Predicates.visit.observation.program)
+
+  }
 
   extension (e: Generator.Error) {
     def toResult: Result[Json] =
