0471db1b5ffa571f1634adfc2186d0a192dd7cb4
Shane Walker <pistes.clary_0w@icloud.com>
2023-10-30 17:47:46 -0300

    LookupFrom

--- modules/service/src/main/scala/lucuma/odb/graphql/mapping/AtomRecordSelectResultMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/AtomRecordSelectResultMapping.scala
@@ -9,18 +9,24 @@ import table.ObservationView
 import table.VisitTable
 
 trait AtomRecordSelectResultMapping[F[_]]
-  extends AtomRecordTable[F[_]]
+  extends AtomRecordTable[F]
+     with LookupFrom[F]
      with ObservationView[F]
      with ResultMapping[F]
      with VisitTable[F] {
 
-  lazy val AtomRecordSelectResultMapping: TypeMapping =
+  lazy val AtomRecordSelectResultMapping: TypeMapping = {
+
+    val fromExecution: ObjectMapping =
+      nestedSelectResultMapping(AtomRecordSelectResultType, ObservationView.Id, Join(ObservationView.Id, AtomRecordTable.ObservationId))
+
+    val fromVisit: ObjectMapping =
+      nestedSelectResultMapping(AtomRecordSelectResultType, VisitTable.Id, Join(VisitTable.Id, AtomRecordTable.VisitId))
+
     SwitchMapping(
       AtomRecordSelectResultType,
-      List(
-        ExecutionType / "atomRecords" -> nestedSelectResultMapping(AtomRecordSelectResultType, ObservationView.Id, Join(ObservationView.Id, AtomRecordTable.ObservationId)),
-        VisitType / "atomRecords"     -> nestedSelectResultMapping(AtomRecordSelectResultType, VisitTable.Id, Join(VisitTable.Id, AtomRecordTable.VisitId))
-      )
+      (ExecutionType / "atomRecords" -> fromExecution) :: lookupFromVisit(fromVisit, "atomRecords")
     )
+  }
 
 }
