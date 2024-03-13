4b762bd8759a19e7c3531c21d506ce53a3f4b319
Shane Walker <pistes.clary_0w@icloud.com>
2024-03-13 13:36:20 -0300

    codec tests

--- modules/service/src/test/scala/lucuma/odb/data/DatasetReferenceSuite.scala
+++ modules/service/src/test/scala/lucuma/odb/data/DatasetReferenceSuite.scala
@@ -4,16 +4,19 @@
 package lucuma.odb.data
 
 import cats.kernel.laws.discipline.*
+import io.circe.testing.ArbitraryInstances
+import io.circe.testing.CodecTests
 import lucuma.core.optics.laws.discipline.*
 import lucuma.odb.data.arb.ArbDatasetReference
 
-final class DatasetReferenceSuite extends munit.DisciplineSuite {
+final class DatasetReferenceSuite extends munit.DisciplineSuite with ArbitraryInstances {
 
   import ArbDatasetReference.given
   import ArbDatasetReference.datasetReferenceStrings
 
   checkAll("DatasetReference", OrderTests[DatasetReference].order)
   checkAll("DatasetReference", FormatTests(DatasetReference.fromString).formatWith(datasetReferenceStrings))
+  checkAll("DatasetReference", CodecTests[DatasetReference].codec)
 
 }
 
