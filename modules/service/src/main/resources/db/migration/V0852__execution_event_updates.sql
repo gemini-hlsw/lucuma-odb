b908e256ef2e66baf3f507716c36815687bd2744
Shane Walker <pistes.clary_0w@icloud.com>
2024-03-18 20:48:44 -0300

    fix migration

--- modules/service/src/main/resources/db/migration/V0852__execution_event_updates.sql
+++ modules/service/src/main/resources/db/migration/V0852__execution_event_updates.sql
@@ -21,15 +21,3 @@ ALTER TYPE e_execution_event_type ADD VALUE 'slew' BEFORE 'step';
 -- Add a column for the slew stage
 ALTER TABLE t_execution_event
   ADD COLUMN c_slew_stage e_slew_stage NULL;
-
--- Update the event type constraint
-ALTER TABLE t_execution_event
-  ADD CONSTRAINT check_event_type_conditions CHECK (
-    CASE
-      WHEN c_event_type = 'sequence' THEN c_sequence_command IS NOT NULL AND c_step_ID IS NULL     AND c_dataset_id IS NULL
-      WHEN c_event_type = 'slew'     THEN c_slew_stage       IS NOT NULL AND c_step_id IS NULL     AND c_dataset_id IS NULL
-      WHEN c_event_type = 'step'     THEN c_step_stage       IS NOT NULL AND c_step_id IS NOT NULL AND c_dataset_id IS NULL
-      WHEN c_event_type = 'dataset'  THEN c_dataset_stage    IS NOT NULL AND c_step_id IS NOT NULL AND c_dataset_id IS NOT NULL
-      ELSE FALSE
-    END
-  );
