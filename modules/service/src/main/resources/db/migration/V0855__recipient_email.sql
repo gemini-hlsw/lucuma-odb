467c2842e9343c71233dc6ed3807c810b61f35dc
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-03-29 17:48:31 -0300

    Use a simpler regex

--- modules/service/src/main/resources/db/migration/V0855__recipient_email.sql
+++ modules/service/src/main/resources/db/migration/V0855__recipient_email.sql
@@ -1,9 +1,8 @@
 
 -- Update recipient email regex check
 
-ALTER DOMAIN d_email DROP CONSTRAINT;
-ALTER DOMAIN d_email ADD CONSTRAINT 
-CHECK(
+ALTER DOMAIN d_email DROP CONSTRAINT d_email_check;
+ALTER DOMAIN d_email ADD CONSTRAINT d_email_check CHECK(
   --  N.B. this is the same pattern used in the EmailAddress class; if we change one we need to change the other.
-  VALUE ~ '(?:[a-z0-9!#$%&'*+x\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+  VALUE ~ '^[a-zA-Z0-9_+&-]+(?:.[a-zA-Z0-9_+&-]+)*@(?:[a-zA-Z0-9-]+.)+[a-zA-Z]{2,7}$'
 );
