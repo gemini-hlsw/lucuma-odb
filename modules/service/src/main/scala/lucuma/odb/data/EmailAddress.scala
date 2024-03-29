0d92a7fc0f1b1964a76a68de4559079b8711cbec
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-03-29 17:48:31 -0300

    Use a simpler regex

--- modules/service/src/main/scala/lucuma/odb/data/EmailAddress.scala
+++ modules/service/src/main/scala/lucuma/odb/data/EmailAddress.scala
@@ -13,7 +13,7 @@ extension (self: EmailAddress) def value: String = self
 object EmailAddress:
 
   // N.B. this is the same pattern used in the database constraint; if we change one we need to change the other.
-  private val Pat = """(?:[a-z0-9!#$%&'*+x\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".r
+  private val Pat = """^[a-zA-Z0-9_+&-]+(?:.[a-zA-Z0-9_+&-]+)*@(?:[a-zA-Z0-9-]+.)+[a-zA-Z]{2,7}$""".r
 
   def fromString: Prism[String, EmailAddress] =
     Prism((s: String) => Option.when(Pat.matches(s))(s))(identity)
