67b1e2edc42be89832d7012311c23e2c6f19a4a1
Shane Walker <pistes.clary_0w@icloud.com>
2024-04-08 15:39:05 -0400

    remove sequenceType in completed atom map next

--- modules/sequence/src/main/scala/lucuma/odb/sequence/data/CompletedAtomMap.scala
+++ modules/sequence/src/main/scala/lucuma/odb/sequence/data/CompletedAtomMap.scala
@@ -7,7 +7,6 @@ import cats.syntax.eq.*
 import cats.syntax.option.*
 import eu.timepit.refined.types.numeric.NonNegShort
 import eu.timepit.refined.types.numeric.PosInt
-import lucuma.core.enums.SequenceType
 import lucuma.core.model.sequence.Atom
 import lucuma.core.model.sequence.StepConfig
 
@@ -18,58 +17,42 @@ import lucuma.core.model.sequence.StepConfig
  *
  * @tparam D dynamic instrument configuration type
  */
-opaque type CompletedAtomMap[D] = Map[CompletedAtomMap.Key[D], PosInt]
+opaque type CompletedAtomMap[D] = Map[CompletedAtomMap.AtomMatch[D], PosInt]
 
 object CompletedAtomMap {
 
   type StepMatch[D] = (D, StepConfig)
   type AtomMatch[D] = List[StepMatch[D]]
 
-  case class Key[D](
-    sequenceType: SequenceType,
-    atomMatch:    AtomMatch[D]
-  )
-
-  object Key {
-
-    def acquisition[D](steps: StepMatch[D]*): Key[D] =
-      Key(SequenceType.Acquisition, steps.toList)
-
-    def science[D](steps: StepMatch[D]*): Key[D] =
-      Key(SequenceType.Science, steps.toList)
-      
-    def fromProtoAtom[D](
-      sequenceType: SequenceType,
-      atom:         ProtoAtom[ProtoStep[D]]
-    ): Key[D] =
-      Key(sequenceType, atom.steps.map(s => (s.value, s.stepConfig)).toList)
-
+  object AtomMatch {
+    def fromProtoAtom[D](atom: ProtoAtom[ProtoStep[D]]): AtomMatch[D] =
+      atom.steps.map(s => (s.value, s.stepConfig)).toList
   }
 
   def Empty[D]: CompletedAtomMap[D] =
     Map.empty
 
-  def from[D](seq: (Key[D], PosInt)*): CompletedAtomMap[D] =
+  def from[D](seq: (AtomMatch[D], PosInt)*): CompletedAtomMap[D] =
     Map.from(seq)
 
   extension [D](m: CompletedAtomMap[D]) {
 
-    def increment(k: Key[D]): CompletedAtomMap[D] =
+    def increment(k: AtomMatch[D]): CompletedAtomMap[D] =
       m.updatedWith(k) {
         case None    => PosInt.unsafeFrom(1).some
         case Some(p) => PosInt.from(p.value + 1).toOption
       }
       
-    def decrement(k: Key[D]): CompletedAtomMap[D] =
+    def decrement(k: AtomMatch[D]): CompletedAtomMap[D] =
       m.updatedWith(k)(_.flatMap(p => PosInt.from(p.value - 1).toOption))
 
-    def contains(k: Key[D]): Boolean =
+    def contains(k: AtomMatch[D]): Boolean =
       m.contains(k)
 
     def isEmpty: Boolean =
       m.isEmpty
 
-    def toMap: Map[Key[D], PosInt] =
+    def toMap: Map[AtomMatch[D], PosInt] =
       m
 
     /**
@@ -79,10 +62,9 @@ object CompletedAtomMap {
      * `false` is returned along with this completed atom map.
      */
     def matchAtom(
-      s: SequenceType,
       a: ProtoAtom[ProtoStep[D]]
     ): (CompletedAtomMap[D], Boolean) = {
-      val k = Key.fromProtoAtom(s, a)
+      val k = AtomMatch.fromProtoAtom(a)
       if (contains(k)) (decrement(k), true) else (m, false)
     }
 
@@ -90,7 +72,7 @@ object CompletedAtomMap {
 
   trait Builder[D] {
     def reset: Builder[D]
-    def next(aid: Atom.Id, count: NonNegShort, sequenceType: SequenceType, step: StepMatch[D]): Builder[D]
+    def next(aid: Atom.Id, count: NonNegShort, step: StepMatch[D]): Builder[D]
     def build: CompletedAtomMap[D]
   }
 
@@ -101,18 +83,17 @@ object CompletedAtomMap {
     private case class Reset[D](completed: CompletedAtomMap[D]) extends Builder[D] {
       override def reset: Builder[D] = this
 
-      override def next(aid: Atom.Id, count: NonNegShort, sequenceType: SequenceType, step: StepMatch[D]): Builder[D] =
-        InProgress(aid, count, sequenceType, List(step), completed)
+      override def next(aid: Atom.Id, count: NonNegShort, step: StepMatch[D]): Builder[D] =
+        InProgress(aid, count, List(step), completed)
 
       override def build: CompletedAtomMap[D] = completed
     }
 
     private case class InProgress[D](
-      inProgressAtomId:       Atom.Id,
-      inProgressCount:        NonNegShort,
-      inProgressSequenceType: SequenceType,
-      inProgressSteps:        AtomMatch[D],
-      completed:              CompletedAtomMap[D]
+      inProgressAtomId: Atom.Id,
+      inProgressCount:  NonNegShort,
+      inProgressSteps:  AtomMatch[D],
+      completed:        CompletedAtomMap[D]
     ) extends Builder[D] {
 
       override def reset: Builder[D] = Reset(completed)
@@ -120,12 +101,11 @@ object CompletedAtomMap {
       private def addStep(step: StepMatch[D]): InProgress[D] =
         copy(inProgressSteps = step :: inProgressSteps)
 
-      override def next(aid: Atom.Id, count: NonNegShort, sequenceType: SequenceType, step: StepMatch[D]): Builder[D] =
-        if (aid === inProgressAtomId) addStep(step) // continue existing atom
-        else InProgress(aid, count, sequenceType, List(step), build) // start a new atom
+      override def next(aid: Atom.Id, count: NonNegShort, step: StepMatch[D]): Builder[D] =
+        if (aid === inProgressAtomId) addStep(step)    // continue existing atom
+        else InProgress(aid, count, List(step), build) // start a new atom
 
-      private def inProgressKey: CompletedAtomMap.Key[D] =
-        CompletedAtomMap.Key(inProgressSequenceType, inProgressSteps.reverse)
+      private def inProgressKey: AtomMatch[D] = inProgressSteps.reverse
 
       override def build: CompletedAtomMap[D] =
         if (inProgressSteps.sizeIs != inProgressCount.value) completed
