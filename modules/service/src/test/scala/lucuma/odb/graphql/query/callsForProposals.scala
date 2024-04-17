ba072f124c6564f553874c18b30837266d9f5318
Shane Walker <pistes.clary_0w@icloud.com>
2024-04-17 10:45:53 -0400

    Initial WHERE clause

--- modules/service/src/test/scala/lucuma/odb/graphql/query/callsForProposals.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/callsForProposals.scala
@@ -4,15 +4,50 @@
 package lucuma.odb.graphql
 package query
 
+import cats.effect.IO
 import cats.syntax.either.*
 import io.circe.Json
 import io.circe.literal.*
+import lucuma.core.model.CallForProposals
 
 class callsForProposals extends OdbSuite {
 
-  val pi   = TestUsers.Standard.pi(1, 30)
+  val pi    = TestUsers.Standard.pi(1, 30)
+  val staff = TestUsers.Standard.staff(3, 103)
 
-  override val validUsers = List(pi).toList
+  override val validUsers = List(pi, staff)
+  /*
+                type:        REGULAR_SEMESTER
+                semester:    "2025A"
+                activeStart: "2025-02-01 14:00:00"
+                activeEnd:   "2025-07-31 14:00:00"
+   */
+
+  def createCall(set: String): IO[CallForProposals.Id] =
+    query(
+      user = staff,
+      query = s"""
+        mutation {
+          createCallForProposals(
+            input: {
+              SET: {
+                $set
+              }
+            }
+          ) {
+            callForProposals {
+              id
+            }
+          }
+        }
+      """
+    ).flatMap {
+      _.hcursor
+       .downFields("createCallForProposals", "callForProposals", "id")
+       .as[CallForProposals.Id]
+       .leftMap(f => new RuntimeException(f.message))
+       .liftTo[IO]
+    }
 
   test("callsForProposals - empty") {
     expect(
@@ -38,5 +73,37 @@ class callsForProposals extends OdbSuite {
     )
   }
 
-  // TODO: more tests when we have a WHERE clause
+  test("WHERE id") {
+    createCall(s"""
+      type:        REGULAR_SEMESTER
+      semester:    "2025A"
+      activeStart: "2025-02-01 14:00:00"
+      activeEnd:   "2025-07-31 14:00:00"
+    """.stripMargin
+    ).flatMap { id =>
+      expect(pi,
+        s"""
+          query {
+            callsForProposals(WHERE: { id: { EQ: "$id" } }) {
+              matches {
+                id
+              }
+            }
+          }
+        """,
+        json"""
+          {
+            "callsForProposals": {
+              "matches": [
+                {
+                  "id": $id
+                }
+              ]
+            }
+          }
+        """.asRight
+      )
+    }
+  }
+
 }
