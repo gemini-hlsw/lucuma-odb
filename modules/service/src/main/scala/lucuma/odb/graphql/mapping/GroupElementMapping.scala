c9919b808bdbf6994499753cfe24aeab24b93dee
Rob Norris <rob_norris@mac.com>
2024-04-29 14:39:10 -0500

    checkpoint

--- modules/service/src/main/scala/lucuma/odb/graphql/mapping/GroupElementMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/GroupElementMapping.scala
@@ -15,8 +15,8 @@ trait GroupElementMapping[F[_]] extends GroupElementView[F] with GroupView[F] wi
 
   lazy val GroupElementMapping =
     ObjectMapping(GroupElementType)(
-      SqlField("id", GroupElementView.Id, key = true),
-      SqlField("programId", GroupElementView.ProgramId),
+      SqlField("id", GroupElementView.Id, key = true, hidden = true),
+      SqlField("programId", GroupElementView.ProgramId, hidden = true),
       SqlField("parentGroupId", GroupElementView.GroupId),
       SqlField("parentIndex", GroupElementView.Index),
       SqlObject("group", Join(GroupElementView.ChildGroupId, GroupView.Id)),
