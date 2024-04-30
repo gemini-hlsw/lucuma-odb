2ce6beddcb8fe3985d780b5eaee21b31bda967d2
Shane Walker <pistes.clary_0w@icloud.com>
2024-04-30 08:28:48 -0400

    Add instrument name to SV titles

--- modules/service/src/main/resources/db/migration/V0867__cfp_titles.sql
+++ modules/service/src/main/resources/db/migration/V0867__cfp_titles.sql
@@ -4,32 +4,35 @@
 CREATE FUNCTION format_cfp_title(
   cfp_type e_cfp_type,
   semester d_semester,
-  active   tsrange
+  active   tsrange,
+  instruments d_tag[]
 ) RETURNS text AS $$
+DECLARE
+  instrument_list text;
 BEGIN
+  instrument_list := array_to_string(instruments, ', ');
   RETURN CASE
-    WHEN cfp_type = 'demo_science'        THEN concat(semester, ' ', 'Demo Science')
-    WHEN cfp_type = 'directors_time'      THEN concat(semester, ' ', 'Director''s Time')
+    WHEN cfp_type = 'demo_science'        THEN concat(semester, ' Demo Science')
+    WHEN cfp_type = 'directors_time'      THEN concat(semester, ' Director''s Time')
     WHEN cfp_type = 'fast_turnaround'     THEN
       concat(
-        left(semester, length(semester) - 1),
+        left(semester, -1),
         ' ',
         to_char(lower(active) + (upper(active) - lower(active))/2, 'FMMonth'),
-        ' Fast Turnaround'
+        ' ',
+        'Fast Turnaround'
       )
-    WHEN cfp_type = 'large_program'       THEN concat(semester, ' ', 'Large Program')
-    WHEN cfp_type = 'poor_weather'        THEN concat(semester, ' ', 'Poor Weather')
-    WHEN cfp_type = 'regular_semester'    THEN concat(semester, ' ', 'Regular Semester')
-    WHEN cfp_type = 'system_verification' THEN concat(semester, ' ', 'System Verification')
-    ELSE concat(semester, ' ', 'Unknown Call Type: ', cfp_type)
-  END;
+    WHEN cfp_type = 'large_program'       THEN concat(semester, ' Large Program')
+    WHEN cfp_type = 'poor_weather'        THEN concat(semester, ' Poor Weather')
+    WHEN cfp_type = 'regular_semester'    THEN concat(semester, ' Regular Semester')
+    WHEN cfp_type = 'system_verification' THEN
+          concat(semester, nullif(' ' || instrument_list, ' '), ' System Verification')
+    ELSE concat(semester, ' ', cfp_type)
+  END CASE;
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
 
-ALTER TABLE t_cfp
-  ADD COLUMN c_title text GENERATED ALWAYS AS (format_cfp_title(c_type, c_semester, c_active)) STORED;
-
--- Recreate the view to pick up the title column.
+-- Recreate the view to add a title column.
 DROP VIEW v_cfp;
 
 CREATE VIEW v_cfp AS
@@ -39,15 +42,18 @@ CREATE VIEW v_cfp AS
     CASE WHEN c.c_ra_end    IS NOT NULL THEN c.c_cfp_id END AS c_ra_end_id,
     CASE WHEN c.c_dec_start IS NOT NULL THEN c.c_cfp_id END AS c_dec_start_id,
     CASE WHEN c.c_dec_end   IS NOT NULL THEN c.c_cfp_id END AS c_dec_end_id,
-    array_remove(array_agg(i.c_instrument ORDER BY i.c_instrument), NULL) AS c_instruments,
+    array_remove(array_agg(ci.c_instrument ORDER BY ci.c_instrument), NULL) AS c_instruments,
     (SELECT COUNT(*)
       FROM t_cfp_partner
      WHERE t_cfp_partner.c_cfp_id = c.c_cfp_id
        AND t_cfp_partner.c_deadline > CURRENT_TIMESTAMP
-    ) > 0 AS c_is_open
+    ) > 0 AS c_is_open,
+    format_cfp_title(c.c_type, c.c_semester, c.c_active, array_remove(array_agg(i.c_long_name ORDER BY i.c_long_name), NULL)) AS c_title
   FROM
     t_cfp c
   LEFT JOIN
-    t_cfp_instrument i ON c.c_cfp_id = i.c_cfp_id
+    t_cfp_instrument ci ON c.c_cfp_id = ci.c_cfp_id
+  LEFT JOIN
+    t_instrument i ON i.c_tag = ci.c_instrument
   GROUP BY
     c.c_cfp_id;
