36814e037b8a4e77700b11b10aa755dd7f8c1cb6
Shane Walker <pistes.clary_0w@icloud.com>
2024-05-05 17:36:58 -0400

    Add poor weather

---
+++ modules/service/src/main/resources/db/migration/V0868__poor_weather.sql
@@ -0,0 +1,7 @@
+ALTER TYPE e_science_subtype ADD VALUE 'poor_weather'    BEFORE 'queue';
+
+ALTER TABLE t_science_subtype
+  DROP CONSTRAINT t_science_subtype_c_abbr_check,
+  ADD CONSTRAINT  t_science_subtype_c_abbr_check CHECK (c_abbr IN ('C', 'D', 'F', 'L', 'P', 'Q', 'S', 'V'));
+
+INSERT INTO t_science_subtype VALUES ('poor_weather', 'P', 'Poor Weather');
