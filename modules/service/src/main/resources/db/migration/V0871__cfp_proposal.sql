8737b4d010f7401ae2e457f0490670f9b8540d6c
Shane Walker <pistes.clary_0w@icloud.com>
2024-05-17 11:07:36 -0400

    cleanup

--- modules/service/src/main/resources/db/migration/V0871__cfp_proposal.sql
+++ modules/service/src/main/resources/db/migration/V0871__cfp_proposal.sql
@@ -3,6 +3,7 @@
 ALTER TABLE t_program
   ADD CONSTRAINT t_program_unique_pid_subtype UNIQUE (c_program_id, c_science_subtype);
 
+-- We'll deal with intensive and exchange "later"
 DELETE FROM t_proposal
   WHERE c_class = 'intensive' OR
         c_class = 'exchange';
@@ -11,6 +12,8 @@ ALTER TABLE t_proposal
   DROP CONSTRAINT t_proposal_check_min_total,
   DROP CONSTRAINT t_proposal_check_total;
 
+-- This trigger would update the science subtype based on the proposal class.
+-- The proposal class is being dropped in favor of the call for proposals type.
 DROP TRIGGER update_science_subtype_trigger ON t_proposal;
 DROP FUNCTION update_science_subtype;
 
@@ -71,17 +74,3 @@ CREATE VIEW v_proposal AS
     END AS c_ft_partner
   FROM
     t_proposal p;
-
-
---    "t_proposal_c_abstract_check" CHECK (c_abstract IS NULL OR length(c_abstract) > 0)
---    "t_proposal_c_title_check" CHECK (c_title IS NULL OR length(c_title) > 0)
---    "t_proposal_check_min_total" CHECK ((c_min_percent_total IS NOT NULL) = (c_class::text = 'intensive'::text OR c_class::text = 'large_program'::text))
---    "t_proposal_check_total" CHECK ((c_total_time IS NOT NULL) = (c_class::text = 'intensive'::text OR c_class::text = 'large_program'::text))
-
---    "t_proposal_c_category_fkey" FOREIGN KEY (c_category) REFERENCES t_tac_category(c_tag)
---    "t_proposal_c_class_fkey" FOREIGN KEY (c_class) REFERENCES t_proposal_class(c_tag)
---    "t_proposal_c_program_id_fkey" FOREIGN KEY (c_program_id) REFERENCES t_program(c_program_id) ON DELETE CASCADE
-
---    ch_program_edit_proposal_trigger AFTER INSERT OR DELETE OR UPDATE ON t_proposal DEFERRABLE INITIALLY IMMEDIATE FOR EACH ROW EXECUTE FUNCTION ch_proposal_edit()
---    reset_proposal_status_trigger AFTER DELETE ON t_proposal FOR EACH ROW EXECUTE FUNCTION reset_proposal_status()
---    update_science_subtype_trigger BEFORE INSERT OR UPDATE ON t_proposal FOR EACH ROW EXECUTE FUNCTION update_science_subtype()
