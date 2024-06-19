f8af0b9f8b6f887eab162efa7ce66b5d4004f5b5
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-06-19 10:11:06 -0400

    Add calibrationRole to the program schema

--- modules/service/src/main/scala/lucuma/odb/graphql/table/ProgramTable.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/table/ProgramTable.scala
@@ -11,11 +11,12 @@ import lucuma.odb.util.Codecs.*
 trait ProgramTable[F[_]] extends BaseMapping[F] {
 
   object ProgramTable extends TableDef("t_program") {
-    val Id             = col("c_program_id", program_id)
-    val PiUserId       = col("c_pi_user_id", user_id)
-    val Existence      = col("c_existence", existence)
-    val Name           = col("c_name", text_nonempty.opt)
-    val ProposalStatus = col("c_proposal_status", tag)
+    val Id              = col("c_program_id", program_id)
+    val PiUserId        = col("c_pi_user_id", user_id)
+    val Existence       = col("c_existence", existence)
+    val Name            = col("c_name", text_nonempty.opt)
+    val ProposalStatus  = col("c_proposal_status", tag)
+    val CalibrationRole = col("c_calibration_role", calibration_role.opt)
     object PlannedTime {
       val Pi        = col("c_pts_pi", time_span)
       val Uncharged = col("c_pts_uncharged", time_span)
