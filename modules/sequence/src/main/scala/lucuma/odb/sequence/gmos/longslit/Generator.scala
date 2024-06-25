c6755f9cda9f09c784713ae404ea3dc41114649f
Shane Walker <pistes.clary_0w@icloud.com>
2024-06-25 11:30:59 -0400

    Move arc generation down to gmos sequence

--- modules/sequence/src/main/scala/lucuma/odb/sequence/gmos/longslit/Generator.scala
+++ modules/sequence/src/main/scala/lucuma/odb/sequence/gmos/longslit/Generator.scala
@@ -1,99 +1,193 @@
 // Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package lucuma.odb.sequence.gmos
-package longslit
+package lucuma.odb.sequence
+package gmos.longslit
 
-import cats.data.NonEmptyList
+import cats.data.State
+import cats.effect.Concurrent
+import cats.syntax.applicative.*
+import cats.syntax.either.*
+import cats.syntax.eq.*
 import cats.syntax.option.*
-import fs2.Pure
+import cats.syntax.functor.*
+import cats.syntax.traverse.*
+import fs2.Pipe
 import fs2.Stream
-import lucuma.core.enums.GmosNorthDetector.Hamamatsu as HamamatsuNorth
-import lucuma.core.enums.GmosNorthFilter
-import lucuma.core.enums.GmosNorthStageMode.FollowXy
-import lucuma.core.enums.GmosSouthDetector.Hamamatsu as HamamatsuSouth
-import lucuma.core.enums.GmosSouthFilter
-import lucuma.core.enums.GmosSouthStageMode.FollowXyz
-import lucuma.core.enums.MosPreImaging.IsNotMosPreImaging
+import lucuma.core.enums.GcalLampType
+import lucuma.core.enums.StepType
+import lucuma.core.model.sequence.StepConfig
+import lucuma.core.model.sequence.gmos.DynamicConfig
 import lucuma.core.model.sequence.gmos.StaticConfig
 import lucuma.itc.IntegrationTime
-import lucuma.odb.sequence.data.AcqExposureTime
+import lucuma.odb.sequence.data.CalLocation
+import lucuma.odb.sequence.data.Completion
 import lucuma.odb.sequence.data.ProtoAtom
 import lucuma.odb.sequence.data.ProtoExecutionConfig
 import lucuma.odb.sequence.data.ProtoStep
-import lucuma.odb.sequence.data.SciExposureTime
 
-
-/**
- * Generator for GMOS Long Slit.
- *
- * @tparam S static configuration type
- * @tparam D dynamic configuration type
- * @tparam G grating enumeration
- * @tparam F filter enumeration
- * @tparam U FPU enumeration
- */
-sealed abstract class Generator[S, D, G, F, U](
-  static:      S,
-  acqFilters:  NonEmptyList[F],
-  acqSequence: Acquisition[D, G, F, U],
-  sciSequence: Science[D, G, F, U]
-) {
+trait Generator[F[_], S, D] {
 
   def generate(
     acquisitionItc: IntegrationTime,
     scienceItc:     IntegrationTime,
-    config:         Config[G, F, U]
-  ): Either[String, ProtoExecutionConfig[Pure, S, ProtoAtom[ProtoStep[D]]]] = {
-
-    val acq = acqSequence.compute(
-      acqFilters,
-      config.fpu,
-      AcqExposureTime(acquisitionItc.exposureTime),
-      config.centralWavelength
-    )
-
-    val sci = sciSequence.compute(config, SciExposureTime(scienceItc.exposureTime))
-
-    Option
-      .when(scienceItc.exposures.value > 0 && acquisitionItc.exposures.value > 0)(
-        ProtoExecutionConfig(
-          static,
-          Stream(ProtoAtom.of("Acquisition - Initial", acq.ccd2, acq.p10, acq.slit)) ++
-            Stream(ProtoAtom.of("Acquisition - Slit", acq.slit)).repeat,
-          sci.map(a => ProtoAtom(a.description.some, a.steps))
-             .take(scienceItc.exposures.value)
-        )
-      ).toRight("ITC prescribes 0 exposures.")
-
-  }
+    completion:     Completion.Matcher[D]
+  ): Either[String, ProtoExecutionConfig[F, S, Either[String, (ProtoAtom[ProtoStep[D]], Long)]]]
 
 }
 
-
 object Generator {
 
-  object GmosNorth extends Generator(
-    StaticConfig.GmosNorth(
-      FollowXy,
-      HamamatsuNorth,
-      IsNotMosPreImaging,
-      None
-    ),
-    GmosNorthFilter.acquisition,
-    Acquisition.GmosNorth,
-    Science.GmosNorth
-  )
+  private def instantiate[F[_]: Concurrent, S, D, G, L, U](
+    pureGen:  PureGenerator[S, D, G, L, U],
+    expander: SmartGcalExpander[F, D],
+    config:   Config[G, L, U]
+  ): Generator[F, S, D] =
 
-  object GmosSouth extends Generator(
-    StaticConfig.GmosSouth(
-      FollowXyz,
-      HamamatsuSouth,
-      IsNotMosPreImaging,
-      None
-    ),
-    GmosSouthFilter.acquisition,
-    Acquisition.GmosSouth,
-    Science.GmosSouth
-  )
+    // Creates a smart arc as a single step in an atom with a given description
+    def smartArc(description: String, d: D): ProtoAtom[ProtoStep[D]] =
+      ProtoAtom.one(description, ProtoStep.smartArc(d))
+
+    // An arc step as a singleton atom at a particular location (before or after
+    // the corresponding science step).
+    case class Arc[D](atom: ProtoAtom[ProtoStep[D]], location: CalLocation)
+
+    // Zips each atom from the pure generator with the corresponding `Arc`.
+    // Most of these arcs will never be incorporated into the actual sequence,
+    // and instead will be filted out.  Only the required arc steps will
+    // actually make it into the generated sequence.  Note, the `Either`s in the
+    // input and output are introduced by smart gcal expansion.  If there was no
+    // matching smart flat in the input, it will be a Left.  If there is no
+    // matching smart arc in the output (or if the input was a Left), the
+    // combined result is a Left.
+    val zipWithArc: Pipe[
+      F,
+      Either[String, ProtoAtom[ProtoStep[D]]],
+      Either[String, (ProtoAtom[ProtoStep[D]], Arc[D])]
+    ] =
+      _.evalMapAccumulate(Map.empty[D, ProtoAtom[ProtoStep[D]]]) { (cache, eAtom) =>
+        eAtom.flatTraverse { atom =>
+          val arcTitle = atom.description.fold("Arc")(s => s"Arc: $s")
+
+          val scienceConfig: Either[String, (D, CalLocation)] =
+            atom
+              .steps
+              .zipWithIndex
+              .find(_._1.stepConfig.stepType === StepType.Science)
+              .map((s, l) => (s.value, if (l === 0) CalLocation.After else CalLocation.Before))
+              .toRight("Atom contains no science step!")
+
+          scienceConfig.flatTraverse { (d, loc) =>
+            cache
+              .get(d)
+              .fold(expander.expandAtom(smartArc(arcTitle, d)))(_.asRight.pure)
+              .map(_.map { arc => (cache.updated(d, arc), atom, Arc(arc, loc)) })
+          }
+        }
+        .map {
+          case Left(error)             => (cache, error.asLeft)
+          case Right(cache, atom, arc) => (cache, (atom, arc).asRight)
+        }
+      }
+      .map(_._2)
+
+    def scienceSequence(
+      comp: Completion.SequenceMatch[D]
+    ): Pipe[F, ProtoAtom[ProtoStep[D]], Either[String, (ProtoAtom[ProtoStep[D]], Long)]] =
+      (s: Stream[F, ProtoAtom[ProtoStep[D]]]) => {
+
+        val matchedArcs: Set[D] = comp.current.toList.flatMap(_._2.toList.flatMap(_._1)).collect {
+          case (d, StepConfig.Gcal(lamp,_,_,_)) if lamp.lampType === GcalLampType.Arc => d
+        }.toSet
+
+        case class Accumulator(
+          comp:          Completion.SequenceMatch[D],
+          index:         Long,
+          generatedArcs: Set[D]
+        ) {
+          def shouldGenerateArc(d: D): Boolean =
+            !(matchedArcs(d) || generatedArcs(d))
+        }
+
+        type MatchState[A] = State[Completion.SequenceMatch[D], A]
+
+        s.through(expander.expandSequence)  // smart-flat expansion
+         .through(zipWithArc)               // tuple with corresponding arc
+         .mapAccumulate(Accumulator(comp, 1L, Set.empty)) {
+
+           case (acc, Left(error))        =>
+             (acc, error.asLeft)
+
+           case (acc, Right((atom, arc))) =>
+             val arcConfig = arc.atom.steps.head.value
+
+             val past: MatchState[(Int, Option[D], List[(ProtoAtom[ProtoStep[D]], Long)])] =
+               (0, none[D], List.empty[(ProtoAtom[ProtoStep[D]], Long)]).pure[MatchState]
+
+             val current: MatchState[(Int, Option[D], List[(ProtoAtom[ProtoStep[D]], Long)])] =
+               Completion.SequenceMatch.matchCurrent(atom).map { vid =>
+                 val genAtom = Option.unless(vid.isDefined)(atom)
+                 val genArc  = Option.when(acc.shouldGenerateArc(arcConfig))(arc.atom)
+                 val atoms = arc.location match {
+                   case CalLocation.Before => genArc.tupleRight(acc.index + 1).toList ++ genAtom.tupleRight(acc.index + 2).toList
+                   case CalLocation.After  => genAtom.tupleRight(acc.index + 1).toList ++ genArc.tupleRight(acc.index + 2).toList
+                 }
+                 (1 + (if (acc.generatedArcs(arcConfig)) 0 else 1), arcConfig.some, atoms)
+               }
+
+             val update = for {
+               vid  <- Completion.SequenceMatch.matchPast(atom)
+               skip <- vid.fold(false.pure[MatchState])(Completion.SequenceMatch.contains(arc.atom)) // same visit must contain the arc
+               m    <- if (skip) past else current
+             } yield m
+
+             val (compʹ, (inc, genArc, steps)) = update.run(acc.comp).value
+             val accʹ = Accumulator(compʹ, acc.index + inc, acc.generatedArcs ++ genArc)
+
+             (accʹ, steps.asRight)
+         }
+         .flatMap {
+           case (_, Left(error))  => Stream.emit(Left(error))
+           case (_, Right(steps)) => Stream.emits(steps.map(_.asRight))
+         }
+      }
+
+
+    new Generator[F, S, D] {
+
+      def generate(
+        acquisitionItc: IntegrationTime,
+        scienceItc:     IntegrationTime,
+        completion:     Completion.Matcher[D]
+      ): Either[String, ProtoExecutionConfig[F, S, Either[String, (ProtoAtom[ProtoStep[D]], Long)]]] =
+        pureGen
+          .generate(acquisitionItc, scienceItc, config)
+          .map(p => p.mapSequences(defaultExpandAndFilter(expander, completion.acq), scienceSequence(completion.sci)))
+
+    }
+
+  case class GmosNorthGenerator[F[_]: Concurrent](
+    expander: SmartGcalExpander[F, DynamicConfig.GmosNorth]
+  ) {
+    def forConfig(c: Config.GmosNorth): Generator[F, StaticConfig.GmosNorth, DynamicConfig.GmosNorth] =
+      instantiate(PureGenerator.GmosNorth, expander, c)
+  }
+
+  case class GmosSouthGenerator[F[_]: Concurrent](
+    expander: SmartGcalExpander[F, DynamicConfig.GmosSouth]
+  ) {
+    def forConfig(c: Config.GmosSouth): Generator[F, StaticConfig.GmosSouth, DynamicConfig.GmosSouth] =
+      instantiate(PureGenerator.GmosSouth, expander, c)
+  }
+
+  def gmosNorth[F[_]: Concurrent](
+    expander: SmartGcalExpander[F, DynamicConfig.GmosNorth]
+  ): GmosNorthGenerator[F] =
+    GmosNorthGenerator(expander)
+
+  def gmosSouth[F[_]: Concurrent](
+    expander: SmartGcalExpander[F, DynamicConfig.GmosSouth]
+  ): GmosSouthGenerator[F] =
+    GmosSouthGenerator(expander)
+
 }
