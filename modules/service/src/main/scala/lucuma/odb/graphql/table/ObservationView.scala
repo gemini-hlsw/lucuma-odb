e5aa6052a10432560301e8bd023764803ad6fccb
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-07-04 17:16:46 -0400

    Switch observerNotes to NonEmptyString

--- modules/service/src/main/scala/lucuma/odb/graphql/table/ObservationView.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/table/ObservationView.scala
@@ -26,7 +26,7 @@ trait ObservationView[F[_]] extends BaseMapping[F] {
       val GroupId: ColumnRef           = col("c_group_id",           group_id.opt)
       val GroupIndex: ColumnRef        = col("c_group_index",        int2_nonneg)
       val CalibrationRole: ColumnRef   = col("c_calibration_role",   calibration_role.opt)
-      val ObserverNotes: ColumnRef     = col("c_observer_notes",     text.opt)
+      val ObserverNotes: ColumnRef     = col("c_observer_notes",     text_nonempty.opt)
 
       object PlannedTime {
         val Pi        = col("c_pts_pi", time_span)
