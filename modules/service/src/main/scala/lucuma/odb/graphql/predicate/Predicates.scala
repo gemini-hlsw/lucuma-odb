57b1875dea270b568baf3cc2d7479e12665661f0
Shane Walker <pistes.clary_0w@icloud.com>
2024-07-11 11:15:01 -0400

    cleanup

--- modules/service/src/main/scala/lucuma/odb/graphql/predicate/Predicates.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/predicate/Predicates.scala
@@ -43,7 +43,6 @@ trait Predicates[F[_]] extends BaseMapping[F] {
     val proposalAttachment            = ProposalAttachmentPredicates(Path.from(ProposalAttachmentType))
     val recordDatasetResult           = RecordDatasetResultPredicates(Path.from(RecordDatasetResultType))
     val sequenceEvent                 = ExecutionEventPredicates(Path.from(SequenceEventType))
-//    val setAllocationsResult          = SetAllocationsResultPredicates(Path.from(SetAllocationsResultType))
     val setProgramReferenceResult     = SetProgramReferenceResult(Path.from(SetProgramReferenceResultType))
     val setProposalStatusResult       = SetProposalStatusResultPredicates(Path.from(SetProposalStatusResultType))
     val slewEvent                     = ExecutionEventPredicates(Path.from(SlewEventType))
