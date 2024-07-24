83edc914dc2c8d7d1d44bd226a7bd43ad2b50fa0
Shane Walker <pistes.clary_0w@icloud.com>
2024-07-24 11:49:24 -0400

    use the cal role

--- modules/sequence/src/main/scala/lucuma/odb/sequence/data/GeneratorParams.scala
+++ modules/sequence/src/main/scala/lucuma/odb/sequence/data/GeneratorParams.scala
@@ -14,6 +14,7 @@ import lucuma.odb.data.CalibrationRole
 import lucuma.odb.sequence.ObservingMode
 import lucuma.odb.sequence.syntax.all.*
 import lucuma.odb.sequence.util.HashBytes
+
 import java.nio.charset.StandardCharsets.UTF_8
 
 case class GeneratorParams(
