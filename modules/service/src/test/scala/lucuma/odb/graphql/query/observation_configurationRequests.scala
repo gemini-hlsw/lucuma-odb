8194fa2318fdbb015c32f8d234253909e6b43a08
Rob Norris <rob_norris@mac.com>
2024-09-10 10:41:27 -0500

    tests

--- modules/service/src/test/scala/lucuma/odb/graphql/query/observation_configurationRequests.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/observation_configurationRequests.scala
@@ -11,36 +11,69 @@ import io.circe.syntax.*
 import lucuma.core.model.User
 import lucuma.odb.data.ConfigurationRequest
 import lucuma.core.model.Observation
+import lucuma.odb.graphql.mutation.UpdateConstraintSetOps
 
-class observation_configurationRequests extends OdbSuite with ObservingModeSetupOperations {
+class observation_configurationRequests 
+  extends OdbSuite 
+     with ObservingModeSetupOperations 
+     with UpdateConstraintSetOps {
 
   val pi       = TestUsers.Standard.pi(1, 30)
   val admin    = TestUsers.Standard.admin(2, 31)
   val validUsers = List(pi, admin)
 
-  test("create and select some configuration requests") {
-
-    def select(user: User, oid: Observation.Id, ids: List[ConfigurationRequest.Id]): IO[Unit] =
-      expect(
-        user = user,
-        query = s"""
-          query {
-            observation(observationId: "$oid") {
-              configurationRequests {
-                id
+  def setExplicitBaseAs(user: User, oid: Observation.Id, hms: String, dms: String): IO[Unit] =
+    updateObservation(
+      user = user, 
+      oid = oid,
+      update = s"""
+        targetEnvironment: {
+          explicitBase: {
+            ra: { hms: "$hms"},
+            dec: { dms: "$dms"}
+          }
+        }
+      """,
+      query = """
+        observations {
+          id
+        }
+      """,
+      expected = Right(json"""
+        {
+          "updateObservations": {
+            "observations": [
+              {
+                "id": $oid
               }
+            ]
+          }
+        }
+      """)
+    )
+
+  def expectRequests(user: User, oid: Observation.Id, ids: List[ConfigurationRequest.Id]): IO[Unit] =
+    expect(
+      user = user,
+      query = s"""
+        query {
+          observation(observationId: "$oid") {
+            configurationRequests {
+              id
             }
           }
-        """,
-        expected = Right(json"""                
-          {
-            "observation" : {
-              "configurationRequests" : ${ids.map(id => Json.obj("id" -> id.asJson))}
-            }
-          }                
-        """)
-      )
+        }
+      """,
+      expected = Right(json"""                
+        {
+          "observation" : {
+            "configurationRequests" : ${ids.map(id => Json.obj("id" -> id.asJson))}
+          }
+        }                
+      """)
+    )
 
+  test("create and select some configuration requests"):
     for
       cfpid <- createCallForProposalsAs(admin)
       pid   <- createProgramAs(pi)
@@ -48,9 +81,46 @@ class observation_configurationRequests extends OdbSuite with ObservingModeSetup
       tid   <- createTargetWithProfileAs(pi, pid)
       oid   <- createGmosNorthLongSlitObservationAs(pi, pid, List(tid))
       ids   <- createConfigurationRequestAs(pi, oid).replicateA(2)
-      _     <- select(pi, oid, ids)
+      _     <- expectRequests(pi, oid, ids)
     yield ()
 
-  }
+  test("configuration request made for one observation should apply to another identical observation"):
+    for
+      cfpid <- createCallForProposalsAs(admin)
+      pid   <- createProgramAs(pi)
+      _     <- addProposal(pi, pid, Some(cfpid), None, "Foo")
+      tid   <- createTargetWithProfileAs(pi, pid)
+      oid   <- createGmosNorthLongSlitObservationAs(pi, pid, List(tid))
+      rid   <- createConfigurationRequestAs(pi, oid)
+      oid2  <- cloneObservationAs(pi, oid)
+      _     <- expectRequests(pi, oid2, List(rid))
+    yield ()
+
+  test("configuration request made for one observation should not apply to an observation with a faraway base position"):
+    for
+      cfpid <- createCallForProposalsAs(admin)
+      pid   <- createProgramAs(pi)
+      _     <- addProposal(pi, pid, Some(cfpid), None, "Foo")
+      tid   <- createTargetWithProfileAs(pi, pid)
+      oid   <- createGmosNorthLongSlitObservationAs(pi, pid, List(tid))
+      rid   <- createConfigurationRequestAs(pi, oid)
+      oid2  <- cloneObservationAs(pi, oid)
+      _     <- setExplicitBaseAs(pi, oid2, "1:23:45", "6:01:23")
+      _     <- expectRequests(pi, oid2, Nil)
+    yield ()
+
+  test("configuration request made for one observation should apply to an observation with a nearby base position"):
+    for
+      cfpid <- createCallForProposalsAs(admin)
+      pid   <- createProgramAs(pi)
+      _     <- addProposal(pi, pid, Some(cfpid), None, "Foo")
+      tid   <- createTargetWithProfileAs(pi, pid)
+      oid1  <- createGmosNorthLongSlitObservationAs(pi, pid, List(tid))
+      _     <- setExplicitBaseAs(pi, oid1, "1:00:00", "2:00:00")
+      oid2  <- cloneObservationAs(pi, oid1)
+      _     <- setExplicitBaseAs(pi, oid2, "1:00:00.01", "2:00:00.01")
+      rid   <- createConfigurationRequestAs(pi, oid1)
+      _     <- expectRequests(pi, oid2, List(rid))
+    yield ()
 
 }
