a8fa9d6aa0107b583b1a33876d5edf58d2caaee6
Shane Walker <pistes.clary_0w@icloud.com>
2025-02-18 15:22:19 -0300

    Don't advance ids for opposite sequence type

--- modules/service/src/test/scala/lucuma/odb/graphql/query/executionSci.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/executionSci.scala
@@ -1265,4 +1265,54 @@ class executionSci extends ExecutionTestSupport {
           """.asRight
       )
     }
+
+  def firstAcquisitionStepId(p: Program.Id, o: Observation.Id): IO[Step.Id] =
+    import lucuma.odb.testsyntax.execution.*
+    generateOrFail(p, o, 5.some).map(_.gmosNorthAcquisition.nextAtom.steps.head.id)
+
+  test("acquisition step ids do not change while executing science"):
+    val execSci: IO[Set[Step.Id]] =
+      for
+        p <- createProgram
+        t <- createTargetWithProfileAs(pi, p)
+        o <- createGmosNorthLongSlitObservationAs(pi, p, List(t))
+        v <- recordVisitAs(serviceUser, Instrument.GmosNorth, o)
+
+        x0 <- firstAcquisitionStepId(p, o)
+
+        a0 <- recordAtomAs(serviceUser, Instrument.GmosNorth, v, SequenceType.Science)
+        s0 <- recordStepAs(serviceUser, a0, Instrument.GmosNorth, gmosNorthArc(0), ArcStep, gcalTelescopeConfig(0), ObserveClass.PartnerCal)
+        _  <- addEndStepEvent(s0)
+
+        x1 <- firstAcquisitionStepId(p, o)
+
+        s1 <- recordStepAs(serviceUser, a0, Instrument.GmosNorth, gmosNorthFlat(0), FlatStep, gcalTelescopeConfig(0), ObserveClass.PartnerCal)
+        _  <- addEndStepEvent(s1)
+
+        x2 <- firstAcquisitionStepId(p, o)
+
+        s2 <- recordStepAs(serviceUser, a0, Instrument.GmosNorth, gmosNorthScience(0), StepConfig.Science, sciTelescopeConfig(0), ObserveClass.Science)
+        _  <- addEndStepEvent(s2)
+
+        x3 <- firstAcquisitionStepId(p, o)
+
+        s3 <- recordStepAs(serviceUser, a0, Instrument.GmosNorth, gmosNorthScience(0), StepConfig.Science, sciTelescopeConfig(0), ObserveClass.Science)
+        _  <- addEndStepEvent(s3)
+
+        x4 <- firstAcquisitionStepId(p, o)
+
+        s4 <- recordStepAs(serviceUser, a0, Instrument.GmosNorth, gmosNorthScience(0), StepConfig.Science, sciTelescopeConfig(0), ObserveClass.Science)
+        _  <- addEndStepEvent(s4)
+
+        x5 <- firstAcquisitionStepId(p, o)
+
+        a1 <- recordAtomAs(serviceUser, Instrument.GmosNorth, v, SequenceType.Science)
+        s5 <- recordStepAs(serviceUser, a1, Instrument.GmosNorth, gmosNorthArc(5), ArcStep, gcalTelescopeConfig(0), ObserveClass.PartnerCal)
+        _  <- addEndStepEvent(s5)
+
+        x5 <- firstAcquisitionStepId(p, o)
+      yield Set(x0, x1, x2, x3, x4, x5)
+
+    assertIO(execSci.map(_.size), 1)
+
 }
