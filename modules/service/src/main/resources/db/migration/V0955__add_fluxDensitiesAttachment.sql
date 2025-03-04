0e233b1cc19377d95a3aefc3b21d5ecc9fe880de
Raúl Piaggio <rpiaggio@gemini.edu>
2025-03-04 09:19:58 -0300

    remove migration

--- modules/service/src/main/resources/db/migration/V0955__add_fluxDensitiesAttachment.sql
+++
@@ -1,3 +0,0 @@
--- Add fluxDensitiesAttachment to SEDs
-
-UPDATE t_target SET c_source_profile = REPLACE(c_source_profile::text, '"fluxDensities": null', '"fluxDensities": null, "fluxDensitiesAttachment": null')::jsonb;
