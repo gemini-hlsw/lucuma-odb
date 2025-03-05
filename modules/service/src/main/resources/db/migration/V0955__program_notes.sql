feea98a8c8fb06e29d5be24fc191db6ce40f0915
Shane Walker <pistes.clary_0w@icloud.com>
2025-03-05 13:15:44 -0300

    ProgramNote

---
+++ modules/service/src/main/resources/db/migration/V0955__program_notes.sql
@@ -0,0 +1,19 @@
+CREATE DOMAIN d_program_note_id AS varchar
+  CHECK (VALUE ~ '^n-[1-9a-f][0-9a-f]*$');
+COMMENT ON DOMAIN d_program_note_id IS 'GID type for program notes.'
+
+CREATE SEQUENCE s_program_note_id START WITH 256;
+
+CREATE TABLE t_program_note (
+
+  c_program_note_id d_program_note_id PRIMARY KEY DEFAULT 'n-' || to_hex(nextval('s_program_note_id')),
+  c_program_id      d_program_id      NOT NULL REFERENCES t_program(c_program_id) ON DELETE CASCADE,
+  c_existence       e_existence       NOT NULL DEFAULT 'present',
+  c_index           smallint          NOT NULL,
+
+  c_title           text              NOT NULL CHECK (length(c_title) > 0),
+  c_text            text              CHECK (c_text IS NULL OR length(c_text) > 0),
+  c_private         boolean           NOT NULL DEFAULT false,
+
+  UNIQUE (c_program_id, c_index)
+);
