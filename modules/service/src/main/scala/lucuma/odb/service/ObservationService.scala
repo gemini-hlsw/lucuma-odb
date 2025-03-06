06c918364c0fcfc55f16cbe52bdbc560be776d5d
Rob Norris <rob_norris@mac.com>
2025-03-06 11:22:16 -0600

    remove internal checks

--- modules/service/src/main/scala/lucuma/odb/service/ObservationService.scala
+++ modules/service/src/main/scala/lucuma/odb/service/ObservationService.scala
@@ -451,15 +451,10 @@ object ObservationService {
       )(using Transaction[F]): F[Result[(Program.Id, Observation.Id)]] = {
 
         // First we need the pid, observing mode, and grouping information
-        val selPid = sql"select c_program_id, c_observing_mode_type, c_group_id, c_group_index, c_calibration_role from t_observation where c_observation_id = $observation_id"
-        session.prepareR(selPid.query(program_id *: observing_mode_type.opt *: group_id.opt *: int2_nonneg *: calibration_role.opt)).use(_.option(observationId)).flatMap {
+        val selPid = sql"select c_program_id, c_observing_mode_type, c_group_id, c_group_index from t_observation where c_observation_id = $observation_id"
+        session.prepareR(selPid.query(program_id *: observing_mode_type.opt *: group_id.opt *: int2_nonneg)).use(_.unique(observationId)).flatMap {
 
-          case None => Result.failure(s"No such observation: $observationId").pure[F]
-
-          case Some((_, _, _, _, Some(_))) =>
-            Result.failure(s"Cannot clone calibration observations: $observationId").pure[F]
-
-          case Some((pid, observingMode, gid, gix, None)) =>
+          case (pid, observingMode, gid, gix, None) =>
 
             // Desired group index is gix + 1
             val destGroupIndex = NonNegShort.unsafeFrom((gix.value + 1).toShort)
