3c4131a2534ffa6358952a130a2611b6f0a8208c
Shane Walker <pistes.clary_0w@icloud.com>
2025-03-06 16:15:52 -0300

    remove user-settable ordering

--- modules/service/src/main/scala/lucuma/odb/graphql/table/ProgramNoteTable.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/table/ProgramNoteTable.scala
@@ -6,7 +6,6 @@ package table
 
 import lucuma.odb.util.Codecs.*
 import skunk.codec.boolean.bool
-import skunk.codec.numeric.int2
 
 trait ProgramNoteTable[F[_]] extends BaseMapping[F]:
 
@@ -14,7 +13,6 @@ trait ProgramNoteTable[F[_]] extends BaseMapping[F]:
     val Id                   = col("c_program_note_id", program_note_id)
     val ProgramId            = col("c_program_id", program_id)
     val Existence            = col("c_existence", existence)
-    val Index                = col("c_index", int2)
 
     val Title                = col("c_title", text_nonempty)
     val Text                 = col("c_text", text_nonempty.opt)
