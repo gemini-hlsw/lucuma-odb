3dc9f052b569c2a8d108a73b9dc08c7f9a6fbea8
Shane Walker <pistes.clary_0w@icloud.com>
2025-03-21 16:00:33 -0300

    Split migration

--- modules/service/src/main/resources/db/migration/V0971__no_partner_time_02.sql
+++ modules/service/src/main/resources/db/migration/V0971__no_partner_time_02.sql
@@ -1,35 +1,9 @@
-UPDATE t_visit
-  SET c_raw_program_time   = c_raw_program_time   + c_raw_partner_time,
-      c_final_program_time = c_final_program_time + c_final_partner_time;
-
-ALTER TABLE t_visit
-  DROP COLUMN c_raw_partner_time,
-  DROP COLUMN c_final_partner_time;
-
-UPDATE t_time_charge_discount
-  SET c_program_discount = c_program_discount + c_partner_discount;
-
-ALTER TABLE t_time_charge_discount
-  DROP COLUMN c_partner_discount;
-
-UPDATE t_execution_digest
-  SET c_acq_program_time = c_acq_program_time + c_acq_partner_time,
-      c_sci_program_time = c_sci_program_time + c_sci_partner_time;
-
-ALTER TABLE t_execution_digest
-  DROP COLUMN c_acq_partner_time,
-  DROP COLUMN c_sci_partner_time;
-
 -- Update the e_obs_class enumeration.  This is complicated because there is
 -- no direct support for dropping enum values.
 
 -- Drop this view because it references a column of type e_obs_class.  We'll
 -- recreate it in the end.
 DROP VIEW v_step_record;
-
--- Add the new `nightCal` value.  We'll change existing `partnerCal` and
--- `programCal` to `nightCal`.
-ALTER TYPE e_obs_class ADD VALUE 'nightCal' BEFORE 'acquisition';
 
 -- programCal -> nightCal
 
