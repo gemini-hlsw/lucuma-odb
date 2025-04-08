a6952db65ddde23211183c29426c96e17196da66
Rob Norris <rob_norris@mac.com>
2025-04-08 12:10:02 -0500

    fix

--- modules/service/src/test/scala/lucuma/odb/graphql/issue/shortcut/5331.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/issue/shortcut/5331.scala
@@ -8,92 +8,98 @@ import cats.syntax.all.*
 import io.circe.literal.*
 import lucuma.core.model.Observation
 import lucuma.odb.graphql.query.ExecutionTestSupport
+import lucuma.odb.graphql.binding.LongBinding
+import grackle.Value
+import grackle.Result
+import grackle.Problem
 
 class ShortCut_5331 extends ExecutionTestSupport:
   // Works up to 2147 seconds
   val secs: Long = 2148000000L
 
-  def updateExpTime(t: Observation.Id) = query(pi,
-    s"""
-      mutation {
-        updateObservations(input: {
-          SET: {
-            scienceRequirements: {
-              spectroscopy: {
-                exposureTimeMode: {
-                  timeAndCount: {
-                    time: {
-                      microseconds: $secs
-                    },
-                    count: 1,
-                    at: {
-                      nanometers: 500
-                    }
-                  }
-                }
-              }
-            }
-          }
-          WHERE: {
-            id: { EQ: "$t"}
-          }
-        }) {
-          observations {
-            id
-          }
-        }
-      }
-    """)
+  // RCN: the test below will never work because 2148000000 is syntactically invalid GraphQL
+
+  // def updateExpTime(t: Observation.Id) = query(pi,
+  //   s"""
+  //     mutation {
+  //       updateObservations(input: {
+  //         SET: {
+  //           scienceRequirements: {
+  //             spectroscopy: {
+  //               exposureTimeMode: {
+  //                 timeAndCount: {
+  //                   time: {
+  //                     microseconds: $secs
+  //                   },
+  //                   count: 1,
+  //                   at: {
+  //                     nanometers: 500
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //         WHERE: {
+  //           id: { EQ: "$t"}
+  //         }
+  //       }) {
+  //         observations {
+  //           id
+  //         }
+  //       }
+  //     }
+  //   """)
 
 
-  test("Support passing large exposure time values in graphql"):
-    val setup =
-      for {
-        p <- createProgram
-        t <- createTargetWithProfileAs(pi, p)
-        o <- createGmosNorthLongSlitObservationAs(pi, p, List(t))
-        _ <- updateExpTime(o)
-      } yield o
-    setup.flatMap { case oid =>
-      expect(
-        user  = pi,
-        query =
-          s"""
-             query {
-               observation(observationId: "$oid") {
-                 scienceRequirements {
-                   spectroscopy {
-                     exposureTimeMode {
-                       timeAndCount {
-                         time {
-                           microseconds
-                         }
-                       }
-                     }
-                   }
-                 }
-               }
-             }
-           """,
-            expected = Right(json"""
-              {
-                "observation" : {
-                  "scienceRequirements" : {
-                    "spectroscopy" : {
-                      "exposureTimeMode" : {
-                        "timeAndCount" : {
-                          "time" : {
-                            "microseconds" : $secs
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            """)
-      )
-    }
+  // test("Support passing large exposure time values in graphql"):
+  //   val setup =
+  //     for {
+  //       p <- createProgram
+  //       t <- createTargetWithProfileAs(pi, p)
+  //       o <- createGmosNorthLongSlitObservationAs(pi, p, List(t))
+  //       _ <- updateExpTime(o)
+  //     } yield o
+  //   setup.flatMap { case oid =>
+  //     expect(
+  //       user  = pi,
+  //       query =
+  //         s"""
+  //            query {
+  //              observation(observationId: "$oid") {
+  //                scienceRequirements {
+  //                  spectroscopy {
+  //                    exposureTimeMode {
+  //                      timeAndCount {
+  //                        time {
+  //                          microseconds
+  //                        }
+  //                      }
+  //                    }
+  //                  }
+  //                }
+  //              }
+  //            }
+  //          """,
+  //           expected = Right(json"""
+  //             {
+  //               "observation" : {
+  //                 "scienceRequirements" : {
+  //                   "spectroscopy" : {
+  //                     "exposureTimeMode" : {
+  //                       "timeAndCount" : {
+  //                         "time" : {
+  //                           "microseconds" : $secs
+  //                         }
+  //                       }
+  //                     }
+  //                   }
+  //                 }
+  //               }
+  //             }
+  //           """)
+  //     )
+  //   }
 
   def updateExpTimeWithVariables(t: Observation.Id) = query(pi,
     s"""
@@ -182,3 +188,34 @@ class ShortCut_5331 extends ExecutionTestSupport:
             """)
       )
     }
+
+
+  List(Long.MinValue, 0L, Long.MaxValue).foreach: long =>
+    test(s"LongBinding ($long)"):
+      assertEquals(
+        LongBinding.unapply(("test", Value.FloatValue(long.toDouble))),
+        Some(("test", Result(long)))
+      )
+    
+  test(s"LongBinding (1.23)"):
+    assertEquals(
+      LongBinding.unapply(("test", Value.FloatValue(1.23))),
+      Some(("test", Result.failure(
+        Problem(
+          "Argument 'test' is invalid: Expected Long, got FloatValue(1.23)",
+          Nil,
+          Nil,
+          Some(
+            json"""
+              {
+                "odb_error" : {
+                  "odb_error.tag" : "invalid_argument",
+                  "odb_error.detail" : "Argument 'test' is invalid: Expected Long, got FloatValue(1.23)",
+                  "odb_error.data" : { }
+                }
+              }
+            """.asObject.get
+          )
+        )
+      )))
+    )
