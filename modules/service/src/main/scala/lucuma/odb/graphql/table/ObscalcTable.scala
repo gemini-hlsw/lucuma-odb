4ffb10f37a4fdbbc9e0e1322252173762e114353
Shane Walker <pistes.clary_0w@icloud.com>
2025-06-09 10:02:22 -0400

    cleanup

--- modules/service/src/main/scala/lucuma/odb/graphql/table/ObscalcTable.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/table/ObscalcTable.scala
@@ -6,7 +6,6 @@ package table
 
 import lucuma.odb.util.Codecs.*
 import skunk.circe.codec.all.*
-import skunk.codec.numeric._int8
 
 trait ObscalcTable[F[_]] extends BaseMapping[F]:
 
@@ -19,23 +18,6 @@ trait ObscalcTable[F[_]] extends BaseMapping[F]:
     val RetryAt: ColumnRef             = col("c_retry_at",             core_timestamp)
     val FailureCount: ColumnRef        = col("c_failure_count",        int4_nonneg)
     val OdbError: ColumnRef            = col("c_odb_error",            jsonb.opt)
-
-    object Digest:
-      val Id: ColumnRef                = col("c_exe_digest_id",        observation_id.embedded)
-      val FullSetupTime: ColumnRef     = col("c_full_setup_time",      time_span.embedded)
-      val ReacqSetupTime: ColumnRef    = col("c_reacq_setup_time",     time_span.embedded)
-
-      case class SequenceDigest(abbr: String):
-        val Id: ColumnRef              = col(s"c_${abbr}_digest_id",        observation_id.embedded)
-        val ObsClass: ColumnRef        = col(s"c_${abbr}_obs_class",        obs_class.embedded)
-        val NonChargedTime: ColumnRef  = col(s"c_${abbr}_non_charged_time", time_span.embedded)
-        val ProgramTime: ColumnRef     = col(s"c_${abbr}_program_time",     time_span.embedded)
-        val Offsets: ColumnRef         = col(s"c_${abbr}_offsets",          _int8.embedded)
-        val AtomCount: ColumnRef       = col(s"c_${abbr}_atom_count",       int4_nonneg.embedded)
-        val ExecutionState: ColumnRef  = col(s"c_${abbr}_execution_state",  execution_state.embedded)
-
-      val Acquisition: SequenceDigest  = SequenceDigest("acq")
-      val Science: SequenceDigest      = SequenceDigest("sci")
 
     object Workflow:
       val State: ColumnRef             = col("c_workflow_state",       observation_workflow_state)
