703fa6d4699205fb9da04564cc032c4539ab6e28
Shane Walker <pistes.clary_0w@icloud.com>
2025-06-10 08:51:28 -0400

    record visits and sequence events

--- modules/sequence/src/main/scala/lucuma/odb/sequence/ExecutionConfigGenerator.scala
+++ modules/sequence/src/main/scala/lucuma/odb/sequence/ExecutionConfigGenerator.scala
@@ -8,10 +8,14 @@ import cats.effect.Concurrent
 import cats.syntax.all.*
 import fs2.Stream
 import lucuma.core.enums.ExecutionState
+import lucuma.core.model.ExecutionEvent.SequenceEvent
 import lucuma.core.model.sequence.Atom
 import lucuma.core.util.Timestamp
+import lucuma.odb.data.OneOf3
 import lucuma.odb.sequence.data.ProtoExecutionConfig
 import lucuma.odb.sequence.data.StepRecord
+import lucuma.odb.sequence.data.VisitRecord
+import lucuma.odb.sequence.util.merge3ByTimestamp
 
 /**
  * Combines the static configuration with generators for the acquisition and
@@ -34,13 +38,16 @@ case class ExecutionConfigGenerator[S, D](
    *             calibration files are only considered valid for a fixed time
    */
   def executionConfig[F[_]: Concurrent](
-    steps: Stream[F, StepRecord[D]],
-    when:  Timestamp
+    visits: Stream[F, VisitRecord],
+    events: Stream[F, SequenceEvent],
+    steps:  Stream[F, StepRecord[D]],
+    when:   Timestamp
   )(using Eq[D]): F[(ProtoExecutionConfig[S, Atom[D]], ExecutionState)] =
-    steps
-      .fold((acquisition, science, ExecutionState.NotStarted)) { case ((a, s, e), step) =>
-        (a.recordStep(step), s.recordStep(step), ExecutionState.Ongoing)
-      }
+    merge3ByTimestamp(visits, events, steps)(_.created, _.received, _.created)
+      .fold((acquisition, science, ExecutionState.NotStarted)):
+        case ((a, s, _), OneOf3.First(visit))  => (a.recordVisit(visit), s.recordVisit(visit), ExecutionState.Ongoing)
+        case ((a, s, _), OneOf3.Second(event)) => (a.recordSequenceEvent(event), s.recordSequenceEvent(event), ExecutionState.Ongoing)
+        case ((a, s, _), OneOf3.Third(step))   => (a.recordStep(step), s.recordStep(step), ExecutionState.Ongoing)
       .compile
       .onlyOrError
       .map: (a, s, e) =>
