e56976d7806116de745c085b3c7dc13347b45587
Shane Walker <pistes.clary_0w@icloud.com>
2025-07-01 11:25:34 -0400

    20 x 5 min

--- modules/service/src/test/scala/lucuma/odb/graphql/query/executionSciFlamingos2.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/executionSciFlamingos2.scala
@@ -6,6 +6,7 @@ package lucuma.odb.graphql.query
 import cats.effect.IO
 import cats.syntax.either.*
 import cats.syntax.option.*
+import eu.timepit.refined.types.numeric.NonNegInt
 import io.circe.Json
 import io.circe.syntax.*
 import lucuma.core.enums.DatasetQaState
@@ -14,9 +15,16 @@ import lucuma.core.enums.ObserveClass
 import lucuma.core.enums.SequenceType
 import lucuma.core.model.Observation
 import lucuma.core.model.sequence.StepConfig
+import lucuma.core.syntax.timespan.*
+import lucuma.core.util.TimeSpan
+import lucuma.itc.IntegrationTime
 import lucuma.odb.json.all.transport.given
 
 class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
+  val ExposureTime: TimeSpan = 20.secondTimeSpan
+
+  override def fakeItcSpectroscopyResult: IntegrationTime =
+    IntegrationTime(ExposureTime, NonNegInt.unsafeFrom(4))
 
   test("simple generation - limited future"):
     val setup: IO[Observation.Id] =
@@ -44,7 +52,7 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
                 "config" -> Json.obj(
                   "flamingos2" -> Json.obj(
                     "science" -> Json.obj(
-                      "nextAtom" -> flamingos2ExpectedScienceAtom((0, 15), (0, -15), (0, -15), (0, 15)),
+                      "nextAtom" -> flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
                       "possibleFuture" -> List(flamingos2ExpectedGcals((0, 15))).asJson,
                       "hasMore" -> false.asJson
                     )
@@ -63,7 +71,7 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
         o <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
         v <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
         a <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
-        s <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _ <- addEndStepEvent(s)
       yield o
 
@@ -85,15 +93,7 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
                 "config" -> Json.obj(
                   "flamingos2" -> Json.obj(
                     "science" -> Json.obj(
-                      "nextAtom" -> Json.obj(
-                        "description" -> s"ABBA Cycle".asJson,
-                        "observeClass" -> "SCIENCE".asJson,
-                        "steps" -> List(
-                          flamingos2ExpectedScience(0, -15),
-                          flamingos2ExpectedScience(0, -15),
-                          flamingos2ExpectedScience(0,  15)
-                        ).asJson
-                      ),
+                      "nextAtom" -> flamingos2ExpectedScienceAtom(ExposureTime, (0, -15), (0, -15), (0, 15)),
                       "possibleFuture" -> List(flamingos2ExpectedGcals((0, 15))).asJson,
                       "hasMore" -> false.asJson
                     )
@@ -112,13 +112,13 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
         o  <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
         v  <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
         a  <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
-        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s0)
-        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s1)
-        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s2)
-        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s3)
       yield o
 
@@ -159,13 +159,13 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
         o  <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
         v  <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
         a  <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
-        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s0)
-        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s1)
-        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s2)
-        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s3)
         s4 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Arc, Flamingos2ArcStep, gcalTelescopeConfig(15), ObserveClass.NightCal)
         _  <- addEndStepEvent(s4)
@@ -206,17 +206,17 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
         o  <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
         v  <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
         a  <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
-        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s0)
-        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s1)
         d  <- recordDatasetAs(serviceUser, s1, "N20240905S1000.fits")
         _  <- setQaState(d, DatasetQaState.Usable)
-        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s2)
-        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s3)
-        s4 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s4 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s4)
       yield o
 
@@ -257,15 +257,15 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
         o  <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
         v  <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
         a  <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
-        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s0 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s0)
-        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s1 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s1)
         d  <- recordDatasetAs(serviceUser, s1, "N20240905S1001.fits")
         _  <- setQaState(d, DatasetQaState.Usable)
-        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
+        s2 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(-15), ObserveClass.Science)
         _  <- addEndStepEvent(s2)
-        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, Flamingos2Science, StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        s3 <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
         _  <- addEndStepEvent(s3)
       yield o
 
@@ -287,15 +287,7 @@ class executionSciFlamingos2 extends ExecutionTestSupportForFlamingos2:
                 "config" -> Json.obj(
                   "flamingos2" -> Json.obj(
                     "science" -> Json.obj(
-                      "nextAtom" -> Json.obj(
-                        "description" -> s"ABBA Cycle".asJson,
-                        "observeClass" -> "SCIENCE".asJson,
-                        "steps" -> List(
-                          flamingos2ExpectedScience(0, -15),
-                          flamingos2ExpectedScience(0, -15),
-                          flamingos2ExpectedScience(0,  15)
-                        ).asJson
-                      ),
+                      "nextAtom" -> flamingos2ExpectedScienceAtom(ExposureTime, (0, -15), (0, -15), (0, 15)),
                       "possibleFuture" -> List(flamingos2ExpectedGcals((0, 15))).asJson,
                       "hasMore" -> false.asJson
                     )
---
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/executionSciFlamingos2_20Steps5MinExposures.scala
@@ -0,0 +1,127 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql.query
+
+import cats.effect.IO
+import cats.syntax.either.*
+import cats.syntax.option.*
+import eu.timepit.refined.types.numeric.NonNegInt
+import io.circe.Json
+import io.circe.syntax.*
+import lucuma.core.enums.Instrument
+import lucuma.core.enums.ObserveClass
+import lucuma.core.enums.SequenceCommand
+import lucuma.core.enums.SequenceType
+import lucuma.core.model.Observation
+import lucuma.core.model.sequence.StepConfig
+import lucuma.core.syntax.timespan.*
+import lucuma.core.util.TimeSpan
+import lucuma.itc.IntegrationTime
+import lucuma.odb.json.all.transport.given
+
+// Match an example in the F2 Long Slit requirements spreadsheet
+class executionSciFlamingos2_20Steps5MinExposures extends ExecutionTestSupportForFlamingos2:
+
+  val ExposureTime: TimeSpan = 5.minuteTimeSpan
+
+  override def fakeItcSpectroscopyResult: IntegrationTime =
+    IntegrationTime(ExposureTime, NonNegInt.unsafeFrom(20))
+
+  test("simple generation"):
+    val setup: IO[Observation.Id] =
+      for
+        p <- createProgram
+        t <- createTargetWithProfileAs(pi, p)
+        o <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
+      yield o
+
+    setup.flatMap: oid =>
+      expect(
+        user  = pi,
+        query =
+          s"""
+             query {
+               observation(observationId: "$oid") {
+                 ${flamingos2ScienceQuery(none)}
+               }
+             }
+           """,
+        expected =
+          Json.obj(
+            "observation" -> Json.obj(
+              "execution" -> Json.obj(
+                "config" -> Json.obj(
+                  "flamingos2" -> Json.obj(
+                    "science" -> Json.obj(
+                      "nextAtom" -> flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                      "possibleFuture" -> List(
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedGcals((0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedGcals((0, 15))
+                      ).asJson,
+                      "hasMore" -> false.asJson
+                    )
+                  )
+                )
+              )
+            )
+          ).asRight
+      )
+
+  test("stop after first step"):
+    val setup: IO[Observation.Id] =
+      for
+        p <- createProgram
+        t <- createTargetWithProfileAs(pi, p)
+        o <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
+        v <- recordVisitAs(serviceUser, Instrument.Flamingos2, o)
+        a <- recordAtomAs(serviceUser, Instrument.Flamingos2, v, SequenceType.Science)
+        s <- recordStepAs(serviceUser, a, Instrument.Flamingos2, flamingos2Science(ExposureTime), StepConfig.Science, sciTelescopeConfig(15), ObserveClass.Science)
+        _ <- addEndStepEvent(s)
+        _ <- addSequenceEventAs(serviceUser, v, SequenceCommand.Stop)
+      yield o
+
+    setup.flatMap: oid =>
+      expect(
+        user  = pi,
+        query =
+          s"""
+             query {
+               observation(observationId: "$oid") {
+                 ${flamingos2ScienceQuery(none)}
+               }
+             }
+           """,
+        expected =
+          Json.obj(
+            "observation" -> Json.obj(
+              "execution" -> Json.obj(
+                "config" -> Json.obj(
+                  "flamingos2" -> Json.obj(
+                    "science" -> Json.obj(
+                      "nextAtom" -> flamingos2ExpectedScienceAtom(ExposureTime, (0, -15), (0, -15), (0, 15)),
+                      "possibleFuture" -> List(
+                        // Gcals to finish up the "stopped" block
+                        flamingos2ExpectedGcals((0, 15)),
+
+                        // New block.  In theory the operator or scheduler will
+                        // stop after the gcals.  At any rate, only 80 minutes
+                        // remaining so no mid-period calibration
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedScienceAtom(ExposureTime, (0, 15), (0, -15), (0, -15), (0, 15)),
+                        flamingos2ExpectedGcals((0, 15))
+                      ).asJson,
+                      "hasMore" -> false.asJson
+                    )
+                  )
+                )
+              )
+            )
+          ).asRight
+      )
