139f67e1a6c10265aa1a570f067af434cc168150
Shane Walker <pistes.clary_0w@icloud.com>
2025-07-08 11:52:24 -0400

    sky subtraction

---
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/executionAcqFlamingos2_WithSkySubtraction.scala
@@ -0,0 +1,81 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql.query
+
+import cats.effect.IO
+import cats.syntax.either.*
+import eu.timepit.refined.types.numeric.NonNegInt
+import io.circe.Json
+import io.circe.literal.*
+import lucuma.core.enums.Breakpoint
+import lucuma.core.model.Observation
+import lucuma.core.syntax.timespan.*
+import lucuma.core.util.TimeSpan
+import lucuma.itc.IntegrationTime
+
+class executionAcqFlamingos2_WithSkySubtraction extends ExecutionTestSupportForFlamingos2:
+
+  val ExposureTime: TimeSpan = 3.secTimeSpan
+
+  override def fakeItcImagingResult: IntegrationTime =
+    IntegrationTime(ExposureTime, NonNegInt.unsafeFrom(1))
+
+  val InitialAcquisition: Json =
+    json"""
+      {
+        "observation": {
+          "execution": {
+            "config": {
+              "flamingos2": {
+                "acquisition": {
+                  "nextAtom": {
+                    "description": "Initial Acquisition",
+                    "observeClass": "ACQUISITION",
+                    "steps": [
+                      ${flamingos2ExpectedAcq(Flamingos2AcqImage, ExposureTime,    0, 10)},
+                      ${flamingos2ExpectedAcq(Flamingos2AcqImage, ExposureTime,    0,  0)},
+                      ${flamingos2ExpectedAcq(Flamingos2AcqSlit,  10.secTimeSpan, 10,  0)},
+                      ${flamingos2ExpectedAcq(Flamingos2AcqSlit,  ExposureTime,    0, 10)},
+                      ${flamingos2ExpectedAcq(Flamingos2AcqSlit,  ExposureTime,    0,  0, Breakpoint.Enabled)}
+                    ]
+                  },
+                  "possibleFuture": [
+                    {
+                      "description": "Fine Adjustments",
+                      "observeClass": "ACQUISITION",
+                      "steps": [
+                        ${flamingos2ExpectedAcq(Flamingos2AcqSlit, ExposureTime, 0, 0)}
+                      ]
+                    }
+                  ],
+                  "hasMore": false
+                }
+              }
+            }
+          }
+        }
+      }
+    """
+
+  test("initial generation"):
+    val setup: IO[Observation.Id] =
+      for
+        p <- createProgram
+        t <- createTargetWithProfileAs(pi, p)
+        o <- createFlamingos2LongSlitObservationAs(pi, p, List(t))
+      yield o
+
+    setup.flatMap: oid =>
+      expect(
+        user  = pi,
+        query =
+          s"""
+             query {
+               observation(observationId: "$oid") {
+                 ${flamingos2AcquisitionQuery(None)}
+               }
+             }
+           """,
+        expected = InitialAcquisition.asRight
+      )
