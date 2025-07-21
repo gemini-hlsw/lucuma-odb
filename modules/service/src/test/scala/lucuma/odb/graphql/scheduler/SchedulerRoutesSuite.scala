98a051111711b6dea5506ee85d1933c50dc16bf7
Shane Walker <pistes.clary_0w@icloud.com>
2025-07-21 11:23:27 -0400

    Test valid observations

--- modules/service/src/test/scala/lucuma/odb/graphql/scheduler/SchedulerRoutesSuite.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/scheduler/SchedulerRoutesSuite.scala
@@ -4,19 +4,23 @@
 package lucuma.odb.graphql
 package scheduler
 
+import cats.Order.catsKernelOrderingForOrder
 import cats.effect.IO
 import cats.syntax.all.*
 import fs2.Stream
 import fs2.text
+import lucuma.core.model.Observation
 import lucuma.core.model.User
+import lucuma.core.model.sequence.Atom
+import lucuma.core.model.sequence.AtomDigest
+import lucuma.core.syntax.string.*
+import lucuma.core.util.Gid
+import lucuma.core.util.Uid
 import org.http4s.*
 import org.http4s.implicits.*
+import scala.jdk.CollectionConverters._
 
 abstract class SchedulerRoutesSuite extends OdbSuite:
-
-  val pi          = TestUsers.Standard.pi(1, 30)
-  val serviceUser = TestUsers.service(2)
-  val validUsers  = List(pi, serviceUser)
 
   protected  def responseStream(s: String): Stream[IO, Byte] =
     Stream[IO, String](s).through(text.utf8.encode)
@@ -32,6 +36,16 @@ abstract class SchedulerRoutesSuite extends OdbSuite:
         body    = Stream.emits(oids.toList).intersperse("\n").through(fs2.text.utf8.encode)
       )
 
+  extension (a: AtomDigest)
+    def fields: List[String] =
+      List(
+        Uid[Atom.Id].show(a.id),
+        a.observeClass.tag.toScreamingSnakeCase,
+        a.timeEstimate.sum.toMicroseconds.toString,
+        a.stepTypes.toList.sorted.map(_.tag.toScreamingSnakeCase).mkString("[", ", ", "]"),
+        a.lampTypes.toList.sorted.map(_.tag.toScreamingSnakeCase).mkString("[", ", ", "]")
+      )
+
   extension (resp: IO[Response[IO]])
     def assertResponse(expectedStatus: Status, expectedBody: Option[String]): IO[Unit] =
       for
@@ -41,3 +55,22 @@ abstract class SchedulerRoutesSuite extends OdbSuite:
         val statusCheck = actual.status === expectedStatus
         val bodyCheck   = expectedBody.fold(body.isEmpty)(_ === body)
         assert(statusCheck && bodyCheck, s"Expected '$expectedStatus:$expectedBody' Actual: '${actual.status}:$body'")
+
+    def assertAtoms(expectedStatus: Status, expectedDigests: List[(Observation.Id, List[AtomDigest])]): IO[Unit] =
+      for
+        actual <- resp
+        body   <- actual.as[String]
+      yield
+        val actualLines = body.lines.toList.asScala.toList.map: line =>
+          val fields = line.split('\t').toList
+          fields.take(2) ++ fields.drop(3)
+        val expectedLines = expectedDigests.flatMap: (oid, atoms) =>
+          atoms.zipWithIndex.map: (a, idx) =>
+            Gid[Observation.Id].show(oid) :: idx.toString :: a.fields.drop(1)
+
+        def formatLines(lines: List[List[String]]): String =
+          lines.map(_.mkString("\t")).mkString("\n")
+
+        val statusCheck = actual.status === expectedStatus
+        val bodyCheck   = actualLines === expectedLines
+        assert(statusCheck && bodyCheck, s"Expected '$expectedStatus':\n${formatLines(expectedLines)}\nActual: '${actual.status}':\n${formatLines(actualLines)}")
