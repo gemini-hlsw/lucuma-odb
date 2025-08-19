47ef362cf4b5feb70845c349224fb8655ff65078
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-08-19 16:56:39 -0400

    cleanup

--- docker-init/03-migrate-odb.sh
+++ docker-init/03-migrate-odb.sh
@@ -7,14 +7,12 @@ echo "Applying ODB migrations to lucuma-odb database..."
 ODB_MIGRATIONS_DIR="/docker-entrypoint-initdb.d/odb-migrations"
 
 if [ -d "$ODB_MIGRATIONS_DIR" ]; then
-    for migration_file in "$ODB_MIGRATIONS_DIR"/*.sql; do
+    # Apply migrations in version order
+    for migration_file in $(ls "$ODB_MIGRATIONS_DIR"/V*.sql | sort -V); do
         if [ -f "$migration_file" ]; then
-            echo "Applying $(basename "$migration_file") to lucuma-odb"
             psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "lucuma-odb" < "$migration_file"
         fi
     done
 else
     echo "Warning: ODB migrations directory $ODB_MIGRATIONS_DIR does not exist"
 fi
-
-echo "ODB migration complete."
