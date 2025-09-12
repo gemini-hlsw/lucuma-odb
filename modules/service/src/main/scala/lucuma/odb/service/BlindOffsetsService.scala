9f94cab27148e25ede262907e4d0b87a636e8fad
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-09-12 10:37:28 -0300

    Make a separate service for BlindOffsets

---
+++ modules/service/src/main/scala/lucuma/odb/service/BlindOffsetsService.scala
@@ -0,0 +1,152 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.service
+
+import cats.effect.Concurrent
+import cats.syntax.all.*
+import grackle.Result
+import lucuma.core.model.Observation
+import lucuma.core.model.Program
+import lucuma.core.model.Target
+import lucuma.odb.data.Nullable
+import lucuma.odb.data.Nullable.NonNull
+import lucuma.odb.graphql.input.TargetEnvironmentInput
+import lucuma.odb.graphql.input.TargetPropertiesInput
+import lucuma.odb.graphql.mapping.AccessControl
+import lucuma.odb.service.Services.ServiceAccess
+import lucuma.odb.util.Codecs.*
+import skunk.*
+import skunk.codec.boolean.bool
+import skunk.implicits.*
+
+sealed trait BlindOffsetsService[F[_]] {
+
+  def handleBlindOffsetTarget(
+    programId: Program.Id,
+    observationId: Observation.Id,
+    input: TargetPropertiesInput.Create
+  )(using Transaction[F], ServiceAccess): F[Result[Target.Id]]
+
+  def removeBlindOffsetTarget(
+    observationId: Observation.Id
+  )(using Transaction[F], ServiceAccess): F[Result[Unit]]
+
+  def getCurrentBlindOffsetTarget(
+    observationId: Observation.Id
+  )(using Transaction[F]): F[Option[Target.Id]]
+
+  def handleBlindOffsetTargetUpdates(
+    observationId: Observation.Id,
+    targetEnvironment: Option[TargetEnvironmentInput.Edit]
+  )(using Transaction[F], ServiceAccess): F[Result[Unit]]
+
+}
+
+object BlindOffsetsService {
+
+  def instantiate[F[_]: Concurrent](using Services[F]): BlindOffsetsService[F] =
+    new BlindOffsetsService[F] {
+
+      import Services.Syntax.*
+
+      override def handleBlindOffsetTarget(
+        programId: Program.Id,
+        observationId: Observation.Id,
+        input: TargetPropertiesInput.Create
+      )(using Transaction[F], ServiceAccess): F[Result[Target.Id]] =
+        Services.asSuperUser:
+          targetService.createTarget(AccessControl.unchecked(input, programId, program_id))
+            .flatMap: rTargetId =>
+              rTargetId.traverse: targetId =>
+                for {
+                  _ <- session.execute(Statements.addBlindOffsetToAsterismAndSetDisposition)(programId, observationId, targetId, targetId)
+                  _ <- session.execute(Statements.updateBlindOffsetFlag)(true, observationId)
+                } yield targetId
+
+      override def removeBlindOffsetTarget(
+        observationId: Observation.Id
+      )(using Transaction[F], ServiceAccess): F[Result[Unit]] =
+        for {
+          _ <- session.execute(Statements.removeBlindOffsetFromAsterism)(observationId)
+        } yield Result.unit
+
+      override def getCurrentBlindOffsetTarget(
+        observationId: Observation.Id
+      )(using Transaction[F]): F[Option[Target.Id]] =
+        session.option(Statements.getCurrentBlindOffsetTargetFromAsterism)(observationId)
+
+      override def handleBlindOffsetTargetUpdates(
+        observationId: Observation.Id,
+        targetEnvironment: Option[TargetEnvironmentInput.Edit]
+      )(using Transaction[F], ServiceAccess): F[Result[Unit]] =
+        targetEnvironment.fold(Result.unit.pure[F]): te =>
+          te.blindOffsetTarget match
+            case Nullable.Absent => Result.unit.pure[F] // No change
+            case Nullable.Null =>
+              // Remove blind offset target and set acquisition reset time
+              for {
+                _ <- removeBlindOffsetTarget(observationId)
+                _ <- session.execute(Statements.setAcquisitionResetTime)(observationId)
+              } yield Result.unit
+            case NonNull(targetInput: TargetPropertiesInput.Create) =>
+              // Handle blind offset target creation/replacement via update
+              for {
+                // First remove any existing blind offset target
+                _ <- removeBlindOffsetTarget(observationId)
+                // Then create the new one
+                rProgramId <- observationService.selectProgram(observationId)
+                result <- rProgramId.flatTraverse: programId =>
+                  handleBlindOffsetTarget(programId, observationId, targetInput).map(_.void)
+              } yield result
+
+      private object Statements {
+
+        val addBlindOffsetToAsterismAndSetDisposition: Command[Program.Id *: Observation.Id *: Target.Id *: Target.Id *: EmptyTuple] =
+          sql"""
+            WITH asterism_insert AS (
+              INSERT INTO t_asterism_target (c_program_id, c_observation_id, c_target_id)
+              VALUES ($program_id, $observation_id, $target_id)
+              ON CONFLICT (c_observation_id, c_target_id) DO NOTHING
+              RETURNING c_target_id
+            )
+            UPDATE t_target
+            SET c_target_disposition = 'blind_offset'
+            WHERE c_target_id = $target_id
+          """.command
+
+        val removeBlindOffsetFromAsterism: Command[Observation.Id] =
+          sql"""
+            DELETE FROM t_asterism_target
+            WHERE c_observation_id = $observation_id
+            AND c_target_id IN (
+              SELECT c_target_id FROM t_target
+              WHERE c_target_disposition = 'blind_offset'
+            )
+          """.command
+
+        val getCurrentBlindOffsetTargetFromAsterism: Query[Observation.Id, Target.Id] =
+          sql"""
+            SELECT a.c_target_id
+            FROM t_asterism_target a
+            JOIN t_target t ON a.c_target_id = t.c_target_id
+            WHERE a.c_observation_id = $observation_id
+            AND t.c_target_disposition = 'blind_offset'
+          """.query(target_id)
+
+        val updateBlindOffsetFlag: Command[Boolean *: Observation.Id *: EmptyTuple] =
+          sql"""
+            UPDATE t_observation
+            SET c_use_blind_offset = ${bool}
+            WHERE c_observation_id = $observation_id
+          """.command
+
+        val setAcquisitionResetTime: Command[Observation.Id] =
+          sql"""
+            UPDATE t_observation
+            SET c_acq_reset_time = now()
+            WHERE c_observation_id = $observation_id
+          """.command
+      }
+    }
+}
