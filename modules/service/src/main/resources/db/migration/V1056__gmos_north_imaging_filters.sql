107fcc0eb021582af6d6a1d9aad9ae21bcc2938d
Shane Walker <pistes.clary_0w@icloud.com>
2025-11-10 10:43:16 -0300

    move last filter table update to separate transaction / migration script

--- modules/service/src/main/resources/db/migration/V1056__gmos_north_imaging_filters.sql
+++ modules/service/src/main/resources/db/migration/V1056__gmos_north_imaging_filters.sql
@@ -77,13 +77,6 @@ BEGIN
   END LOOP;
 END $$;
 
--- Going forward, we want every imaging filter to be associated with a unique
--- science ETM.
-ALTER TABLE t_gmos_north_imaging_filter
-  ALTER COLUMN c_exposure_time_mode_id SET NOT NULL,
-  ADD CONSTRAINT t_gmos_north_imaging_filter_unique_exposure_time_mode_id
-    UNIQUE (c_exposure_time_mode_id);
-
 DROP VIEW v_gmos_north_imaging;
 DROP VIEW v_gmos_north_imaging_filter;
 DROP VIEW v_gmos_north_imaging_initial_filter;
