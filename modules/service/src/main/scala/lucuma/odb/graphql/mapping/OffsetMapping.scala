0bca04dba832b3740a55eb195558fcab2a5224e1
Shane Walker <pistes.clary_0w@icloud.com>
2025-12-10 21:43:42 -0300

    filters out of variant, interleaved offsets

--- modules/service/src/main/scala/lucuma/odb/graphql/mapping/OffsetMapping.scala
+++ modules/service/src/main/scala/lucuma/odb/graphql/mapping/OffsetMapping.scala
@@ -5,7 +5,6 @@ package lucuma.odb.graphql
 package mapping
 
 import grackle.Path
-import grackle.TypeRef
 import lucuma.core.math.Angle
 
 import table.EnumeratedOffsetView
@@ -48,27 +47,27 @@ trait OffsetMapping[F[_]] extends EnumeratedOffsetView[F]
   private lazy val StepRecordPath: Path = StepRecordType / "telescopeConfig" / "offset"
 
   private def preImagingMappings(
-    t: TypeRef,
+    p: Path,
     c: GmosImagingCommonColumns
   ): List[TypeMapping] =
     List(
-      offsetMappingAtPath(t / "offset1", c.PreImaging.ObservationId),
-      offsetComponentMappingAtPath(t / "offset1" / "p", c.PreImaging.Offset1P),
-      offsetComponentMappingAtPath(t / "offset1" / "q", c.PreImaging.Offset1Q),
-      offsetMappingAtPath(t / "offset2", c.PreImaging.ObservationId),
-      offsetComponentMappingAtPath(t / "offset2" / "p", c.PreImaging.Offset2P),
-      offsetComponentMappingAtPath(t / "offset2" / "q", c.PreImaging.Offset2Q),
-      offsetMappingAtPath(t / "offset3", c.PreImaging.ObservationId),
-      offsetComponentMappingAtPath(t / "offset3" / "p", c.PreImaging.Offset3P),
-      offsetComponentMappingAtPath(t / "offset3" / "q", c.PreImaging.Offset3Q),
-      offsetMappingAtPath(t / "offset4", c.PreImaging.ObservationId),
-      offsetComponentMappingAtPath(t / "offset4" / "p", c.PreImaging.Offset4P),
-      offsetComponentMappingAtPath(t / "offset4" / "q", c.PreImaging.Offset4Q),
+      offsetMappingAtPath(p / "offset1", c.PreImaging.ObservationId),
+      offsetComponentMappingAtPath(p / "offset1" / "p", c.PreImaging.Offset1P),
+      offsetComponentMappingAtPath(p / "offset1" / "q", c.PreImaging.Offset1Q),
+      offsetMappingAtPath(p / "offset2", c.PreImaging.ObservationId),
+      offsetComponentMappingAtPath(p / "offset2" / "p", c.PreImaging.Offset2P),
+      offsetComponentMappingAtPath(p / "offset2" / "q", c.PreImaging.Offset2Q),
+      offsetMappingAtPath(p / "offset3", c.PreImaging.ObservationId),
+      offsetComponentMappingAtPath(p / "offset3" / "p", c.PreImaging.Offset3P),
+      offsetComponentMappingAtPath(p / "offset3" / "q", c.PreImaging.Offset3Q),
+      offsetMappingAtPath(p / "offset4", c.PreImaging.ObservationId),
+      offsetComponentMappingAtPath(p / "offset4" / "p", c.PreImaging.Offset4P),
+      offsetComponentMappingAtPath(p / "offset4" / "q", c.PreImaging.Offset4Q),
     )
 
   lazy val OffsetMappings: List[TypeMapping] =
-    preImagingMappings(GmosNorthPreImagingType, GmosNorthImagingView.Common) ++
-    preImagingMappings(GmosSouthPreImagingType, GmosSouthImagingView.Common) ++
+    preImagingMappings(GmosNorthImagingType / "variant" / "preImaging", GmosNorthImagingView.Common) ++
+    preImagingMappings(GmosSouthImagingType / "variant" / "preImaging", GmosSouthImagingView.Common) ++
     List(
       offsetMappingAtPath(EnumeratedPath, EnumeratedOffsetView.ObservationId, EnumeratedOffsetView.OffsetGeneratorRole, EnumeratedOffsetView.Index),
       offsetComponentMappingAtPath(EnumeratedPath / "p", EnumeratedOffsetView.OffsetP, EnumeratedOffsetView.ObservationId, EnumeratedOffsetView.OffsetGeneratorRole, EnumeratedOffsetView.Index),
