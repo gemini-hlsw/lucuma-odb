51837f1a632d0fcdf6ceb3d454e8783e75020bd8
Shane Walker <pistes.clary_0w@icloud.com>
2025-12-22 13:39:37 -0300

    mode data

--- modules/schema/src/main/scala/lucuma/odb/data/Itc.scala
+++ modules/schema/src/main/scala/lucuma/odb/data/Itc.scala
@@ -7,37 +7,13 @@ import cats.Order
 import cats.data.NonEmptyList
 import eu.timepit.refined.cats.*
 import lucuma.core.data.Zipper
+import lucuma.core.enums.GmosNorthFilter
+import lucuma.core.enums.GmosSouthFilter
 import lucuma.core.model.Target
+import lucuma.core.util.Enumerated
 import lucuma.core.util.TimeSpan
 import lucuma.itc.IntegrationTime
 import lucuma.itc.SignalToNoiseAt
-
-/*
-type Itc {
-  """
-  The ITC result for the science part of the sequence
-  """
-  science: ItcResultSet!
-
-  """
-  The ITC result for the acquisition part of the sequence, if any.
-  """
-  acquisition: ItcResultSet
-}
-
-type ItcResultSet {
-  selected: ItcResult!
-  all: [ItcResult!]
-  index: NonNegInt!
-}
-
-type ItcResult {
-  targetId: TargetId!
-  exposureTime: TimeSpan!
-  exposureCount: NonNegInt!
-  signalToNoiseAt: SignalToNoiseAt
-}
-*/
 
 case class Itc(
   acquisition: Option[Itc.ResultSet],
@@ -60,18 +36,37 @@ object Itc:
     given Order[Result] =
       Order.by(s => (s.totalTime, s.targetId))
 
+  enum ModeDataType(val tag: String) derives Enumerated:
+    case Empty            extends ModeDataType("empty")
+    case GmosNorthImaging extends ModeDataType("gmos_north_imaging")
+    case GmosSouthImaging extends ModeDataType("gmos_south_imaging")
+
+  sealed trait ModeData:
+    def dataType: ModeDataType =
+      this match
+        case ModeData.Empty                    => ModeDataType.Empty
+        case ModeData.GmosNorthImaging(filter) => ModeDataType.GmosNorthImaging
+        case ModeData.GmosSouthImaging(filter) => ModeDataType.GmosSouthImaging
+
+  object ModeData:
+    case object Empty                                    extends ModeData
+    case class GmosNorthImaging(filter: GmosNorthFilter) extends ModeData
+    case class GmosSouthImaging(filter: GmosSouthFilter) extends ModeData
+
   // Corresponds to ItcResultSet in the OdbSchema.graphql
   case class ResultSet(
-    results: Zipper[Result]
+    modeData: ModeData,
+    results:  Zipper[Result]
   )
 
   private def targetIds(z: Zipper[Result]): NonEmptyList[Target.Id] =
     z.toNel.map(_.targetId).sortBy(_.value)
 
   def fromResults(
+    modeData:    ModeData,
     acquisition: Option[Zipper[Result]],
     science:     Zipper[Result]
   ): Option[Itc] =
     Option.when(acquisition.forall(z => targetIds(z) === targetIds(science)))(
-      Itc(acquisition.map(ResultSet.apply), ResultSet(science))
+      Itc(acquisition.map(ResultSet(modeData, _)), ResultSet(modeData, science))
     )
