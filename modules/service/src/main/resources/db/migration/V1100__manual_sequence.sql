394d1b86d8a5636a37713cf3ebfb84b17b7ba75d
Shane Walker <pistes.clary_0w@icloud.com>
2026-01-27 12:06:30 -0300

    wip manual sequence generation

---
+++ modules/service/src/main/resources/db/migration/V1100__manual_sequence.sql
@@ -0,0 +1,62 @@
+CREATE TABLE t_atom (
+  c_atom_id         d_atom_id        PRIMARY KEY,
+  c_instrument      d_tag            NOT NULL,
+  c_atom_index      integer          NOT NULL,
+
+  UNIQUE (c_atom_id, c_instrument),
+
+  c_observation_id  d_observation_id NOT NULL,
+  c_sequence_type   e_sequence_type  NOT NULL,
+  c_visit_id        d_visit_id       REFERENCES t_visit(c_visit_id) ON DELETE CASCADE,
+  c_execution_state d_tag            NOT NULL REFERENCES t_atom_execution_state(c_tag) DEFAULT ('not_started'::character varying)::d_tag
+);
+
+ALTER TABLE ONLY t_atom
+  ADD CONSTRAINT t_atom_c_observation_id_c_instrument_fkey FOREIGN KEY (c_observation_id, c_instrument) REFERENCES t_observation(c_observation_id, c_instrument) ON DELETE CASCADE;
+
+CREATE TABLE t_step (
+  c_step_id          d_step_id     PRIMARY KEY,
+  c_atom_id          d_atom_id     NOT NULL REFERENCES t_atom(c_atom_id) DEFERRABLE INITIALLY DEFERRED,
+  c_instrument       d_tag         NOT NULL,
+  c_step_type        e_step_type   NOT NULL,
+  c_step_index       integer       NOT NULL,
+
+  UNIQUE (c_step_id, c_instrument),
+  UNIQUE (c_step_id, c_step_type),
+
+  c_observe_class    e_obs_class   NOT NULL,
+  c_time_estimate    interval      NOT NULL DEFAULT '00:00:00'::interval,
+  c_execution_state  d_tag         NOT NULL REFERENCES t_step_execution_state(c_tag) DEFAULT ('not_started'::character varying)::d_tag,
+  c_offset_p         d_angle_µas   NOT NULL DEFAULT 0,
+  c_offset_q         d_angle_µas   NOT NULL DEFAULT 0,
+  c_guide_state      e_guide_state NOT NULL DEFAULT 'enabled'::e_guide_state,
+
+  c_completed        timestamp without time zone,
+  c_first_event_time timestamp without time zone,
+  c_last_event_time  timestamp without time zone
+);
+
+ALTER TABLE ONLY t_step
+  ADD CONSTRAINT t_atom_c_atom_id_c_instrument_fkey FOREIGN KEY (c_atom_id, c_instrument) REFERENCES t_atom(c_atom_id, c_instrument) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+CREATE TRIGGER step_obscalc_invalidate_trigger
+  AFTER UPDATE ON t_step FOR EACH ROW WHEN ((num_nulls(new.c_completed, old.c_completed) = 1)) EXECUTE FUNCTION step_record_invalidate();
+
+-- TEMPORARILY relax the FK constraints for these tables while t_step_record
+-- continues to exist.  We will have entries referring to steps in t_step_record
+-- and entries referring to t_step.
+
+ALTER TABLE t_step_config_gcal
+  DROP CONSTRAINT t_step_config_gcal_c_step_id_c_step_type_fkey;
+
+ALTER TABLE t_step_config_smart_gcal
+  DROP CONSTRAINT t_step_config_smart_gcal_c_step_id_c_step_type_fkey;
+
+ALTER TABLE t_gmos_north_dynamic
+  DROP CONSTRAINT t_gmos_north_dynamic_c_step_id_c_instrument_fkey;
+
+ALTER TABLE t_gmos_south_dynamic
+  DROP CONSTRAINT t_gmos_south_dynamic_c_step_id_c_instrument_fkey;
+
+ALTER TABLE t_flamingos_2_dynamic
+  DROP CONSTRAINT t_flamingos_2_dynamic_c_step_id_c_instrument_fkey;
