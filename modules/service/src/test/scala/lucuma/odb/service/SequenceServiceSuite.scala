d112ed75012baca60e4511f8f0f6f5859f1c1e85
Shane Walker <pistes.clary_0w@icloud.com>
2026-01-27 18:40:51 -0300

    stream from database

--- modules/service/src/test/scala/lucuma/odb/service/SequenceServiceSuite.scala
+++ modules/service/src/test/scala/lucuma/odb/service/SequenceServiceSuite.scala
@@ -8,13 +8,16 @@ import cats.syntax.option.*
 import eu.timepit.refined.types.numeric.PosInt
 import fs2.Stream
 import lucuma.core.enums.SequenceType
+import lucuma.core.model.Observation
+import lucuma.core.model.Program
+import lucuma.core.model.sequence.Atom
 import lucuma.core.model.sequence.InstrumentExecutionConfig
+import lucuma.core.model.sequence.gmos.DynamicConfig.GmosNorth
 import lucuma.core.syntax.timespan.*
 import lucuma.itc.IntegrationTime
 import lucuma.odb.graphql.query.ExecutionTestSupportForGmos
+import lucuma.odb.sequence.gmos.InitialConfigs.GmosNorthStatic
 import lucuma.odb.service.Services.Syntax.*
-
-import java.time.Duration
 
 class SequenceServiceSuite extends ExecutionTestSupportForGmos:
 
@@ -24,25 +27,60 @@ class SequenceServiceSuite extends ExecutionTestSupportForGmos:
       PosInt.unsafeFrom(100)
     )
 
-  test("exercise serialization"):
-    val time = for
-      p <- createProgram
-      t <- createTargetWithProfileAs(pi, p)
-      o <- createGmosNorthLongSlitObservationAs(pi, p, List(t))
-      s <- withServices(serviceUser): services =>
-             services.generator.generate(p, o).map: e =>
-               val g = e.toOption
-                 .flatMap: iec =>
-                   InstrumentExecutionConfig.gmosNorth.getOption(iec)
-                 .get.executionConfig.science.get
-               g.nextAtom :: g.possibleFuture
-      b <- IO.realTimeInstant
-      _ <- withServices(serviceUser): services =>
-             services
-               .transactionally:
-                 sequenceService
-                   .insertGmosNorthSequence(o, SequenceType.Science, none, Stream.emits(s))
-      e <- IO.realTimeInstant
-    yield Duration.between(b, e)
+  private def generateSequence(
+    p: Program.Id,
+    o: Observation.Id
+  ): IO[List[Atom[GmosNorth]]] =
+    withServices(serviceUser): services =>
+      services.generator.generate(p, o).map: e =>
+        val g =
+          e.toOption
+           .flatMap: iec =>
+             InstrumentExecutionConfig.gmosNorth.getOption(iec)
+           .get.executionConfig.science.get
+        g.nextAtom :: g.possibleFuture
 
-    assertIOBoolean(time.flatTap(d => IO.println(s"Duration: $d")).map(d => d.compareTo(Duration.ofSeconds(10L)) < 0))
+  private def writeSequence(
+    o: Observation.Id,
+    s: List[Atom[GmosNorth]]
+  ): IO[Unit] =
+    withServices(serviceUser): services =>
+      services
+        .transactionally:
+          sequenceService
+            .insertGmosNorthSequence(o, SequenceType.Science, none, Stream.emits(s))
+
+  private def readSequence(
+    o: Observation.Id
+  ): IO[List[Atom[GmosNorth]]] =
+    for
+      t <- gmosNorthTimeEstimateCalculator
+      s <- withServices(serviceUser): services =>
+        services
+          .transactionally:
+            sequenceService
+              .streamGmosNorthSequence(o, SequenceType.Science, GmosNorthStatic, t)
+              .compile
+              .toList
+    yield s
+
+  test("exercise serialization"):
+    val res = for
+      p  <- createProgram
+      t  <- createTargetWithProfileAs(pi, p)
+      o  <- createGmosNorthLongSlitObservationAs(pi, p, List(t))
+
+      // Generate the Sequence
+      sn <- generateSequence(p, o)
+      b  <- IO.realTimeInstant
+      _  <- writeSequence(o, sn)
+      e0 <- IO.realTimeInstant
+      so <- readSequence(o)
+      e1 <- IO.realTimeInstant
+
+//      _  <- IO.println(s"WRITE TIME: ${java.time.Duration.between(b,  e0)}")
+//      _  <- IO.println(s"READ TIME.: ${java.time.Duration.between(e0, e1)}")
+//      _  <- IO.println(s"SEQUENCE..: ${so.mkString("\n")}")
+    yield (sn, so)
+
+    assertIOBoolean(res.map((in, out) => in == out))
