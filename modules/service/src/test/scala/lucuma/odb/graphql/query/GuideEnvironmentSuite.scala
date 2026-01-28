75e8b49da0e0b2e535378fb0710b551cf97dc2ba
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-01-28 20:11:58 -0300

    Update wavelength calculations for AGS and add support for gmos imaging

--- modules/service/src/test/scala/lucuma/odb/graphql/query/GuideEnvironmentSuite.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/GuideEnvironmentSuite.scala
@@ -580,6 +580,81 @@ trait GuideEnvironmentSuite extends ExecutionTestSupport:
     }
     """.asRight
 
+  val gmosImagingOIWFSResult  =
+    json"""
+    {
+      "observation": {
+        "title": "V1647 Orionis",
+        "targetEnvironment": {
+          "guideEnvironment": {
+            "posAngle": {
+              "degrees": 200.000000
+            },
+            "guideTargets": [
+              {
+                "name": "Gaia DR3 3219118090462918016",
+                "probe": "GMOS_OIWFS",
+                "sourceProfile": {
+                  "point": {
+                    "bandNormalized": {
+                      "brightnesses": [
+                        {
+                          "band": "GAIA"
+                        },
+                        {
+                          "band": "GAIA_RP"
+                        }
+                      ]
+                    }
+                  }
+                },
+                "sidereal": {
+                  "catalogInfo": {
+                    "name": "GAIA",
+                    "id": "3219118090462918016",
+                    "objectType": null
+                  },
+                  "epoch": "J2016.000",
+                  "ra": {
+                    "microseconds": 20782433789,
+                    "hms": "05:46:22.433789",
+                    "hours": 5.772898274722222222222222222222222,
+                    "degrees": 86.59347412083333333333333333333333
+                  },
+                  "dec": {
+                    "dms": "-00:08:52.645460",
+                    "degrees": 359.8520429277778,
+                    "microarcseconds": 1295467354540
+                  },
+                  "radialVelocity": {
+                    "metersPerSecond": 10090.042000,
+                    "centimetersPerSecond": 1009004,
+                    "kilometersPerSecond": 10.090042
+                  },
+                  "properMotion": {
+                    "ra": {
+                      "microarcsecondsPerYear": 438,
+                      "milliarcsecondsPerYear": 0.438
+                    },
+                    "dec": {
+                      "microarcsecondsPerYear": -741,
+                      "milliarcsecondsPerYear": -0.741
+                    }
+                  },
+                  "parallax": {
+                    "microarcseconds": 2432,
+                    "milliarcseconds": 2.432
+                  }
+                },
+                "nonsidereal": null
+              }
+            ]
+          }
+        }
+      }
+    }
+    """.asRight
+
   def createObservationAs(user: User, pid: Program.Id, tids: List[Target.Id]): IO[Observation.Id]
 
   // Create ephemeris at the same coordinates as the sidereal target
@@ -606,6 +681,81 @@ trait GuideEnvironmentSuite extends ExecutionTestSupport:
           "guideEnvironment": {
             "posAngle": {
               "degrees": 350.000000
+            },
+            "guideTargets": [
+              {
+                "name": "Gaia DR3 3219118640218737920",
+                "probe": "PWFS2",
+                "sourceProfile": {
+                  "point": {
+                    "bandNormalized": {
+                      "brightnesses": [
+                        {
+                          "band": "GAIA"
+                        },
+                        {
+                          "band": "GAIA_RP"
+                        }
+                      ]
+                    }
+                  }
+                },
+                "sidereal": {
+                  "catalogInfo": {
+                    "name": "GAIA",
+                    "id": "3219118640218737920",
+                    "objectType": null
+                  },
+                  "epoch": "J2016.000",
+                  "ra": {
+                    "microseconds": 20760247957,
+                    "hms": "05:46:00.247957",
+                    "hours": 5.766735543611111111111111111111111,
+                    "degrees": 86.50103315416666666666666666666667
+                  },
+                  "dec": {
+                    "dms": "-00:08:26.290793",
+                    "degrees": 359.85936366861114,
+                    "microarcseconds": 1295493709207
+                  },
+                  "radialVelocity": {
+                    "metersPerSecond": 0,
+                    "centimetersPerSecond": 0,
+                    "kilometersPerSecond": 0
+                  },
+                  "properMotion": {
+                    "ra": {
+                      "microarcsecondsPerYear": 806,
+                      "milliarcsecondsPerYear": 0.806
+                    },
+                    "dec": {
+                      "microarcsecondsPerYear": -1093,
+                      "milliarcsecondsPerYear": -1.093
+                    }
+                  },
+                  "parallax": {
+                    "microarcseconds": 2371,
+                    "milliarcseconds": 2.371
+                  }
+                },
+                "nonsidereal": null
+              }
+            ]
+          }
+        }
+      }
+    }
+    """.asRight
+
+  val pwfs2ImagingResult =
+    json"""
+    {
+      "observation": {
+        "title": "Nonsidereal Target",
+        "targetEnvironment": {
+          "guideEnvironment": {
+            "posAngle": {
+              "degrees": 210.000000
             },
             "guideTargets": [
               {
