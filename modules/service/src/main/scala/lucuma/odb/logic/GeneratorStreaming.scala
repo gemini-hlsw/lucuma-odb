6ace17c1c2fa01c6f21e5b5934637aa0ad6a445c
Shane Walker <pistes.clary_0w@icloud.com>
2026-02-10 10:19:01 -0300

    materialize method

--- modules/service/src/main/scala/lucuma/odb/logic/GeneratorStreaming.scala
+++ modules/service/src/main/scala/lucuma/odb/logic/GeneratorStreaming.scala
@@ -22,7 +22,6 @@ import lucuma.odb.sequence.ObservingMode.Syntax.*
 import lucuma.odb.sequence.data.StreamingExecutionConfig
 import lucuma.odb.sequence.util.CommitHash
 import lucuma.odb.sequence.util.SequenceIds
-import lucuma.odb.service.NoTransaction
 import lucuma.odb.service.Services
 import lucuma.odb.service.Services.Syntax.*
 import skunk.Transaction
@@ -31,30 +30,60 @@ import java.util.UUID
 
 /**
  * GeneratorStreaming produces StreamingExecutionConfig values for each of the
- * observing modes, hiding the detail of whether it is read from the database or
- * generated from observation configuration.
+ * observing modes.  Two varieties are offered for each mode.  The 'generate'
+ * method (e.g., `generateFlamingos2LongSlit`) produces a sequence from scratch
+ * ignoring any partial or previous execution of the sequence.  The
+ * 'selectOrGenerate' method will read the unexecuted portion of the sequence
+ * from the database if present, or else generate the sequence from scratch.
  */
 sealed trait GeneratorStreaming[F[_]]:
 
-  def flamingos2LongSlit(
+  def selectOrGenerateFlamingos2LongSlit(
     context: GeneratorContext
-  )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]]
+  )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]]
 
-  def gmosNorthImaging(
+  // TODO: can we remove the effect?  I don't think we need it:
+  //  Either[OdbError, StreamingExecutionConfig[Pure, Flamingos2Static, Flamingos2Dynamic]]
+  def generateFlamingos2LongSlit(
     context: GeneratorContext
-  )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
+  ): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]]
 
-  def gmosNorthLongSlit(
-    context: GeneratorContext
-  )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
 
-  def gmosSouthImaging(
+  def selectOrGenerateGmosNorthImaging(
     context: GeneratorContext
-  )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+  )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
 
-  def gmosSouthLongSlit(
+  def generateGmosNorthImaging(
     context: GeneratorContext
-  )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+  ): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
+
+
+  def selectOrGenerateGmosNorthLongSlit(
+    context: GeneratorContext
+  )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
+
+  def generateGmosNorthLongSlit(
+    context: GeneratorContext
+  ): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]]
+
+
+  def selectOrGenerateGmosSouthImaging(
+    context: GeneratorContext
+  )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+
+  def generateGmosSouthImaging(
+    context: GeneratorContext
+  ): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+
+
+  def selectOrGenerateGmosSouthLongSlit(
+    context: GeneratorContext
+  )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+
+  def generateGmosSouthLongSlit(
+    context: GeneratorContext
+  ): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]]
+
 
 object GeneratorStreaming:
 
@@ -90,9 +119,9 @@ object GeneratorStreaming:
       private val exp = SmartGcalImplementation.fromService(smartGcalService)
 
       private def select[S, D](
-        lookup: (Transaction[F], Services.ServiceAccess) ?=> F[Option[StreamingExecutionConfig[F, S, D]]]
-      )(using NoTransaction[F], Services.ServiceAccess): OptionT[F, Either[OdbError, StreamingExecutionConfig[F, S, D]]] =
-        OptionT(services.transactionally(lookup)).map(_.asRight[OdbError])
+        lookup: (Transaction[F]) ?=> F[Option[StreamingExecutionConfig[F, S, D]]]
+      )(using Transaction[F]): OptionT[F, Either[OdbError, StreamingExecutionConfig[F, S, D]]] =
+        OptionT(lookup).map(_.asRight[OdbError])
 
       private def extractMode[A](
         expected: String,
@@ -101,66 +130,90 @@ object GeneratorStreaming:
         val mode = context.params.observingMode
         EitherT.fromOption[F](f(mode), GeneratorError.unexpectedMode(context.oid, expected, mode))
 
-      override def flamingos2LongSlit(
+      override def selectOrGenerateFlamingos2LongSlit(
         context: GeneratorContext
-      )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]] =
-        select(sequenceService.streamingFlamingos2ExecutionConfig(context.oid))
-          .getOrElseF:
-            import lucuma.odb.sequence.flamingos2.longslit.LongSlit
-            (for
-              cfg <- extractMode(ObservingMode.Flamingos2LongSlitName, context)(_.asFlamingos2LongSlit)
-              itc  = requireSpectroscopyItc(context.oid, context.itcRes)
-              gen <- EitherT(LongSlit.instantiate(context.oid, calculator.flamingos2, context.namespace, exp.flamingos2, cfg, itc, context.params.acqResetTime))
-            yield gen.streamingExecutionConfig[F]).value
+      )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]] =
+        select(sequenceService.selectFlamingos2ExecutionConfig(context.oid))
+          .getOrElseF(generateFlamingos2LongSlit(context))
 
-      override def gmosNorthImaging(
+      override def generateFlamingos2LongSlit(
         context: GeneratorContext
-      )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
-        select(sequenceService.streamingGmosNorthExecutionConfig(context.oid))
-          .getOrElseF:
-            import lucuma.odb.sequence.gmos.imaging.Imaging
-            (for
-              cfg <- extractMode(ObservingMode.GmosNorthImagingName, context)(_.asGmosNorthImaging)
-              itc  = requireImagingItc(ObservingMode.GmosNorthImagingName, context.oid, context.itcRes, Itc.gmosNorthImaging.getOption)
-              gen <- EitherT(Imaging.gmosNorth(calculator.gmosNorth, context.namespace, cfg, itc))
-            yield gen.streamingExecutionConfig[F]).value
+      ): F[Either[OdbError, StreamingExecutionConfig[F, Flamingos2Static, Flamingos2Dynamic]]] =
+        import lucuma.odb.sequence.flamingos2.longslit.LongSlit
+        (for
+          cfg <- extractMode(ObservingMode.Flamingos2LongSlitName, context)(_.asFlamingos2LongSlit)
+          itc  = requireSpectroscopyItc(context.oid, context.itcRes)
+          gen <- EitherT(LongSlit.instantiate(context.oid, calculator.flamingos2, context.namespace, exp.flamingos2, cfg, itc, context.params.acqResetTime))
+        yield gen.streamingExecutionConfig[F]).value
 
-      override def gmosNorthLongSlit(
-        context: GeneratorContext
-      )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
-        select(sequenceService.streamingGmosNorthExecutionConfig(context.oid))
-          .getOrElseF:
-            import lucuma.odb.sequence.gmos.longslit.LongSlit
-            (for
-              cfg <- extractMode(ObservingMode.GmosNorthLongSlitName, context)(_.asGmosNorthLongSlit)
-              itc  = requireSpectroscopyItc(context.oid, context.itcRes)
-              rol  = context.params.calibrationRole
-              rst  = context.params.acqResetTime
-              gen <- EitherT(LongSlit.gmosNorth(context.oid, calculator.gmosNorth, context.namespace, exp.gmosNorth, cfg, itc, rol, rst))
-            yield gen.streamingExecutionConfig[F]).value
 
-      override def gmosSouthImaging(
+      override def selectOrGenerateGmosNorthImaging(
         context: GeneratorContext
-      )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
-        select(sequenceService.streamingGmosSouthExecutionConfig(context.oid))
-          .getOrElseF:
-            import lucuma.odb.sequence.gmos.imaging.Imaging
-            (for
-              cfg <- extractMode(ObservingMode.GmosSouthImagingName, context)(_.asGmosSouthImaging)
-              itc  = requireImagingItc(ObservingMode.GmosSouthImagingName, context.oid, context.itcRes, Itc.gmosSouthImaging.getOption)
-              gen <- EitherT(Imaging.gmosSouth(calculator.gmosSouth, context.namespace, cfg, itc))
-            yield gen.streamingExecutionConfig[F]).value
+      )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
+        select(sequenceService.selectGmosNorthExecutionConfig(context.oid))
+          .getOrElseF(generateGmosNorthImaging(context))
 
-      override def gmosSouthLongSlit(
+      override def generateGmosNorthImaging(
         context: GeneratorContext
-      )(using NoTransaction[F], Services.ServiceAccess): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
-        select(sequenceService.streamingGmosSouthExecutionConfig(context.oid))
-          .getOrElseF:
-            import lucuma.odb.sequence.gmos.longslit.LongSlit
-            (for
-              cfg <- extractMode(ObservingMode.GmosSouthLongSlitName, context)(_.asGmosSouthLongSlit)
-              itc  = requireSpectroscopyItc(context.oid, context.itcRes)
-              rol  = context.params.calibrationRole
-              rst  = context.params.acqResetTime
-              gen <- EitherT(LongSlit.gmosSouth(context.oid, calculator.gmosSouth, context.namespace, exp.gmosSouth, cfg, itc, rol, rst))
-            yield gen.streamingExecutionConfig[F]).value
+      ): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
+        import lucuma.odb.sequence.gmos.imaging.Imaging
+        (for
+          cfg <- extractMode(ObservingMode.GmosNorthImagingName, context)(_.asGmosNorthImaging)
+          itc  = requireImagingItc(ObservingMode.GmosNorthImagingName, context.oid, context.itcRes, Itc.gmosNorthImaging.getOption)
+          gen <- EitherT(Imaging.gmosNorth(calculator.gmosNorth, context.namespace, cfg, itc))
+        yield gen.streamingExecutionConfig[F]).value
+
+
+      override def selectOrGenerateGmosNorthLongSlit(
+        context: GeneratorContext
+      )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
+        select(sequenceService.selectGmosNorthExecutionConfig(context.oid))
+          .getOrElseF(generateGmosNorthLongSlit(context))
+
+      override def generateGmosNorthLongSlit(
+        context: GeneratorContext
+      ): F[Either[OdbError, StreamingExecutionConfig[F, GmosNorthStatic, GmosNorthDynamic]]] =
+        import lucuma.odb.sequence.gmos.longslit.LongSlit
+        (for
+          cfg <- extractMode(ObservingMode.GmosNorthLongSlitName, context)(_.asGmosNorthLongSlit)
+          itc  = requireSpectroscopyItc(context.oid, context.itcRes)
+          rol  = context.params.calibrationRole
+          rst  = context.params.acqResetTime
+          gen <- EitherT(LongSlit.gmosNorth(context.oid, calculator.gmosNorth, context.namespace, exp.gmosNorth, cfg, itc, rol, rst))
+        yield gen.streamingExecutionConfig[F]).value
+
+
+      override def selectOrGenerateGmosSouthImaging(
+        context: GeneratorContext
+      )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
+        select(sequenceService.selectGmosSouthExecutionConfig(context.oid))
+          .getOrElseF(generateGmosSouthImaging(context))
+
+      override def generateGmosSouthImaging(
+        context: GeneratorContext
+      ): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
+        import lucuma.odb.sequence.gmos.imaging.Imaging
+        (for
+          cfg <- extractMode(ObservingMode.GmosSouthImagingName, context)(_.asGmosSouthImaging)
+          itc  = requireImagingItc(ObservingMode.GmosSouthImagingName, context.oid, context.itcRes, Itc.gmosSouthImaging.getOption)
+          gen <- EitherT(Imaging.gmosSouth(calculator.gmosSouth, context.namespace, cfg, itc))
+        yield gen.streamingExecutionConfig[F]).value
+
+
+      override def selectOrGenerateGmosSouthLongSlit(
+        context: GeneratorContext
+      )(using Transaction[F]): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
+        select(sequenceService.selectGmosSouthExecutionConfig(context.oid))
+          .getOrElseF(generateGmosSouthLongSlit(context))
+
+      override def generateGmosSouthLongSlit(
+        context: GeneratorContext
+      ): F[Either[OdbError, StreamingExecutionConfig[F, GmosSouthStatic, GmosSouthDynamic]]] =
+        import lucuma.odb.sequence.gmos.longslit.LongSlit
+        (for
+          cfg <- extractMode(ObservingMode.GmosSouthLongSlitName, context)(_.asGmosSouthLongSlit)
+          itc  = requireSpectroscopyItc(context.oid, context.itcRes)
+          rol  = context.params.calibrationRole
+          rst  = context.params.acqResetTime
+          gen <- EitherT(LongSlit.gmosSouth(context.oid, calculator.gmosSouth, context.namespace, exp.gmosSouth, cfg, itc, rol, rst))
+        yield gen.streamingExecutionConfig[F]).value
