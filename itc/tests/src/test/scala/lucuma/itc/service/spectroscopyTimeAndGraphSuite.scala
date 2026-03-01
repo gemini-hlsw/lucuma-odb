185de623943d0fdf99a4b757f73107ff80199c6d
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-03-01 12:53:35 -0300

    Added chart tests for igrins 2

--- itc/tests/src/test/scala/lucuma/itc/service/spectroscopyTimeAndGraphSuite.scala
+++ itc/tests/src/test/scala/lucuma/itc/service/spectroscopyTimeAndGraphSuite.scala
@@ -5,7 +5,7 @@ package lucuma.itc.service
 
 import io.circe.literal.*
 
-class GraphQLSpectroscopyTimeAndGraphSuite extends GraphQLSuite {
+class spectroscopyTimeAndGraphSuite extends GraphQLSuite {
 
   test("gmos graph") {
     query(
@@ -70,6 +70,166 @@ class GraphQLSpectroscopyTimeAndGraphSuite extends GraphQLSuite {
                 },
                 grating: B1200_G5301
               }
+            },
+            significantFigures: {
+              xAxis: 4
+            }
+          }) {
+            targetTimesAndGraphs {
+              ... on TargetTimeAndGraphs {
+                integrationTime {
+                  selected {
+                    exposureTime {
+                      seconds
+                    }
+                    exposureCount
+                  }
+                }
+                graphs {
+                  ccds {
+                    singleSNRatio
+                    totalSNRatio
+                    peakPixelFlux
+                    wellDepth
+                    ampGain
+                  }
+                  graphData {
+                    graphType
+                    series {
+                      title
+                      seriesType
+                      data
+                      xAxis {
+                        start
+                        end
+                        count
+                      }
+                      dataY
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        """,
+      json"""{
+          "data": {
+            "spectroscopyIntegrationTimeAndGraphs": {
+              "targetTimesAndGraphs": [
+                {
+                  "integrationTime": {
+                    "selected": {
+                      "exposureCount" : 10,
+                      "exposureTime" : {
+                        "seconds" : 1.000000000
+                      }
+                    }
+                  },
+                  "graphs": {
+                    "ccds" : [
+                      {
+                        "singleSNRatio" : 1.0,
+                        "totalSNRatio" : 2.0,
+                        "peakPixelFlux" : 3.0,
+                        "wellDepth" : 4.0,
+                        "ampGain" : 5.0
+                      }
+                    ],
+                    "graphData": [
+                      {
+                        "graphType": "S2N_GRAPH",
+                        "series": [
+                          {
+                            "title": "title",
+                            "seriesType": "FINAL_S2_NDATA",
+                            "data": [
+                              [
+                                1.0,
+                                1000.0
+                              ],
+                              [
+                                2.0,
+                                1001.0
+                              ]
+                            ],
+                            "xAxis" : {
+                              "start" : 1.0,
+                              "end" : 2.0,
+                              "count" : 2
+                            },
+                            "dataY": [
+                              1000.0,
+                              1001.0
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        }
+        """
+    )
+  }
+
+  test("igrins2 graph") {
+    query(
+      """
+        query {
+          spectroscopyIntegrationTimeAndGraphs(input: {
+            exposureTimeMode: {
+              signalToNoise: {
+                value: 2,
+                at: { nanometers: 1600 }
+              }
+            },
+            asterism: [
+              {
+                sourceProfile: {
+                  point: {
+                    bandNormalized: {
+                      sed: {
+                        planet: JUPITER
+                      }
+                      brightnesses: [ {
+                        band: R
+                        value: 3
+                        units: ERG_PER_S_PER_CM_SQUARED_PER_A
+                      }, {
+                        band: J
+                        value: 2.1
+                        units: AB_MAGNITUDE
+                      }]
+                    }
+                  }
+                },
+                radialVelocity: {
+                  kilometersPerSecond: 1000
+                }
+              }
+            ],
+            constraints: {
+              imageQuality: {
+                preset: POINT_THREE
+              },
+              cloudExtinction: {
+                preset: POINT_FIVE
+              },
+              skyBackground: DARK,
+              waterVapor: DRY,
+              elevationRange: {
+                airMass: {
+                  min: 1,
+                  max: 2
+                }
+              }
+            },
+            mode: {
+              igrins2Spectroscopy: {}
             },
             significantFigures: {
               xAxis: 4
