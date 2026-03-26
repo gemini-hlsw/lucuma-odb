3984abf8da7dcc33b812f5a3cead94a8f0df721c
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-03-26 15:48:44 -0300

    Send traces to an otel4s endpoint

--- modules/service/src/main/scala/lucuma/odb/util/LucumaEntryPoint.scala
+++ modules/service/src/main/scala/lucuma/odb/util/LucumaEntryPoint.scala
@@ -5,28 +5,58 @@ package lucuma.odb.util
 
 import cats.effect.*
 import cats.syntax.all.*
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
+import io.opentelemetry.sdk.resources.Resource as OTelResource
+import io.opentelemetry.sdk.trace.SdkTracerProvider
+import io.opentelemetry.sdk.trace.`export`.BatchSpanProcessor
+import io.opentelemetry.api.common.AttributeKey
 import lucuma.odb.Config
 import natchez.EntryPoint
-import natchez.honeycomb.Honeycomb
 import natchez.noop.NoopEntrypoint
+import natchez.opentelemetry.OpenTelemetry
 import org.typelevel.log4cats.Logger
 
+import java.util.Base64
+
 object LucumaEntryPoint:
-  def tracingBackend(config: Config) = config.honeycomb match
-    case Some(_) => "Honeycomb"
+  def tracingBackend(config: Config) = config.otel match
+    case Some(_) => "OpenTelemetry (OTLP)"
     case None    => "No-op (silent)"
 
-  def entryPointResource[F[_]: Sync: Logger](serviceName: String, config: Config): Resource[F, EntryPoint[F]] =
-    config.honeycomb match
-      case Some(honeycombConfig) =>
-        Resource.eval(Logger[F].info("Initializing Honeycomb tracing backend")) *>
-          Honeycomb.entryPoint(serviceName) { cb =>
-            Sync[F].delay {
-              cb.setWriteKey(honeycombConfig.writeKey)
-              cb.setDataset(honeycombConfig.dataset)
-              cb.build()
-            }
+  def entryPointResource[F[_]: Sync: Logger](
+    serviceName: String,
+    config: Config
+  ): Resource[F, EntryPoint[F]] =
+    config.otel match
+      case Some(otelConfig) =>
+        Resource.eval(Logger[F].info("Initializing OpenTelemetry tracing backend")) *>
+          OpenTelemetry.entryPoint[F]() { sdkBuilder =>
+            Resource.make(
+              Sync[F].delay {
+                val credentials =
+                  Base64.getEncoder.encodeToString(
+                    s"${otelConfig.instanceId}:${otelConfig.apiKey}".getBytes
+                  )
+                val endpoint =
+                  val base = otelConfig.endpoint.stripSuffix("/")
+                  if base.endsWith("/v1/traces") then base
+                  else s"$base/v1/traces"
+                val exporter = OtlpHttpSpanExporter.builder()
+                  .setEndpoint(endpoint)
+                  .addHeader("Authorization", s"Basic $credentials")
+                  .build()
+                val processor = BatchSpanProcessor.builder(exporter).build()
+                val resource = OTelResource.builder()
+                  .put(AttributeKey.stringKey("service.name"), serviceName)
+                  .build()
+                val tracerProvider = SdkTracerProvider.builder()
+                  .setResource(OTelResource.getDefault.merge(resource))
+                  .addSpanProcessor(processor)
+                  .build()
+                sdkBuilder.setTracerProvider(tracerProvider)
+              }
+            )(_ => Sync[F].unit)
           }
       case None =>
-        Resource.eval(Logger[F].info("No Honeycomb configuration")) *>
+        Resource.eval(Logger[F].info("No OpenTelemetry configuration")) *>
           Resource.pure(NoopEntrypoint())
