b709d87e3957eb9a7c4bd86804e0e020235c5179
Todd Burnside <tburnside@gemini.edu>
2026-04-14 08:31:04 -0700

    A few more roundToSignificantFigures tests.

--- itc/tests/src/test/scala/lucuma/itc/MathSuite.scala
+++ itc/tests/src/test/scala/lucuma/itc/MathSuite.scala
@@ -5,11 +5,14 @@ package lucuma.itc.math
 
 class MathSuite extends munit.FunSuite:
   test("rounding") {
-    assertEquals(10.0, roundToSignificantFigures(10.0, 0))
-    assertEquals(10.0, roundToSignificantFigures(10.0, 1))
-    assertEquals(10.0001, roundToSignificantFigures(10.0001, 6))
-    assertEquals(10.0, roundToSignificantFigures(10.0001, 5))
-    assertEquals(10.001, roundToSignificantFigures(10.0005, 5))
-    assertEquals(10.0, roundToSignificantFigures(10.0004, 5))
-    assertEquals(11110.0, roundToSignificantFigures(11111.5, 4))
+    assertEquals(roundToSignificantFigures(10.0, 0), 10.0)
+    assertEquals(roundToSignificantFigures(10.0, 1), 10.0)
+    assertEquals(roundToSignificantFigures(10.0001, 6), 10.0001)
+    assertEquals(roundToSignificantFigures(10.0001, 5), 10.0)
+    assertEquals(roundToSignificantFigures(10.0005, 5), 10.001)
+    assertEquals(roundToSignificantFigures(10.0004, 5), 10.0)
+    assertEquals(roundToSignificantFigures(11111.5, 4), 11110.0)
+    assertEquals(roundToSignificantFigures(0.123456, 4), 0.1235)
+    assertEquals(roundToSignificantFigures(0.0123456, 3), 0.0123)
+    assertEquals(roundToSignificantFigures(0.000123456, 3), 0.000123)
   }
