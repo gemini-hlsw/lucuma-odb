70a4a4f7550c7644261bba03f4850c4aa17be76c
Shane Walker <pistes.clary_0w@icloud.com>
2026-05-05 16:30:32 -0400

    GHOST central wavelength in dynamic config

--- modules/service/src/test/scala/lucuma/odb/graphql/query/executionSciGhostIfu.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/executionSciGhostIfu.scala
@@ -155,6 +155,7 @@ class executionSciGhostIfu extends ExecutionTestSupportForGhost:
                               }
                               ifu1FiberAgitator
                               ifu2FiberAgitator
+                              centralWavelength { nanometers }
                             }
                           }
                         }
