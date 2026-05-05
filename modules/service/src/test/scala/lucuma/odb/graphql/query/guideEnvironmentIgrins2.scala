8a736f79bac0a8e4a850cef16e60a8dfa5437965
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-05-05 10:55:20 -0400

    Fix broken test after changing the limits

--- modules/service/src/test/scala/lucuma/odb/graphql/query/guideEnvironmentIgrins2.scala
+++ modules/service/src/test/scala/lucuma/odb/graphql/query/guideEnvironmentIgrins2.scala
@@ -35,11 +35,11 @@ class guideEnvironmentIgrins2 extends ExecutionTestSupportForIgrins2
         "targetEnvironment": {
           "guideEnvironment": {
             "posAngle": {
-              "degrees": 240.000000
+              "degrees": 60.000000
             },
             "guideTargets": [
               {
-                "name": "Gaia DR3 3219118640218737920",
+                "name": "Gaia DR3 3219118090462918016",
                 "probe": "PWFS2",
                 "sourceProfile": {
                   "point": {
@@ -58,39 +58,39 @@ class guideEnvironmentIgrins2 extends ExecutionTestSupportForIgrins2
                 "sidereal": {
                   "catalogInfo": {
                     "name": "GAIA",
-                    "id": "3219118640218737920",
+                    "id": "3219118090462918016",
                     "objectType": null
                   },
                   "epoch": "J2016.000",
                   "ra": {
-                    "microseconds": 20760247957,
-                    "hms": "05:46:00.247957",
-                    "hours": 5.766735543611111111111111111111111,
-                    "degrees": 86.50103315416666666666666666666667
+                    "microseconds": 20782433789,
+                    "hms": "05:46:22.433789",
+                    "hours": 5.772898274722222222222222222222222,
+                    "degrees": 86.59347412083333333333333333333333
                   },
                   "dec": {
-                    "dms": "-00:08:26.290793",
-                    "degrees": 359.85936366861114,
-                    "microarcseconds": 1295493709207
+                    "dms": "-00:08:52.645460",
+                    "degrees": 359.8520429277778,
+                    "microarcseconds": 1295467354540
                   },
                   "radialVelocity": {
-                    "metersPerSecond": 0,
-                    "centimetersPerSecond": 0,
-                    "kilometersPerSecond": 0
+                    "metersPerSecond": 10090.042000,
+                    "centimetersPerSecond": 1009004,
+                    "kilometersPerSecond": 10.090042
                   },
                   "properMotion": {
                     "ra": {
-                      "microarcsecondsPerYear": 806,
-                      "milliarcsecondsPerYear": 0.806
+                      "microarcsecondsPerYear": 438,
+                      "milliarcsecondsPerYear": 0.438
                     },
                     "dec": {
-                      "microarcsecondsPerYear": -1093,
-                      "milliarcsecondsPerYear": -1.093
+                      "microarcsecondsPerYear": -741,
+                      "milliarcsecondsPerYear": -0.741
                     }
                   },
                   "parallax": {
-                    "microarcseconds": 2371,
-                    "milliarcseconds": 2.371
+                    "microarcseconds": 2432,
+                    "milliarcseconds": 2.432
                   }
                 },
                 "nonsidereal": null
