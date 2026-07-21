4fb285ee7eb6fbd198ec69f7b8739917cc591d6d
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-07-21 10:09:31 -0400

    Add the Archive Duplication Search query policy

---
+++ modules/service/src/main/scala/lucuma/odb/logic/GoaSearchCenter.scala
@@ -0,0 +1,18 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.logic
+
+import cats.Eq
+import cats.derived.*
+import eu.timepit.refined.cats.given
+import eu.timepit.refined.types.string.NonEmptyString
+import lucuma.core.math.Coordinates
+
+/**
+ * The single point an Archive Duplication Search is run around.  GOA locates
+ * sidereal pointings by coordinates and moving targets by object name.
+ */
+enum GoaSearchCenter derives Eq:
+  case Sidereal(coordinates: Coordinates)
+  case NonSidereal(targetName: NonEmptyString)
