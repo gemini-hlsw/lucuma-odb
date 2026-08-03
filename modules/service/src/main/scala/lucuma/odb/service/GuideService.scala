4bad2f476c80272e602fd46c7d7b27b6a47e80d8
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-08-03 12:38:13 -0400

    Include more span attributes on ags searches

--- modules/service/src/main/scala/lucuma/odb/service/GuideService.scala
+++ modules/service/src/main/scala/lucuma/odb/service/GuideService.scala
@@ -86,6 +86,7 @@ import lucuma.odb.syntax.result.*
 import lucuma.odb.util.Codecs.*
 import monocle.Focus
 import monocle.Lens
+import org.typelevel.otel4s.Attribute
 import org.typelevel.otel4s.trace.Tracer
 import skunk.*
 import skunk.implicits.*
@@ -695,23 +696,25 @@ object GuideService {
         angles:        NonEmptyList[Angle],
         candidates:    NonEmptyList[GuideStarCandidate],
         trackType:     TrackType
-      ): Option[AgsAnalysis.Usable] =
-        genInfo.agsParamsFor(trackType).flatMap: params =>
-          Ags
-            .agsAnalysis(obsInfo.constraints,
-                        wavelength,
-                        baseCoords,
-                        scienceCoords,
-                        blindOffset,
-                        angles,
-                        genInfo.acqOffsets,
-                        genInfo.sciOffsets,
-                        params,
-                        candidates.toList
+      ): F[Option[AgsAnalysis.Usable]] =
+        genInfo.agsParamsFor(trackType).flatTraverse: params =>
+          val result =
+            Ags.agsAnalysis(obsInfo.constraints,
+                            wavelength,
+                            baseCoords,
+                            scienceCoords,
+                            blindOffset,
+                            angles,
+                            genInfo.acqOffsets,
+                            genInfo.sciOffsets,
+                            params,
+                            candidates.toList
             )
-            ._1
-            .sortUsablePositions
-            .headOption
+          T.currentSpanOrNoop
+            .flatMap: span =>
+              span.addAttributes(Attribute("ags.mode", params.mode)) *>
+                span.addAttributes(result.stats.toSpanAttributes*)
+            .as(result.sortUsablePositions.headOption)
 
       def buildAvailabilityAndCache(
         pid:             Program.Id,
@@ -969,7 +972,7 @@ object GuideService {
                               .toResult(generalError(s"No angles to test for guide target candidates for observation $oid.").asProblem)
                            )
           blindOffsetOpt <- ResultT.liftF(getBlindOffsetCoordinates(oid, obsTime.toInstant))
-          optUsable      = chooseBestGuideStar(obsInfo, genInfo.agsWavelength, genInfo, baseCoords, scienceCoords, blindOffsetOpt, angles, candidates, trackType)
+          optUsable      <- ResultT.liftF(chooseBestGuideStar(obsInfo, genInfo.agsWavelength, genInfo, baseCoords, scienceCoords, blindOffsetOpt, angles, candidates, trackType))
           tgts           = original.map(x => (x._2.id, x._1)).toList.toMap
           env            <- ResultT.fromResult(
                              optUsable
