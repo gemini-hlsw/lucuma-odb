18aad1643ad87cb9c41dde37ae1c2000b95036f8
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2026-09-02 16:15:09 -0400

    Add a queue for proposal PDF summary jobs

---
+++ modules/service/src/main/scala/lucuma/odb/graphql/input/RegenerateProposalSummariesInput.scala
@@ -0,0 +1,14 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.odb.graphql.input
+
+import lucuma.core.model.Program
+import lucuma.odb.graphql.binding.*
+
+case class RegenerateProposalSummariesInput(programId: Program.Id)
+
+object RegenerateProposalSummariesInput:
+  val Binding: Matcher[RegenerateProposalSummariesInput] =
+    ObjectFieldsBinding.rmap:
+      case List(ProgramIdBinding("programId", rPid)) => rPid.map(apply)
