635794e33d182460def9926c0cf9e39704b7baad
Rob Norris <rob_norris@mac.com>
2026-09-02 16:53:05 -0500

    update migration

--- modules/service/src/main/resources/db/migration/V1304__warning_dismissal.sql
+++ modules/service/src/main/resources/db/migration/V1304__warning_dismissal.sql
@@ -6,8 +6,16 @@ add column c_dismissed_warnings d_tag[] not null default '{}';
 
 create view v_program as
   select
-    p.*,
-    coalesce(rc.c_resource_count, 0) as c_resource_count,
-    ((now() at time zone 'UTC')::date between p.c_active_start and p.c_active_end) as c_is_active
-  from t_program p
-  left join t_program_resource_count rc on rc.c_program_id = p.c_program_id;
+    q.*,
+    coalesce(q.c_explicit_status, q.c_default_status) as c_status
+  from (
+    select
+      p.*,
+      coalesce(rc.c_resource_count, 0) as c_resource_count,
+      (case
+         when (now() at time zone 'UTC')::date between p.c_active_start and p.c_active_end then 'active'
+         else 'inactive'
+       end)::d_tag as c_default_status
+    from t_program p
+    left join t_program_resource_count rc on rc.c_program_id = p.c_program_id
+  ) q;
