2691106d77ee1c237884cae6eb78f48b889be0ee
Raúl Piaggio <rpiaggio@gemini.edu>
2026-09-07 16:39:53 -0300

    key per-program calibrations by their creation input

--- modules/service/src/test/scala/lucuma/odb/service/arb/ArbCalibrationConfigSubset.scala
+++ modules/service/src/test/scala/lucuma/odb/service/arb/ArbCalibrationConfigSubset.scala
@@ -3,15 +3,40 @@
 
 package lucuma.odb.service.arb
 
+import cats.syntax.option.*
 import lucuma.core.enums.*
 import lucuma.core.math.Wavelength
 import lucuma.core.math.arb.ArbWavelength.given
 import lucuma.core.util.arb.ArbEnumerated.given
+import lucuma.odb.graphql.input.GmosIfuInput
+import lucuma.odb.graphql.input.GmosLongSlitInput
 import lucuma.odb.service.CalibrationConfigSubset.*
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
 
 object ArbCalibrationConfigSubset:
+
+  // Shaped like the inputs toConfigSubset builds: every effective value pinned, nothing else set.
+  private val longSlitCommon: Gen[GmosLongSlitInput.Create.Common] =
+    for
+      w <- arbitrary[Wavelength]
+      x <- arbitrary[GmosXBinning]
+      y <- arbitrary[GmosYBinning]
+      r <- arbitrary[GmosAmpReadMode]
+      a <- arbitrary[GmosAmpGain]
+      o <- arbitrary[GmosRoi]
+    yield GmosLongSlitInput.Create.Common(w, none, x.some, y.some, r.some, a.some, o.some, none, none)
+
+  private val ifuCommon: Gen[GmosIfuInput.Create.Common] =
+    for
+      w <- arbitrary[Wavelength]
+      x <- arbitrary[GmosXBinning]
+      y <- arbitrary[GmosYBinning]
+      r <- arbitrary[GmosAmpReadMode]
+      a <- arbitrary[GmosAmpGain]
+      o <- arbitrary[GmosRoi]
+    yield GmosIfuInput.Create.Common(w, none, none, x.some, y.some, r.some, a.some, o.some, none, none)
 
   given Arbitrary[GmosNConfigs] =
     Arbitrary:
@@ -19,13 +44,8 @@ object ArbCalibrationConfigSubset:
         g <- arbitrary[GmosNorthGrating]
         f <- arbitrary[Option[GmosNorthFilter]]
         u <- arbitrary[GmosNorthFpu]
-        w <- arbitrary[Wavelength]
-        x <- arbitrary[GmosXBinning]
-        y <- arbitrary[GmosYBinning]
-        r <- arbitrary[GmosAmpReadMode]
-        a <- arbitrary[GmosAmpGain]
-        o <- arbitrary[GmosRoi]
-      yield GmosNConfigs(g, f, u, w, x, y, r, a, o)
+        c <- longSlitCommon
+      yield GmosNConfigs(GmosLongSlitInput.Create.North(g, f, u, c, none))
 
   given Arbitrary[GmosSConfigs] =
     Arbitrary:
@@ -33,13 +53,26 @@ object ArbCalibrationConfigSubset:
         g <- arbitrary[GmosSouthGrating]
         f <- arbitrary[Option[GmosSouthFilter]]
         u <- arbitrary[GmosSouthFpu]
-        w <- arbitrary[Wavelength]
-        x <- arbitrary[GmosXBinning]
-        y <- arbitrary[GmosYBinning]
-        r <- arbitrary[GmosAmpReadMode]
-        a <- arbitrary[GmosAmpGain]
-        o <- arbitrary[GmosRoi]
-      yield GmosSConfigs(g, f, u, w, x, y, r, a, o)
+        c <- longSlitCommon
+      yield GmosSConfigs(GmosLongSlitInput.Create.South(g, f, u, c, none))
+
+  given Arbitrary[GmosNIfuConfigs] =
+    Arbitrary:
+      for
+        g <- arbitrary[GmosNorthGrating]
+        f <- arbitrary[Option[GmosNorthFilter]]
+        u <- arbitrary[GmosNorthIfuFpu]
+        c <- ifuCommon
+      yield GmosNIfuConfigs(GmosIfuInput.Create.North(g, f, u, none, c))
+
+  given Arbitrary[GmosSIfuConfigs] =
+    Arbitrary:
+      for
+        g <- arbitrary[GmosSouthGrating]
+        f <- arbitrary[Option[GmosSouthFilter]]
+        u <- arbitrary[GmosSouthIfuFpu]
+        c <- ifuCommon
+      yield GmosSIfuConfigs(GmosIfuInput.Create.South(g, f, u, none, c))
 
   given Arbitrary[Flamingos2Configs] =
     Arbitrary:
@@ -48,31 +81,3 @@ object ArbCalibrationConfigSubset:
         f <- arbitrary[Flamingos2Filter]
         u <- arbitrary[Flamingos2Fpu]
       yield Flamingos2Configs(d, f, u)
-
-  given Arbitrary[GmosNIfuConfigs] =
-    Arbitrary:
-      for
-        g <- arbitrary[GmosNorthGrating]
-        f <- arbitrary[Option[GmosNorthFilter]]
-        u <- arbitrary[GmosNorthIfuFpu]
-        w <- arbitrary[Wavelength]
-        x <- arbitrary[GmosXBinning]
-        y <- arbitrary[GmosYBinning]
-        r <- arbitrary[GmosAmpReadMode]
-        a <- arbitrary[GmosAmpGain]
-        o <- arbitrary[GmosRoi]
-      yield GmosNIfuConfigs(g, f, u, w, x, y, r, a, o)
-
-  given Arbitrary[GmosSIfuConfigs] =
-    Arbitrary:
-      for
-        g <- arbitrary[GmosSouthGrating]
-        f <- arbitrary[Option[GmosSouthFilter]]
-        u <- arbitrary[GmosSouthIfuFpu]
-        w <- arbitrary[Wavelength]
-        x <- arbitrary[GmosXBinning]
-        y <- arbitrary[GmosYBinning]
-        r <- arbitrary[GmosAmpReadMode]
-        a <- arbitrary[GmosAmpGain]
-        o <- arbitrary[GmosRoi]
-      yield GmosSIfuConfigs(g, f, u, w, x, y, r, a, o)
