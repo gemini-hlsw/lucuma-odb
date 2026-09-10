8f99714ac2652ae5bf5125cc9fabc3dc6bb37bd7
Raúl Piaggio <rpiaggio@gemini.edu>
2026-09-10 18:32:23 -0300

    fix p0 tests and parser for combined filters

--- modules/phase0/src/test/scala/lucuma/odb/phase0/Phase0LoaderSuite.scala
+++ modules/phase0/src/test/scala/lucuma/odb/phase0/Phase0LoaderSuite.scala
@@ -26,11 +26,11 @@ class Phase0LoaderSuite extends CatsEffectSuite:
       .compile
       .toList
       .map: rows =>
-        assertEquals(rows.length, 324)
-        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Multislit), 161)
-        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Singleslit), 161)
-        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Ifu), 2)
-        assertEquals(rows.count(_._1.customSlitWidth.isDefined), 161)
+        assertEquals(rows.length, 314)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Multislit), 140)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Singleslit), 140)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Ifu), 34)
+        assertEquals(rows.count(_._1.customSlitWidth.isDefined), 140)
         assert(rows.forall(r => r._1.fpu.isDefined != r._1.customSlitWidth.isDefined))
 
   test("loadAll gmosSouth spectroscopy configurations"):
@@ -48,9 +48,11 @@ class Phase0LoaderSuite extends CatsEffectSuite:
       .compile
       .toList
       .map: rows =>
-        assertEquals(rows.length, 364)
-        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Multislit), 182)
-        assertEquals(rows.count(_._1.customSlitWidth.isDefined), 182)
+        assertEquals(rows.length, 359)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Multislit), 161)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Singleslit), 161)
+        assertEquals(rows.count(_._1.spec.fpuOption == FpuOption.Ifu), 37)
+        assertEquals(rows.count(_._1.customSlitWidth.isDefined), 161)
         assert(rows.forall(r => r._1.fpu.isDefined != r._1.customSlitWidth.isDefined))
 
   test("loadAll flamingos2 spectroscopy configurations"):
